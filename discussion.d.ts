70951e15761d9b7c12096999733e0df1b4362412
Hong Shik Branden Kim <kimploo@gmail.com>
2022-05-17 00:24:30 +0900

    initial commit

---
+++ discussion.d.ts
@@ -0,0 +1,17 @@
+interface Discussions {
+  id: string;
+  createdAt: string;
+  title: string;
+  url: string;
+  author: string;
+  answer?: Answer | null;
+  bodyHTML: string;
+}
+
+interface Answer {
+  id: string;
+  createdAt: string;
+  url: string;
+  author: string;
+  bodyHTML: string;
+}
