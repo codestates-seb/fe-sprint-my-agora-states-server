b329932b1fecb44c04494510b9e919f3c919deaa
Hong Shik Branden Kim <kimploo@gmail.com>
2022-06-17 00:15:26 +0900

    student repository

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -5,20 +5,18 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 // TODO: cors를 적용합니다.
-app.use(cors());
+
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-app.use(express.json());
 // OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 const port = 3001;
 const discussionsRouter = require('./router/discussions');
 
 // TODO: /discussions 경로로 라우팅합니다. 
-app.use('/discussions', discussionsRouter);
 
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome, My Agora States Server!');
+  // TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
+  throw '';
 });
 
 const server = app.listen(port, () => {
