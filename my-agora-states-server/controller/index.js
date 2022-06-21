9263778e8ca991c6525055a89f68c82b74b8ae94
crdrst777 <crdrst777@gmail.com>
2022-06-21 15:44:41 +0900

    SEB_FE_39_이혜진 과제 제출합니다

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,14 +1,26 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    res.status(200).json(discussionsData);
+
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+
+    const data = discussionsData.filter((el) => {
+      return Number(req.params.id) === el.id;
+    });
+
+    if (data.length !== 0) {
+      return res.status(200).json(...data);
+    } else {
+      return res.status(404).json('Not Found!');
+    }
   },
 
   createOne: (req, res) => {
