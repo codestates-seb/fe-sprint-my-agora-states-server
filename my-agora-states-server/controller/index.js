a2d8f1f8d331e8fb974a10bad29149205a5ba849
cleats01 <cleats01@gmail.com>
2022-06-21 15:23:12 +0900

    pagination

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,9 +4,35 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    let data = discussionsData;
-    return res.status(200).json(data);
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+    let data = discussionsData;
+    if (req.query.page) {
+      if (req.query.limit) {
+        if (req.query.limit > discussionsData.length || req.query.limit < 0) {
+          return res.status(200).json([]);
+        } else if (isNaN(+req.query.limit)) {
+          return res.status(400).json();
+        }
+        data = discussionsData.slice((req.query.page-1)*req.query.limit, req.query.page*req.query.limit);
+        return res.status(200).json(data);
+      } else {
+        data = discussionsData.slice(0,10);
+        return res.status(200).json(data);
+      }
+    } else {
+      if (req.query.limit) {
+        if (req.query.limit > discussionsData.length || req.query.limit < 0) {
+          return res.status(200).json([]);
+        } else if (isNaN(+req.query.limit)) {
+          return res.status(400).json();
+        }
+        data = discussionsData.slice(0,req.query.limit);
+        return res.status(200).json(data);
+      } else {
+        data = discussionsData.slice(0,10);
+        return res.status(200).json(data);
+      }
+    }
   },
 
   findById: (req, res) => {
