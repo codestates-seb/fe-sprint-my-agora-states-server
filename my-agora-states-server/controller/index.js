6bef03b121e44c378487467b2efbb1bc0325926b
mini <0miiii@naver.com>
2022-06-21 16:30:51 +0900

    bare minimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,11 +4,34 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    res.send(discussionsData);
+    console.log('응답했어요');
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+
+    // res.send(req.params.id);
+    // res.json(discussionsData[1].id);
+    
+    // console.log(filteredId);
+    // console.log(typeof parseInt(req.params.id))
+    // console.log(typeof discussionsData[0].id)
+    
+    
+    let filteredId = discussionsData.filter(el => parseInt(req.params.id) === el.id);
+    if(filteredId.length === 0) {
+      // res.status(404)뒤에 .end()를 붙여줘야함
+      // 근데 왜?
+      return res.status(404).end('일치하는 아이디가 없어요');
+    } else {
+      // 구조분해할당말고 방법없나?
+      // return res.status(200).json(filteredId[0]);
+      const [{id, createdAt, updatedAt, title, url, author, answer, bodyHTML, avatarUrl}] = filteredId;
+      return res.status(200).json({id, createdAt, updatedAt, title, url, author, answer, bodyHTML, avatarUrl});
+    }
+
   },
 
   createOne: (req, res) => {
