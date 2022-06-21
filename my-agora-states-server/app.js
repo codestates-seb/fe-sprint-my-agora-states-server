2a5335c3998a25e3142539fa8bdc0f1efcc382fc
seokmin Hong <seokmin0016@gmail.com>
2022-06-21 13:46:50 +0900

    app.js완료

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -7,17 +7,20 @@ const morgan = require('morgan');
 // TODO: cors를 적용합니다.o
 
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.o
-// OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
+// OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.0
 
 const port = 3001;
 const discussionsRouter = require('./router/discussions');
 app.use(cors());//cors처리
 app.use(express.json());//express.json 적용
-// TODO: /discussions 경로로 라우팅합니다. 
+app.use(morgan("tiny"));//morgan 적용
+// TODO: /discussions 경로로 라우팅합니다. o
+app.use('/discussions', discussionsRouter); //  /discussions 경로로 라우팅
 
 app.get('/', (req, res) => {
   // TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
-  throw '';
+  res.status(200).send('this is agora-states-server');
+  throw 'Not Found!';
 });
 
 const server = app.listen(port, () => {
