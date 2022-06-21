ee8a8d56ff0ce660696059a25f528d7e073f9a27
sweesweett <sweesweet@naver.com>
2022-06-21 10:45:30 +0900

    bare minimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,13 +1,27 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
-
+let filtering;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    console.log('여긴들어오니?');
+    return res.status(200).json(agoraStatesDiscussions);
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
+    console.log('들어오긴하니?');
+    filtering = agoraStatesDiscussions.filter(
+      (el) => Number(req.params.id) === el.id
+    );
+    console.log(filtering.length);
+
+    if (filtering.length === 0) {
+      return res.status(404).json({});
+    } else {
+      return res.status(200).json(...filtering);
+    }
+
     // TODO: path parameter id를 가진 discussion을 응답합니다.
   },
 
