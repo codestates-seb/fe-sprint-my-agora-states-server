1469645ee6684d2466c3285ac92004c8c5b73a7d
pongdang <pongdang0121@gmail.com>
2022-06-21 09:04:22 +0900

    feat: findAll 구현

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,9 +1,10 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+const discussionsData = [...agoraStatesDiscussions];
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    return res.status(200).json(discussionsData);
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
