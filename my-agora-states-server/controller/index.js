6414d88e67a4c99d7d72fa04d08ec62599c854a2
tahupapa <xognl3164@gmail.com>
2022-06-21 17:38:43 +0900

    done

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,10 +5,15 @@ const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+
+    const data = discussionsData.filter(item => item.id + '' === req.params.id);
+    return data.length ? res.json(data[0]) : res.status(404).send("error");
   },
 
   createOne: (req, res) => {
@@ -24,6 +29,6 @@ const discussionsController = {
   },
 };
 
-module.exports = {
+module.exports = { 
   discussionsController,
 };
