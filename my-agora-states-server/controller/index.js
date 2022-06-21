24e2643bef7373197e6b823f60e1e318bb1fc170
HelloHailie <alsltjsals@gmail.com>
2022-06-21 14:38:18 +0900

    Bare Minimum Requirement done

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,13 +1,33 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { isErrored } = require('jsdom/lib/jsdom/living/helpers/http-request');
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
+    // console.log(req.query);
+    // console.log(req.params);
     // TODO: 모든 discussions 목록을 응답합니다.
+    return res.status(200).json(discussionsData);
+
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
+    // console.log(req.params);
+    // console.log(req.query);
+    // console.log(req.params.id);
+    // console.log(req.query.id);
+    const data = discussionsData.filter(
+      (discussion) => discussion.id === Number(req.params.id)
+    );
+
+    if (data.length > 0) {
+      return res.status(200).json(...data);
+    } else {
+      // return res.status(404).json(null);
+      return res.status(404).end();
+    }
+
     // TODO: path parameter id를 가진 discussion을 응답합니다.
   },
 
