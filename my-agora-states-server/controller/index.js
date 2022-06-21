ed49669f3132bdf52842e26cdd4228c0ba12b50d
RohHeeJin <ranin00421@gmail.com>
2022-06-21 16:25:11 +0900

    Bareminimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,10 +5,29 @@ const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+    let data = discussionsData;
+    return res.status(200).json(data);
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    // if (discussionsData.filter((el) => el.id === +req.params.id).length) {
+    //   let data = discussionsData.filter((el) => {
+    //     return el.id === +req.params.id;
+    //   })[0];
+    //   return res.status(200).json(data);
+    // } else {
+    //   return res.status(404).json();
+    // }
+    let data = discussionsData;
+    if (req.params.id) {
+      data = data.filter((el) => el.id === Number(req.params.id));
+      if (data.length === 0) {
+        return res.status(404).send();
+      } else {
+        return res.status(200).json(...data);
+      }
+    }
   },
 
   createOne: (req, res) => {
