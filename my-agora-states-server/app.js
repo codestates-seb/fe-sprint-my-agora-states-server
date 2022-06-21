7d8147ff44f84a7f37b7f2013340a4d28ecadcd8
juyo <jjbox7@naver.com>
2022-06-21 16:01:56 +0900

    test1

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
 const morgan = require('morgan');
-
+const port = 3001;
+const discussionsRouter = require('./router/discussions');
 // TODO: cors를 적용합니다.
 app.use(cors());
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
 app.use(express.json({strict:false}));
+app.use(morgan());
 // OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
 
-const port = 3001;
-const discussionsRouter = require('./router/discussions');
 app.use('/discussions',discussionsRouter);
 
 // TODO: /discussions 경로로 라우팅합니다. 
 
 app.get('/', (req, res) => {
   // TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
-  res.status(200).send("Good")
+  res.status(200).json("Hi Juyo");
+  throw '';
 });
-
 const server = app.listen(port, () => {
   console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
 });
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,33 +1,39 @@
+const { json } = require("express");
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    let data = discussionsData;
-    return res.status(200).json(data);
+    return res.status(200).json(discussionsData);
     // TODO: 모든 discussions 목록을 응답합니다.
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
-    if (discussionsData.filter(el => el.id === req.params.id).length);
-    let data = discussionsData.filter(el => {
-      return (el.id === req.params.id);
-    })[0];
-    return res.status(200).json(data);
+    let list = [];
+    list = discussionsData.filter((el)=>el.id === Number(req.params.id))
+    
+    if (list.length !== 0){
+      return res.status(200).json(...list);
+    }
+    return res.status(404).json({message : 'empty ID'})
     // TODO: path parameter id를 가진 discussion을 응답합니다.
   },
+  //a
 
 
   createOne: (req, res) => {
+    return res.status(200).json(list);
     // ADVANCED: 새로운 discussion을 생성합니다.
   },
 
   updateById: (req, res) => {
+    return res.status(200).json(list);
     // ADVANCED: path parameter id를 가진 discussion을 요청에 맞게 변경합니다.
   },
 
-  deleteById: (req, res) => {
+  deleteById: async(req, res) => {
+    return res.status(200).json(discussionsData);
     // ADVANCED: path parameter id를 가진 discussion을 삭제합니다.
   },
 };
