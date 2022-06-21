101fe4cdf91e5370983c9a09a39320b4ed211203
leejuhanKr <juhan96@gmail.com>
2022-06-21 15:05:38 +0900

    advanced 기능 구현

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,8 +5,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    return res.json(discussionsData);
+    if (!Object.keys(req.query).length) {
+      return res.status(200).json(discussionsData);
+    }
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+    let limit = Number(req.query.limit??10)
+    let page = Number(req.query.page??0)
+
+    if (Number.isNaN(limit) || Number.isNaN(page)) {
+      return res.status(400).json([])
+    }
+    const filtered = discussionsData.slice(page*limit, (page+1)*limit)
+    return res.status(200).json(filtered)
   },
 
   findById: (req, res) => {
