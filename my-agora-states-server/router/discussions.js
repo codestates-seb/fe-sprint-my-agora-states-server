8f6af46bd78fe0f034a0822ee518372d25d06f7b
seokmin Hong <seokmin0016@gmail.com>
2022-06-21 13:53:35 +0900

    rouoter/discussions 베어미니멈까지 완료

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,13 +1,13 @@
 // TODO: discussions 라우터를 완성합니다.
-const { discussionsController } = require('../controller');
+const { discussionsController } = require('../controller/index');
 const { findAll, findById, createOne, updateById, deleteById } = discussionsController;
 const express = require('express');
 const router = express.Router();
 
-// TODO: 모든 discussion 목록을 조회하는 라우터를 작성합니다.
-
+// TODO: 모든 discussion 목록을 조회하는 라우터를 작성합니다.o
+router.get('/', findAll);
 // TODO: discussion 하나를 조회하는 라우터를 작성합니다.
-
+router.get('/:id',findById);
 // ADVANCED: discussion 하나를 생성하는 라우터를 작성합니다.
 
 // ADVANCED: discussion 하나를 수정하는 라우터를 작성합니다.
