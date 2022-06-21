9ec870155804a8f1ac7aa011279e9d435cdc6ab8
jjjjinyoung2 <okk950122@gmail.com>
2022-06-21 14:08:35 +0900

    과제

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,14 +1,20 @@
+const { it } = require("mocha");
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    let data = discussionsData;
+    return res.status(200).json(data);
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    const { id } = req.params;
+    const item = discussionsData.filter((e) => e.id === Number(id));
+    return item.length ? res.status(200).json(...item) : res.status(404).send();
   },
 
   createOne: (req, res) => {
