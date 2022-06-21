1d5b6d8ac95b9b0dea6cae572f4839f0ba605134
2prize <dlsvlslxl12@gmail.com>
2022-06-21 15:43:01 +0900

    FE-39 이상원 과제제출

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -3,6 +3,7 @@ const { discussionsController } = require('../controller');
 const { findAll, findById, createOne, updateById, deleteById } = discussionsController;
 const express = require('express');
 const router = express.Router();
+const { app } = require('../app')
 
 // TODO: 모든 discussion 목록을 조회하는 라우터를 작성합니다.
 
@@ -13,5 +14,17 @@ const router = express.Router();
 // ADVANCED: discussion 하나를 수정하는 라우터를 작성합니다.
 
 // ADVANCED: discussion 하나를 삭제하는 라우터를 작성합니다.
+
+router.get('/', findAll);
+
+router.get('/:id', findById);
+
+router.put('/:id', updateById);
+
+router.post('/', createOne);
+
+router.delete('/', deleteById);
+
+
 
 module.exports = router;
