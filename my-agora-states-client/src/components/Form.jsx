09a7ea638e7568a61c1b3fd369dc75c2bd8624ee
pongdang <pongdang0121@gmail.com>
2022-06-21 12:07:28 +0900

    feat: api 사용

--- my-agora-states-client/src/components/Form.jsx
+++ my-agora-states-client/src/components/Form.jsx
@@ -1,25 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { postDiscussion } from "../api/discussions";
 
 export default function Form() {
+  const [username, setUsername] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  function handleNameChange(event) {
+    console.log(event.target.value);
+    setUsername(event.target.value);
+  }
+
+  function handleTitleChange(event) {
+    console.log(event.target.value);
+    setTitle(event.target.value);
+  }
+
+  function handleContentChange(event) {
+    console.log(event.target.value);
+    setContent(event.target.value);
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function handleSubmit(event) {
+    event.preventDefault();
+    postDiscussion({
+      author: username,
+      title: title,
+      bodyHTML: content,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      avatarUrl: "https://avatars.githubusercontent.com/u/76990149?v=4",
+    });
+    window.location.replace("/");
+  }
+
   return (
     <section className='form__container'>
-      <form action='' method='post' className='form'>
+      <form className='form'>
         <div className='form__input--wrapper'>
           <div className='form__input--name'>
             <label htmlFor='name'>이름을 입력해주세요</label>
-            <input type='text' name='name' id='name' required />
+            <input type='text' name='name' id='name' required onChange={handleNameChange} />
           </div>
           <div className='form__input--title'>
             <label htmlFor='title'>제목을 입력해주세요</label>
-            <input type='text' name='title' id='title' required />
+            <input type='text' name='title' id='title' required onChange={handleTitleChange} />
           </div>
           <div className='form__textbox'>
             <label htmlFor='story'>질문을 입력해주세요</label>
-            <textarea id='story' name='story' placeholder='질문을 작성하세요' required />
+            <textarea id='story' name='story' placeholder='질문을 작성하세요' required onChange={handleContentChange} />
           </div>
         </div>
         <div className='form__submit'>
-          <button type='submit' className='submit-button'>
+          <button type='submit' className='submit-button' onClick={handleSubmit}>
             제출
           </button>
         </div>
