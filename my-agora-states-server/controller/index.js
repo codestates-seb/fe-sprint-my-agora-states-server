ad78c2c9d5034cd64c5da8c9d490b1810cabb357
chaeyun <104177189+onecyn@users.noreply.github.com>
2022-06-21 19:57:38 +0900

    bareminimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,11 +5,43 @@ const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: path parameter id를 가진 discussion을 응답합니다.
-  },
+        // TODO: path parameter id를 가진 discussion을 응답합니다.
+        let list = discussionsData;
+        if (req.params.id !== undefined) {
+          list = list.filter((item) => Number(req.params.id) === item.id);
+          if (list.length === 0) {
+            return res.status(404).end();
+          }
+          const [
+            {
+              id,
+              createdAt,
+              updatedAt,
+              title,
+              url,
+              author,
+              answer,
+              bodyHTML,
+              avatarUrl,
+            },
+          ] = list;
+          return res.status(200).json({
+            id,
+            createdAt,
+            updatedAt,
+            title,
+            url,
+            author,
+            answer,
+            bodyHTML,
+            avatarUrl,
+          });
+        }
+      },
 
   createOne: (req, res) => {
     // ADVANCED: 새로운 discussion을 생성합니다.
