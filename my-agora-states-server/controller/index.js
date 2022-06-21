27a448e68cbb69e9091590ab5fe712908e0147b5
seul <seul.dev@gmail.com>
2022-06-21 11:49:08 +0900

    bare minimum requirements fullfiled

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,14 +1,23 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    const data = discussionsData;
+    return res.status(200).json(data);
+
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    const data = discussionsData.filter((item) => +req.params.id === item.id);
+    if (data.length > 0) {
+      return res.status(200).json(...data);
+    } else {
+      return res.status(404).json(null);
+    }
   },
 
   createOne: (req, res) => {
