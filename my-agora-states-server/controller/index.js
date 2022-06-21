177a207e3279d6f698c39625f233b75cd37f4ce2
JiYJ725 <wldudwns07@naver.com>
2022-06-21 16:53:51 +0900

    FE39 지영준 1차제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,11 +4,24 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    return res.status(200).json(discussionsData);
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    const { id } = req.params;
+    const result = discussionsData.filter((obj) => obj.id === Number(id));
+    if (result.length) return res.status(200).json(...result);
+    throw res.status(404).json("ID가 존재하지 않습니다.");
+  },
+
+  findByName: (req, res) => {
+    // TODO: path parameter id를 가진 discussion을 응답합니다.
+    const { author } = req.params;
+    const result = discussionsData.filter((obj) => obj.author === author);
+    if (result.length) return res.status(200).json(result);
+    throw res.status(404).json("ID가 존재하지 않습니다.");
   },
 
   createOne: (req, res) => {
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,13 +1,15 @@
 // TODO: discussions 라우터를 완성합니다.
-const { discussionsController } = require('../controller');
-const { findAll, findById, createOne, updateById, deleteById } = discussionsController;
-const express = require('express');
+const { discussionsController } = require("../controller");
+const { findAll, findById, createOne, updateById, deleteById, findByName } =
+  discussionsController;
+const express = require("express");
 const router = express.Router();
 
 // TODO: 모든 discussion 목록을 조회하는 라우터를 작성합니다.
-
+router.get("/", findAll);
 // TODO: discussion 하나를 조회하는 라우터를 작성합니다.
-
+router.get("/:id", findById);
+router.get("/name/:author", findByName);
 // ADVANCED: discussion 하나를 생성하는 라우터를 작성합니다.
 
 // ADVANCED: discussion 하나를 수정하는 라우터를 작성합니다.
