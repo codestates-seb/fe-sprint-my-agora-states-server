ba813c3078c18728656c3dec242c66a56cccd65b
TAETAEMO <pjh7179200@gmail.com>
2022-06-21 13:33:12 +0900

    FE39기안태건과제제출

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -5,20 +5,28 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 // TODO: cors를 적용합니다.
-
+app.use(cors());
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+app.use(express.json());
 // OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
+app.use(morgan());
 
 const port = 3001;
 const discussionsRouter = require('./router/discussions');
 
 // TODO: /discussions 경로로 라우팅합니다. 
-
+//수정1
 app.get('/', (req, res) => {
   // TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
+  res.status(200).send('Welcome, My States!');
   throw '';
 });
-
+//수정2
+app.use('/discussions', discussionsRouter);
+ 
+app.use((req, res, next) => {
+  res.status(404).send('Not Found!');
+})
 const server = app.listen(port, () => {
   console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
 });
