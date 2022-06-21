ebd4d867b246d696820a9bd39b5fec8770efcf20
daunjeong <qkrtjrfo1@google.com>
2022-06-21 16:01:56 +0900

    fullRequest

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,12 +1,14 @@
 // TODO: discussions 라우터를 완성합니다.
-const { discussionsController } = require('../controller');
-const { findAll, findById, createOne, updateById, deleteById } = discussionsController;
-const express = require('express');
+const { discussionsController } = require("../controller/index.js");
+const { findAll, findById, createOne, updateById, deleteById } =
+  discussionsController;
+const express = require("express");
 const router = express.Router();
 
 // TODO: 모든 discussion 목록을 조회하는 라우터를 작성합니다.
-
+router.get("/", findAll);
 // TODO: discussion 하나를 조회하는 라우터를 작성합니다.
+router.get("/:id", findById);
 
 // ADVANCED: discussion 하나를 생성하는 라우터를 작성합니다.
 
