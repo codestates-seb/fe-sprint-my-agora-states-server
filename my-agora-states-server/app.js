8342a659ea12ecbe66001996d496c73680248dc1
Beanxx <tnqls991211@naver.com>
2022-06-21 14:42:34 +0900

    SEB_FE_39_최수빈 과제 제출합니다.

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 
 // TODO: cors를 적용합니다.
 
@@ -10,13 +10,23 @@ const morgan = require('morgan');
 // OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
 
 const port = 3001;
-const discussionsRouter = require('./router/discussions');
+const discussionsRouter = require("./router/discussions");
 
-// TODO: /discussions 경로로 라우팅합니다. 
+app.use(cors());
+app.use(morgan());
+app.use(express.json());
 
-app.get('/', (req, res) => {
+// TODO: /discussions 경로로 라우팅합니다.
+app.use("/discussions", discussionsRouter);
+
+app.get("/", (req, res) => {
   // TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
-  throw '';
+  res.status(200).send("what the agora?");
+  throw new error("my error");
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Not Found!");
 });
 
 const server = app.listen(port, () => {
