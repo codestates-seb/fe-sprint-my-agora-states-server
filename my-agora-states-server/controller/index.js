b80618c55e7a91d7a47285d7e31bbe854c2b3290
TaeHyung KIM <th1122kim@gmail.com>
2022-06-21 11:49:52 +0900

    Bare Minimum test completed

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,12 +3,24 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
+    let data = agoraStatesDiscussions;
     // TODO: 모든 discussions 목록을 응답합니다.
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+    return res.status(200).json(data);
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    let data = agoraStatesDiscussions;
+    if(req.params.id) {
+      data = data.filter( el => el.id === Number(req.params.id) )
+      if(data.length === 0) {
+        return res.status(404).send('Not Found!')
+      }
+      else {
+        return res.status(200).json(...data)
+      }
+    }
   },
 
   createOne: (req, res) => {
