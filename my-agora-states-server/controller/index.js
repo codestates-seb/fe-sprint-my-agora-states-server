a7029df3442517d61e4be1612f4a5b2eabae8546
xuperxonic <xuperxonic12@gmail.com>
2022-06-21 16:14:25 +0900

    SEB_FE_39 황인준

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,28 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    let newData = agoraStatesDiscussions;
+
+    return res.status(200).json(newData);
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    let newData = agoraStatesDiscussions;
+    if(req.params.id) {
+      newData = newData.filter( el => el.id === Number(req.params.id) )
+      if (newData.length === 0) {
+        return res.status(404).send('Not Found!')
+      } else {
+        return res.status(200).json(...newData)
+      }
+    }
   },
 
   createOne: (req, res) => {
     // ADVANCED: 새로운 discussion을 생성합니다.
+    
   },
 
   updateById: (req, res) => {
