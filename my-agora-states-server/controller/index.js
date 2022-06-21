fec18e922038d53b8b0239d5407e31929a83c1b6
so191925 <so191925@gmail.com>
2022-06-21 16:25:32 +0900

    myAgora

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,11 +4,34 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+    res.status(200).json(discussionsData);
   },
+
+  // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+
+  // findById: (req, res) => {
+  //   // TODO: path parameter id를 가진 discussion을 응답합니다.
+  //   for (let el of discussionsData) {
+  //     if (String(el.id) === req.params.id) {
+  //       res.status(200).json(el);
+  //     }
+  //   }
+  //   res.status(404).send("ID does not exist.");
+  // },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+
+    let data = discussionsData;
+    //req.params는 객체. 빈객체는 true로 판단이 된다.Object.keys로 req.params의 키를 뽑아서 배열로 만들었을때 배열의 길이 판별
+    if (Object.keys(req.params).length !== 0) {
+      data = data.filter((path) => Number(req.params.id) === path.id);
+    }
+    console.log(req.params);
+    if (data.length) {
+      res.status(200).json(data[0]);
+    }
+    res.status(404).end();
   },
 
   createOne: (req, res) => {
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,17 +1,19 @@
 // TODO: discussions 라우터를 완성합니다.
-const { discussionsController } = require('../controller');
-const { findAll, findById, createOne, updateById, deleteById } = discussionsController;
-const express = require('express');
+const { discussionsController } = require("../controller");
+const { findAll, findById, createOne, updateById, deleteById } =
+  discussionsController;
+const express = require("express");
 const router = express.Router();
 
 // TODO: 모든 discussion 목록을 조회하는 라우터를 작성합니다.
-
+router.get("/", findAll);
 // TODO: discussion 하나를 조회하는 라우터를 작성합니다.
+router.get("/:id", findById);
 
-// ADVANCED: discussion 하나를 생성하는 라우터를 작성합니다.
-
-// ADVANCED: discussion 하나를 수정하는 라우터를 작성합니다.
-
-// ADVANCED: discussion 하나를 삭제하는 라우터를 작성합니다.
-
+// // ADVANCED: discussion 하나를 생성하는 라우터를 작성합니다.
+// router.post("/", createOne);
+// // ADVANCED: discussion 하나를 수정하는 라우터를 작성합니다.
+// router.put("/", updateById);
+// // ADVANCED: discussion 하나를 삭제하는 라우터를 작성합니다.
+// router.delete("/", deleteById);
 module.exports = router;
