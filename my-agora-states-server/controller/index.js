0aa9b96f5b60df722b1abdb86d32f145fdc6e24e
dormirMais <74685291+dormirMais@users.noreply.github.com>
2022-06-21 13:32:28 +0900

    bareminimum완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,10 +5,32 @@ const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+
+    let newDiscussions = discussionsData;
+
+    return res.status(200).send(newDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    let newDiscussions = discussionsData;
+    let discussion;
+    let isConstain = false;
+
+    for(let i=0; i<newDiscussions.length; i++){
+      if(newDiscussions[i].id === Number(req.params.id)) {
+        isConstain = true;
+        discussion = newDiscussions[i]; 
+        break;
+      }
+      console.log("param.id => ", req.params.id , typeof(req.params.id));
+      console.log("discussion.id => ",newDiscussions[i].id, typeof(newDiscussions[i].id))
+    }
+    if(isConstain) return res.status(200).send(discussion);
+    else return res.status(404).send("없다..");
+
+
+    return res.send(newDiscussions);
   },
 
   createOne: (req, res) => {
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -4,9 +4,11 @@ const { findAll, findById, createOne, updateById, deleteById } = discussionsCont
 const express = require('express');
 const router = express.Router();
 
-// TODO: 모든 discussion 목록을 조회하는 라우터를 작성합니다.
+// TODO: 모든 discussion 목록을 조회하는 라우터를 작성합니다.'
+router.get("/", findAll);
 
 // TODO: discussion 하나를 조회하는 라우터를 작성합니다.
+router.get("/:id", findById);
 
 // ADVANCED: discussion 하나를 생성하는 라우터를 작성합니다.
 
