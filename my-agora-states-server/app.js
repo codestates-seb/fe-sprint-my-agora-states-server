6fb0d78180f2e2ca09125491cb41713fa099c9cc
JoEunsoo <dcpg95@gmail.com>
2022-06-21 14:46:19 +0900

    agora-states-sever Porject

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
+const { use } = require('./router/discussions');
 const cors = require('cors');
 const morgan = require('morgan');
+const jsonParser = express.json({strict:false})
 
 // TODO: cors를 적용합니다.
 
@@ -12,12 +13,21 @@ const morgan = require('morgan');
 const port = 3001;
 const discussionsRouter = require('./router/discussions');
 
+
+app.use(jsonParser)
+app.use(cors())
+app.use('/discussions',discussionsRouter);
 // TODO: /discussions 경로로 라우팅합니다. 
+
 
 app.get('/', (req, res) => {
   // TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
-  throw '';
+   throw res.status(200).send('helloWolrd!');
 });
+
+app.use((req,res,next) => {
+  res.status(404).send('Not Found!');
+})
 
 const server = app.listen(port, () => {
   console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
