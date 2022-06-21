eb11295b1877c63b67f58cc007896ccfe1bc0549
songseul <song.codestates@gmail.com>
2022-06-21 14:25:07 +0900

    FE_39_송슬기 과제제출합니다.

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,14 +1,26 @@
+const e = require("cors");
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
+    // TODO: 모든 discussions 목록을 응답합니다
+    return res.status(200).json(discussionsData);
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    const { id } = req.params;
+    if (id !== undefined) {
+      const data = discussionsData.find((el) => el.id === Number(id));
+
+      if (data == null) {
+        return res.status(404).end();
+      }
+      return res.status(200).json(data);
+    }
+    return res.status(404).end();
   },
 
   createOne: (req, res) => {
