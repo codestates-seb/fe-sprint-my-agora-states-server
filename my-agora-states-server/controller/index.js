8420615e49b64f1b4a865e6d99194f937f16194b
mellomello-made <mellomello.made@gmail.com>
2022-06-21 11:53:43 +0900

    agora states server

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,11 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    let data = discussionsData;
+    return res.status(200).json(data);
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
-    // TODO: path parameter id를 가진 discussion을 응답합니다.
+    // TODO: path parameter id를 가진 discussion을 응답합니다.req.params.id 문자열의 상태에서 앞에 +하면 number로 변환하는 방법
+    if (discussionsData.filter((el) => el.id === +req.params.id).length) {
+      let data = discussionsData.filter((el) => {
+        return el.id === +req.params.id;
+      })[0];
+      return res.status(200).json(data);
+    } else {
+      return res.status(404).json();
+    }
   },
 
   createOne: (req, res) => {
