84729855d0027335c9dc0b4bc084733101d868ae
manyb2auties <damidang51@gmail.com>
2022-06-21 13:52:41 +0900

    bare-minimum 통과

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,14 +1,36 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
+
+
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    return res.json(discussionsData)
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    // let diss = discussionsData.filter(el => {
+    //   return req.params.id === el.id
+    // })
+    // console.log(diss) // 왜 빈배열로 나오지?
+    // return res.json(diss)
+    const {id} = req.params; 
+    let data;
+    // console.log(typeof discussionsData[0].id) // number 타입
+    // console.log(typeof id) // string 타입
+    for(let i=0; i<discussionsData.length; i++){
+      if(discussionsData[i].id === Number(id)){
+        data = discussionsData[i]
+        return res.status(200).json(data)
+      }
+    }
+    if(data === undefined){
+      return res.status(404).send('NOT FOUND!')
+    }
+    
   },
 
   createOne: (req, res) => {
