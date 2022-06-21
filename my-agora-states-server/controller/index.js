96b16898f759ea84bb535bd38babcfc2e7ec0abe
juyo <jjbox7@naver.com>
2022-06-21 11:14:11 +0900

    test

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,13 +3,21 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
+    let data = discussionsData;
+    return res.status(200).json(data);
     // TODO: 모든 discussions 목록을 응답합니다.
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
+    if (discussionsData.filter(el => el.id === req.params.id).length);
+    let data = discussionsData.filter(el => {
+      return (el.id === req.params.id);
+    })[0];
+    return res.status(200).json(data);
     // TODO: path parameter id를 가진 discussion을 응답합니다.
   },
+
 
   createOne: (req, res) => {
     // ADVANCED: 새로운 discussion을 생성합니다.
