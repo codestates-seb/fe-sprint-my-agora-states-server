4a3539dcbf40b24e6b04abbe86bba5d9681ba407
sehyeon <hyeon4687@naver.com>
2022-06-21 14:03:38 +0900

    과제제출

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -8,14 +8,19 @@ const morgan = require('morgan');
 
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
 // OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
-
+// app.use(logger())
+app.use(cors())
+app.use(express.json({strict : false}))
 const port = 3001;
 const discussionsRouter = require('./router/discussions');
+const router = require('./router/discussions');
 
 // TODO: /discussions 경로로 라우팅합니다. 
+app.use('/discussions', discussionsRouter)
 
 app.get('/', (req, res) => {
   // TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
+  res.status(200).send('')
   throw '';
 });
 
