46cafe34885dce477f07ec69dc6525cbef45a15e
LastCarol <lastcarol0315@gmail.com>
2022-06-21 14:01:19 +0900

    베어미니엄 완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,11 +3,22 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
+    let a = discussionsData;
+    return res.status(200).json(a);
     // TODO: 모든 discussions 목록을 응답합니다.
+
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
+    let a = discussionsData.filter((e) => {
+      return Number(req.params.id) === e.id
+    })
+    if(a.length !== 0){
+      return res.status(200).json(...a)
+    }else{
+      return res.status(404).send()
+    }
     // TODO: path parameter id를 가진 discussion을 응답합니다.
   },
 
