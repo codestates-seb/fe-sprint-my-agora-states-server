540d1eea9226f84ca8d275c8ea2b800ce7919f4b
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-06-21 16:58:14 +0900

    feat: 페이징 네이션 기능 구현

--- my-agora-states-server/__test__/index.test.js
+++ my-agora-states-server/__test__/index.test.js
@@ -20,7 +20,7 @@ describe('Bare Minimum Requirements - Server', () => {
     test('상태 코드 200을 응답합니다.', () => {
       return request(server)
         .get('/')
-        .then((res) => {
+        .then(res => {
           const { status } = res;
           expect(status).toEqual(200);
           return;
@@ -40,7 +40,7 @@ describe('Bare Minimum Requirements - Server', () => {
     test('상태 코드 200을 응답합니다.', () => {
       return request(server)
         .get('/discussions')
-        .then((res) => {
+        .then(res => {
           const { status } = res;
           expect(status).toEqual(200);
           return;
@@ -50,12 +50,12 @@ describe('Bare Minimum Requirements - Server', () => {
     test('모든 discussions 목록을 응답합니다.', () => {
       return request(server)
         .get('/discussions')
-        .then((res) => {
+        .then(res => {
           const { body } = res;
           expect(Array.isArray(body)).toEqual(true);
           agoraStatesDiscussions.forEach((discussion, index) => {
-            expect(body[index].title).toEqual(discussion.title)
-          })
+            expect(body[index].title).toEqual(discussion.title);
+          });
           return;
         });
     });
@@ -73,7 +73,7 @@ describe('Bare Minimum Requirements - Server', () => {
     test('해당 id가 존재하는 경우, 상태 코드 200를 응답합니다.', () => {
       return request(server)
         .get('/discussions/2')
-        .then((res) => {
+        .then(res => {
           const { status } = res;
           expect(status).toEqual(200);
           return;
@@ -83,7 +83,7 @@ describe('Bare Minimum Requirements - Server', () => {
     test('해당 id가 존재하지 않는경우, 상태 코드 404를 응답합니다.', () => {
       return request(server)
         .get('/discussions/1239018')
-        .then((res) => {
+        .then(res => {
           const { status } = res;
           expect(status).toEqual(404);
           return;
@@ -93,7 +93,7 @@ describe('Bare Minimum Requirements - Server', () => {
     test('응답의 body 속성으로 id, title, url, author, bodyHTML, avatarUrl가 있어야 합니다.', () => {
       return request(server)
         .get('/discussions/2')
-        .then((res) => {
+        .then(res => {
           const { body } = res;
           expect(body.id).toEqual(2);
           expect(body.title).toEqual('[notice] 질문 템플릿');
@@ -108,9 +108,9 @@ describe('Bare Minimum Requirements - Server', () => {
     test('해당 id를 가진 discussion을 응답합니다.', () => {
       return request(server)
         .get('/discussions/2')
-        .then((res) => {
+        .then(res => {
           const { body } = res;
-          expect(body).toEqual(agoraStatesDiscussions.find((d) => d.id === 2));
+          expect(body).toEqual(agoraStatesDiscussions.find(d => d.id === 2));
           return;
         });
     });
@@ -118,95 +118,93 @@ describe('Bare Minimum Requirements - Server', () => {
 });
 
 // Advanced Challenge 테스트가 필요하면 주석을 해제합니다.
-// describe('Advanced Challenge - Server', () => {
-//   let server;
-//   describe('GET /discussions 요청 - Pagination', () => {
-//     beforeAll(() => {
-//       server = require('../app').server;
-//     });
-
-//     afterAll(() => {
-//       server.close();
-//     });
-
-//     test('쿼리 파라미터 limit 값에 따라 한 번에 볼 수 있는 discussion 개수를 정할 수 있어야 합니다.', () => {
-//       return request(server)
-//         .get('/discussions?limit=5')
-//         .then((res) => {
-//           const { body } = res;
-//           expect(body.length).toEqual(5);
-//           return request(server).get('/discussions?limit=9');
-//         })
-//         .then((res) => {
-//           const { body } = res;
-//           expect(body.length).toEqual(9);
-//           return;
-//         });
-//     });
-//     test('쿼리 파라미터 limit가 없는 요청은 discussions 목록 10개를 응답합니다.', () => {
-//       return request(server)
-//         .get('/discussions?page=1')
-//         .then((res) => {
-//           const { body } = res;
-//           expect(body.length).toEqual(10);
-//           return;
-//         });
-//     });
-//     test('쿼리 파라미터 page가 없는 요청은 쿼리 파라미터 limit값 만큼의 discussion 목록을 응답합니다.', () => {
-//       return request(server)
-//         .get('/discussions?limit=5')
-//         .then((res) => {
-//           const { body } = res;
-//           expect(body.length).toEqual(5);
-//           return request(server).get('/discussions?limit=9');
-//         })
-//         .then((res) => {
-//           const { body } = res;
-//           expect(body.length).toEqual(9);
-//           return;
-//         });
-//     });
-//     test('limit이 3인 경우, 쿼리 파라미터 page 값에 따라 보고 싶은 페이지를 정할 수 있어야 합니다.', () => {
-//       return request(server)
-//         .get('/discussions?limit=3&page=1')
-//         .then((res) => {
-//           const { body } = res;
-//           expect(body.length).toEqual(3);
-//           return request(server).get('/discussions?limit=3&page=3');
-//         })
-//         .then((res) => {
-//           const { body } = res;
-//           expect(body.length).toEqual(3);
-//           return;
-//         });
-//     });
-//     test('잘못된 쿼리 파라미터 값을 요청받은 경우, 상태 코드 400을 응답합니다.', () => {
-//       // https://stackoverflow.com/a/3050561
-//       return request(server)
-//         .get('/discussions?limit=codestates')
-//         .then((res) => {
-//           expect(res.status).toEqual(400);
-//           return;
-//         });
-//     });
-//     test('범위를 벗어난 파라미터 값을 요청받은 경우, 빈 배열을 응답해야 합니다.', () => {
-//       // https://api.gov.au/standards/national_api_standards/pagination.html#query-parameters
-//       return request(server)
-//         .get('/discussions?limit=9999&page=2')
-//         .then((res) => {
-//           expect(res.status).toEqual(200);
-//           expect(res.body).toEqual([]);
-//           return;
-//         });
-//     });
-//     test('범위를 벗어난 파라미터 값을 요청받은 경우, 상태 코드 200을 응답합니다.', () => {
-//       // https://api.gov.au/standards/national_api_standards/pagination.html#query-parameters
-//       return request(server)
-//         .get('/discussions?limit=9999&page=2')
-//         .then((res) => {
-//           expect(res.status).toEqual(200);
-//           return;
-//         });
-//     });
-//   });
-// });
+describe('Advanced Challenge - Server', () => {
+  let server;
+  describe('GET /discussions 요청 - Pagination', () => {
+    beforeAll(() => {
+      server = require('../app').server;
+    });
+    afterAll(() => {
+      server.close();
+    });
+    test('쿼리 파라미터 limit 값에 따라 한 번에 볼 수 있는 discussion 개수를 정할 수 있어야 합니다.', () => {
+      return request(server)
+        .get('/discussions?limit=5')
+        .then(res => {
+          const { body } = res;
+          expect(body.length).toEqual(5);
+          return request(server).get('/discussions?limit=9');
+        })
+        .then(res => {
+          const { body } = res;
+          expect(body.length).toEqual(9);
+          return;
+        });
+    });
+    test('쿼리 파라미터 limit가 없는 요청은 discussions 목록 10개를 응답합니다.', () => {
+      return request(server)
+        .get('/discussions?page=1')
+        .then(res => {
+          const { body } = res;
+          expect(body.length).toEqual(10);
+          return;
+        });
+    });
+    test('쿼리 파라미터 page가 없는 요청은 쿼리 파라미터 limit값 만큼의 discussion 목록을 응답합니다.', () => {
+      return request(server)
+        .get('/discussions?limit=5')
+        .then(res => {
+          const { body } = res;
+          expect(body.length).toEqual(5);
+          return request(server).get('/discussions?limit=9');
+        })
+        .then(res => {
+          const { body } = res;
+          expect(body.length).toEqual(9);
+          return;
+        });
+    });
+    test('limit이 3인 경우, 쿼리 파라미터 page 값에 따라 보고 싶은 페이지를 정할 수 있어야 합니다.', () => {
+      return request(server)
+        .get('/discussions?limit=3&page=1')
+        .then(res => {
+          const { body } = res;
+          expect(body.length).toEqual(3);
+          return request(server).get('/discussions?limit=3&page=3');
+        })
+        .then(res => {
+          const { body } = res;
+          expect(body.length).toEqual(3);
+          return;
+        });
+    });
+    test('잘못된 쿼리 파라미터 값을 요청받은 경우, 상태 코드 400을 응답합니다.', () => {
+      // https://stackoverflow.com/a/3050561
+      return request(server)
+        .get('/discussions?limit=codestates')
+        .then(res => {
+          expect(res.status).toEqual(400);
+          return;
+        });
+    });
+    test('범위를 벗어난 파라미터 값을 요청받은 경우, 빈 배열을 응답해야 합니다.', () => {
+      // https://api.gov.au/standards/national_api_standards/pagination.html#query-parameters
+      return request(server)
+        .get('/discussions?limit=9999&page=2')
+        .then(res => {
+          expect(res.status).toEqual(200);
+          expect(res.body).toEqual([]);
+          return;
+        });
+    });
+    test('범위를 벗어난 파라미터 값을 요청받은 경우, 상태 코드 200을 응답합니다.', () => {
+      // https://api.gov.au/standards/national_api_standards/pagination.html#query-parameters
+      return request(server)
+        .get('/discussions?limit=9999&page=2')
+        .then(res => {
+          expect(res.status).toEqual(200);
+          return;
+        });
+    });
+  });
+});
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,26 +1,70 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
+const divide = (arr, count) => {
+  let result = [];
+  for (let i = 0; i < arr.length; i += count) {
+    const value = arr.slice(i, i + count);
+    result.push(value);
+  }
+  return result;
+};
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+    if (!Object.keys(req.query).length)
+      return res.status(200).json(discussionsData);
+    let { page, limit } = req.query;
+    page = Number(page);
+    limit = Number(limit);
+    if (isNaN(page) && isNaN(limit)) return res.status(400).end();
+    if (limit > discussionsData.length) return res.status(200).json([]);
+    if (page && limit) {
+      const paged = divide(discussionsData, limit)[page - 1];
+      return res.status(200).json(paged);
+    }
+    if (page && !limit) {
+      const paged = divide(discussionsData, 10)[page - 1];
+      return res.status(200).json(paged);
+    }
+    if (!page && limit) {
+      return res.status(200).json(discussionsData.slice(0, limit));
+    }
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    let idArr = [];
+    const getId = discussionsData.filter(el => idArr.push(String(el.id)));
+
+    if (idArr.indexOf(req.params.id) !== -1) {
+      const data = discussionsData.filter(
+        el => String(el.id) === req.params.id
+      );
+      return res.json(...data);
+    } else {
+      res.status(404).send('해당 id의 discussion이 존재하지 않습니다.');
+    }
   },
 
   createOne: (req, res) => {
     // ADVANCED: 새로운 discussion을 생성합니다.
+    const newDiscussionsData = discussionsData.unshift(req.body);
+    return res.status(201).json(newDiscussionsData);
   },
 
   updateById: (req, res) => {
     // ADVANCED: path parameter id를 가진 discussion을 요청에 맞게 변경합니다.
+    const data = discussionsData.filter(el => String(el.id) === req.params.id);
+    const updatedData = Object.assign(...data, req.body);
+    return res.status(200).json(updatedData);
   },
 
   deleteById: (req, res) => {
     // ADVANCED: path parameter id를 가진 discussion을 삭제합니다.
+    const data = discussionsData.filter(el => String(el.id) !== req.params.id);
+    return res.status(200).json(data);
   },
 };
 
