f0d9397c42faf97f22a71cc6e7bfa09606c5778b
guswns3198 <guswns3198@naver.com>
2022-06-21 15:19:15 +0900

    이거 안되면 안한다 이제

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -2,14 +2,39 @@ const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
+  
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+    res.status(200).json(discussionsData)
   },
+
+  // let sameId = discussionsData.find((item) => item.id === Number(req.params.id))
+  //   if (sameId) {
+  //     return res.status(200).json(sameId);
+  //   } else {
+  //     return res.status(404).json();
+  //   }
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
-  },
+    if(req.params) {
+      let sameId = discussionsData.filter ((item) => {
+        return Number(req.params.id) === item.id;
+      });
+
+    if (sameId.length === 0){
+      return res.status(404).end();
+    }
+
+    if (sameId) {
+      return res.status(200).json(sameId[0]);
+    }
+    else {
+      return res.status(404).end();
+    }
+  }
+},
+  
+
 
   createOne: (req, res) => {
     // ADVANCED: 새로운 discussion을 생성합니다.
