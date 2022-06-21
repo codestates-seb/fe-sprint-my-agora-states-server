40091bbbc5e04ee5abb9e4064103c819c5cf1012
97tlsdk <61078612+nomga@users.noreply.github.com>
2022-06-21 16:54:03 +0900

    FE_39_엄신영_fe-sprint-my-agora-states-server (1)
    - index.js 만지는 중
    - 테스트 통과 안됨 ㅜㅜ
    - 알듯 말듯 모르겠고...

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,14 +1,25 @@
+const { query } = require("express");
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    agoraStatesDiscussions.push(req.body);
+
+    return res.status(200).json({});
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    let data = discussionsData;
+    if(req.params.agoraStatesDiscussions.id) {
+      data = data.filter((item)=> {
+        return req.params.id === item.id;
+      });
+    }
+    return res.status(200).json(data);
   },
 
   createOne: (req, res) => {
