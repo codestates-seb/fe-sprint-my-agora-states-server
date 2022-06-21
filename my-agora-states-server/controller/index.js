579a7b279b2755fbb97380ff3a8d50e297ee3716
yoni <sdy32@naver.com>
2022-06-21 14:44:36 +0900

    agora states server bare minimum 구현

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,10 +5,21 @@ const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    if(req.params.id !== undefined) {
+      const list = discussionsData.filter((item) => {
+        return Number(req.params.id) === item.id
+      })
+      if (list.length !== 0) {
+        return res.status(200).json(...list)
+      } else {
+        return res.status(404).send('Not Found!')
+      }
+    }
   },
 
   createOne: (req, res) => {
