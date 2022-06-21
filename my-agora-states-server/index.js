fecdbb232b9678d88fd9ba8dbd0e2c2ea79727af
dulce04 <56382387+dulce04@users.noreply.github.com>
2022-06-21 16:49:59 +0900

    SEB_FE_39_윤다현 과제 제출

---
+++ my-agora-states-server/index.js
@@ -0,0 +1,38 @@
+const { agoraStatesDiscussions } = require("../repository/discussions");
+const discussionsData = agoraStatesDiscussions;
+
+const discussionsController = {
+  findAll: (req, res) => {
+    // TODO: 모든 discussions 목록을 응답합니다.
+    return res.status(200).json(discussionsData);
+    // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+  },
+
+  findById: (req, res) => {
+    // TODO: path parameter id를 가진 discussion을 응답합니다.
+    const data = discussionsData.filter((el) => {
+      return  el.id === Number(req.params.id);
+    });
+
+    if (data.length !== 0) {
+      return res.status(200).json(...data);
+    } 
+    return res.status(404).json('Not Found');
+  },
+
+  createOne: (req, res) => {
+    // ADVANCED: 새로운 discussion을 생성합니다.
+  },
+
+  updateById: (req, res) => {
+    // ADVANCED: path parameter id를 가진 discussion을 요청에 맞게 변경합니다.
+  },
+
+  deleteById: (req, res) => {
+    // ADVANCED: path parameter id를 가진 discussion을 삭제합니다.
+  },
+};
+
+module.exports = {
+  discussionsController,
+};
