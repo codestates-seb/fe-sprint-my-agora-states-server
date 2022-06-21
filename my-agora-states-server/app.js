7883e20d76090ba5f34e7099881b4f090dad1639
cjsgmail <cjs0097@gmail.com>
2022-06-21 14:39:03 +0900

    과제 완료

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -5,19 +5,32 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 // TODO: cors를 적용합니다.
-
+app.use(cors())
+app.use(express.json())
+app.use(morgan())
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
 // OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
 
+
 const port = 3001;
 const discussionsRouter = require('./router/discussions');
+const router = require('./router/discussions');
+// const { route } = require('./router/discussions');
 
-// TODO: /discussions 경로로 라우팅합니다. 
+// TODO: /discussions 경로로 라우팅합니다.
+app.use('/discussions', discussionsRouter)
+
+
+router.get('/discussions', (req, res)=>{
+  res.status(200).send('hi');
+})
 
 app.get('/', (req, res) => {
   // TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
+  res.status(200).send('hi agora');
   throw '';
 });
+
 
 const server = app.listen(port, () => {
   console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
