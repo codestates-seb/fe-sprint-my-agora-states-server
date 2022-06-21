706efa0156d0b4ec1f07c0529aa7b27ac4982f11
YHJ96 <yhj878812@naver.com>
2022-06-21 11:52:47 +0900

    feat: discussions 라우터 기능 생성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,11 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    return res.status(200).json(discussionsData);
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    const id = Number(req.params.id);
+    let index = 0;
+    for(let data of discussionsData) {
+      if (data.id === id) {
+        index = id
+        break;
+      };
+    }
+    if (index === 0) return res.status(404).end();
+    const [ temp ] = discussionsData.filter((item) => item.id === id);
+    return res.status(200).json(temp);
   },
 
   createOne: (req, res) => {
