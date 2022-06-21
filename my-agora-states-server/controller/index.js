799e07929c9569246e7277e8b4201aadee4846a3
ddaisylee <elliejay9988@gmail.com>
2022-06-21 14:29:45 +0900

    주석 삭제

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,15 +5,11 @@ const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
-    // let list = discussionsData;
     return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
-    // 오브젝트의 id 라는 key를 반복해서 req.params.id와 동일한 데이터가 있으면
-    // 해당 값을 리턴한다.
-    // 없으면 404를 리턴한다.
     let list = discussionsData.filter((item)=> item.id === Number(req.params.id))
     return list.length ? res.status(200).json(...list) : res.status(404).end();
   },
