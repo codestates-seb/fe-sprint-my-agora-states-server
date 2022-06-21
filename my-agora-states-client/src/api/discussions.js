b5728e2562dbb65e1bb4907ca1ccce46929310f5
pongdang <pongdang0121@gmail.com>
2022-06-21 15:45:08 +0900

    chore: 필요없는 함수 제거

--- my-agora-states-client/src/api/discussions.js
+++ my-agora-states-client/src/api/discussions.js
@@ -1,10 +1,6 @@
 export async function findAll(page = 1) {
   const response = await fetch(`http://localhost:3002/discussions?page=${page}`);
-  return await response.json();
-}
 
-export async function getDiscussion(id) {
-  const response = await fetch(`http://localhost:3002/discussions/${id}`);
   return await response.json();
 }
 
