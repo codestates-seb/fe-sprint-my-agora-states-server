260c3ba18a631958dcd15564a24babaf5a87044c
“황보영” <hby2233@naver.com>
2022-06-21 15:11:13 +0900

    라우팅

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,11 +4,17 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    return res.status(200).json(discussionsData);
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    if (req.params.id) {
+      const result = discussionsData.filter((el) => req.params.id === String(el.id));
+      if (result.length === 0) res.status(404).send('Not Found!').json(discussionsData);
+      if (result.length > 0) res.status(200).json(...result);
+    }
   },
 
   createOne: (req, res) => {
