122ddf5ae7f6eea2e6b639501b27bd8565c34533
bizbaeja <75407823+bizbaeja@users.noreply.github.com>
2022-06-21 22:21:53 +0900

    Update index.js

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,23 +4,37 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    return res.status(200).json(discussionsData)
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
   },
 
   findById: (req, res) => {
-    // TODO: path parameter id를 가진 discussion을 응답합니다.
+    let id = req.param('id');
+    var discussionData = discussionsData.find(x => x.id == id);//filter 시도해보고 선민님께 보고. ();
+    if(discussionData == undefined)
+    {
+      console.log("is Undefined");
+      throw res.status(404);
+    }
+    else
+    {
+      return res.status(200).json(discussionData);
+    }    
   },
-
+  
   createOne: (req, res) => {
     // ADVANCED: 새로운 discussion을 생성합니다.
+    return res.status(200).json(discussionsData)
   },
 
   updateById: (req, res) => {
     // ADVANCED: path parameter id를 가진 discussion을 요청에 맞게 변경합니다.
+    return res.status(200).json(discussionsData)
   },
 
   deleteById: (req, res) => {
     // ADVANCED: path parameter id를 가진 discussion을 삭제합니다.
+    return res.status(200).json(discussionsData)
   },
 };
 
