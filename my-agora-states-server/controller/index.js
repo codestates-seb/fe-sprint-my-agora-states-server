3db3ca2bbf34752302c02a36257d62f342b7f970
Hong-sk <sooyun9600@naver.com>
2022-06-21 15:34:22 +0900

    bare-minimum clear

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,23 +5,34 @@ const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
     // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: path parameter id를 가진 discussion을 응답합니다.
+    const { id } = req.params;
+    console.log(req.params);
+    const data = discussionsData.filter(
+      (discussion) => discussion.id === Number(id)
+    );
+    if (data.length !== 0) {
+      return res.status(200).json(data[0]);
+    } else {
+      return res.status(404).send("no data ");
+    }
   },
 
-  createOne: (req, res) => {
-    // ADVANCED: 새로운 discussion을 생성합니다.
-  },
+  // createOne: (req, res) => {
+  //   // ADVANCED: 새로운 discussion을 생성합니다.
+  // },
 
-  updateById: (req, res) => {
-    // ADVANCED: path parameter id를 가진 discussion을 요청에 맞게 변경합니다.
-  },
+  // updateById: (req, res) => {
+  //   // ADVANCED: path parameter id를 가진 discussion을 요청에 맞게 변경합니다.
+  // },
 
-  deleteById: (req, res) => {
-    // ADVANCED: path parameter id를 가진 discussion을 삭제합니다.
-  },
+  // deleteById: (req, res) => {
+  //   // ADVANCED: path parameter id를 가진 discussion을 삭제합니다.
+  // },
 };
 
 module.exports = {
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,17 +1,25 @@
 // TODO: discussions 라우터를 완성합니다.
-const { discussionsController } = require('../controller');
-const { findAll, findById, createOne, updateById, deleteById } = discussionsController;
-const express = require('express');
+const { discussionsController } = require("../controller");
+const { findAll, findById, createOne, updateById, deleteById } =
+  discussionsController;
+const express = require("express");
 const router = express.Router();
 
 // TODO: 모든 discussion 목록을 조회하는 라우터를 작성합니다.
+router.get("/", findAll);
+// http:localhost:3001/
 
 // TODO: discussion 하나를 조회하는 라우터를 작성합니다.
+router.get("/:id", findById);
+// http::localhost:3001/42
 
 // ADVANCED: discussion 하나를 생성하는 라우터를 작성합니다.
+// router.get("/:id", createOne);
 
 // ADVANCED: discussion 하나를 수정하는 라우터를 작성합니다.
+// router.get("/:id", updateById);
 
 // ADVANCED: discussion 하나를 삭제하는 라우터를 작성합니다.
+// router.get("/:id", deleteById);
 
 module.exports = router;
