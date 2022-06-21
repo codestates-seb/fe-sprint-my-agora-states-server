897b5e2f97a21bc8601ef74346968ca6e7691ef7
pongdang <pongdang0121@gmail.com>
2022-06-21 12:07:00 +0900

    feat: api 받아오기

---
+++ my-agora-states-client/src/api/discussions.js
@@ -0,0 +1,20 @@
+export async function getDiscussions() {
+  const response = await fetch("http://localhost:3002/discussions");
+  return await response.json();
+}
+
+export async function getDiscussion(id) {
+  const response = await fetch(`http://localhost:3002/discussions/${id}`);
+  return await response.json();
+}
+
+export async function postDiscussion(body) {
+  const response = await fetch(`http://localhost:3002/discussions`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  return await response.json();
+}
