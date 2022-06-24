63f37ba96e629b14fa97922991f560b011028a1e
ksheer506 <ksheer506@naver.com>
2022-06-24 18:51:11 +0900

    CONNECTION REFUSE 에러: fetch 요청 App 컴포넌트로 이동

--- my-agora-states-client/src/component/InputArea.js
+++ my-agora-states-client/src/component/InputArea.js
@@ -1,30 +1,64 @@
+import { useEffect, useState } from 'react';
 import Button from './Button';
 import './InputArea.css'
 
 const childInputs = [
-  { title: "Enter your name", id: "name", placeholder: "유저명을 입력하세요.", key: 1 },
+  { title: "Enter your name", id: "author", placeholder: "유저명을 입력하세요.", key: 1 },
   { title: "Enter your title", id: "title", placeholder: "제목을 입력하세요.", key: 2 },
-  { title: "Your question", id: "story", placeholder: "질문을 입력하세요.", type: "textarea", key: 3 }
+  { title: "Your question", id: "bodyHTML", placeholder: "질문을 입력하세요.", type: "textarea", key: 3 }
 ]
 
-const InputArea = ({ title, type, id, placeholder }) => {
+const InputArea = ({ title, type, id, placeholder, value, onChange }) => {
 
   return (
     <>
       <label htmlFor={id}>{title}</label>
       {type === "textarea"
-        ? <textarea type={type} name="discussion" id={id} placeholder={placeholder} required />
-        : <input type={type} name="discussion" id={id} placeholder={placeholder} required />}
+        ? <textarea type={type} name="discussion" id={id} value={value} placeholder={placeholder} onChange={onChange} required />
+        : <input type={type} name="discussion" id={id} value={value} placeholder={placeholder} onChange={onChange} required />}
     </>
   );
 }
 
-const Form = () => {
+const Form = ({ newItem, setNewItem, setReRender }) => {
+  /* const [newItem, setNewItem] = useState({ name: "", title: "", bodyHTML: "" }); */
+  const [fetching, setFetching] = useState(false);
+
+  const onInputChange = (e, prop) => {
+    setNewItem(prev => ({ ...prev, [prop]: e.target.value }));
+  }
+  const submitDiscussion = (e) => {
+    e.preventDefault();  // FIXME: 꼭 써야 하나?
+    setReRender("newItem");
+  }
+
+/*   useEffect(() => {
+    if (!fetching) return;
+
+    const option = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(newItem)
+    }
+    console.log(newItem);
+    fetch("http://localhost:3003/discussions", option)
+      .then((res) => res.json())
+      .then(({ newDiscussionID }) => {
+        if (newDiscussionID) {
+          setNewItem({ name: "", title: "", bodyHTML: "" });
+          setFetching(false);
+          onSubmitItem(true);
+        }
+      })
+  }, [fetching]); */
 
   return (
-    <form action="" method="post" className="form" /* onSubmit={} */>
+    <form className="form" onSubmit={submitDiscussion}>
       <div className="form__input--wrapper">
-        {childInputs.map((opt) => <InputArea {...opt} />)}
+        {childInputs.map((opt) => <InputArea {...opt} value={newItem[opt.id]} onChange={(e) => onInputChange(e, opt.id)} />)}
         <Button type="submit" title="submit" />
       </div>
     </form>
