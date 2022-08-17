0f64a15096e3c4908c51dabc0c530d8d477ffd30
Mina <00minha@gmail.com>
2022-08-17 14:24:41 +0900

    bareMinimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,16 +1,16 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
-
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { uuid } = req.params;
+    let list = discussionsData.filter(el => Number(el.id) === Number(uuid));
+    (list.length !== 0) ? res.status(200).send(...list) : res.status(404).json('not-found');
   }
 
 };
