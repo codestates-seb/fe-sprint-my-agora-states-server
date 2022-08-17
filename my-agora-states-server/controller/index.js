6ce8f0e417ce8b2f011a7230348a4e4cb1997998
tmdn01cs <tmdn01cs@gmail.com>
2022-08-17 13:07:49 +0900

    agora states server

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    if (id) {
+      const filteredDiscussionsData = discussionsData.filter((data) => data.id === Number(id))
+      if (filteredDiscussionsData.length !== 0) {
+        return res.status(200).send(filteredDiscussionsData[0])
+      } else {
+        return res.status(404).send('Not Found')
+      }
+    }
   }
-
 };
 
 module.exports = {
