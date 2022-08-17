0fb541ae6e22ef3dd2be061a82302c7bc582327b
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-17 11:04:33 +0900

    Bare Minimum 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,29 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    //console.log(req.params);
+    const { id } = req.params;
+    let hasId = false;
+    discussionsData.forEach((discussion) => {
+      if (discussion.id === Number(id)) hasId = true;
+    });
+    if (hasId) {
+      res
+        .status(200)
+        .json(
+          ...discussionsData.filter(
+            (discussion) => discussion.id === Number(id)
+          )
+        );
+    } else {
+      res.status(404).send("ID doesn't exist.");
+    }
+  },
 };
 
 module.exports = {
