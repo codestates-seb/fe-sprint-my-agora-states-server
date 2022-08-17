7e4f47444c1a28bd56cd8dc0f98476f223c6c4c2
Wusi-Jeong <jws9595@naver.com>
2022-08-17 20:09:44 +0900

    COMMIT

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const {id} = req.params;
+    // console.log(id);
+    // console.log(typeof id);
+    const found = discussionsData.find(data => data.id === Number(id));
+    // console.log(found);
+
+    if(found) {
+      res.status(200).send(found);
+    } else {
+      res.status(404).send('Not Found');
+    }
     
-    res.send('TODO:')
   }
 
 };
