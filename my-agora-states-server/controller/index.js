955df3142c734d2b03e5f4c508e541f2d48fc4a0
jihwanAn <97936397+jihwanAn@users.noreply.github.com>
2022-08-17 21:15:51 +0900

    SEB_FE_40 안지환 과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    const found = discussionsData.find(data => data.id === Number(id));
+    if(found) {
+      res.status(200).send(found);
+    } else {
+      res.status(404).send('Not Found');
+    }
   }
 
 };
