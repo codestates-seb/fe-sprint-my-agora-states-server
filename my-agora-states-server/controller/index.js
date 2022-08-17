20b8e216740093192313508dc75dbbc1df85f29d
JinhuiKim <public.jinhui.kim@gmail.com>
2022-08-17 12:12:03 +0900

    Feat: my-agora-states api 추가

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    const filteredDiscussion = discussionsData.filter(
+      (discussion) => discussion.id === Number(id)
+    );
+
+    if (filteredDiscussion.length > 0) res.status(200);
+    else res.status(404);
+
+    return res.json(filteredDiscussion[0]);
+  },
 };
 
 module.exports = {
