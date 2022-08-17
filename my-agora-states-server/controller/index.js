542a95f196e303fb00709a7cb61badc84ce6b93d
leejehoon92 <2eejehoon@gmail.com>
2022-08-17 10:51:17 +0900

    Bare Minimum Requirement 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    if (id !== undefined) {
+      const discussion = discussionsData.find((discussion) => {
+        return discussion.id === Number(id);
+      })
+      if (discussion !== undefined) {
+        return res.status(200).send(discussion);
+      } else {
+        return res.status(404).send("데이터가 존재하지 않습니다")
+      }
+    }
   }
 
 };
