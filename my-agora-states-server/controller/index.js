d41232e9a71bec324b6a7965326ec2d3e5175b7f
daldalchoi <vocolate1990@gmail.com>
2022-08-17 13:47:54 +0900

    first

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,17 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const data = discussionsData.filter((discussion) => discussion.id === id);
+    return data !== -1
+      ? res.status(200).json(data)
+      : res.status(400).send("please type correctly");
+  },
 };
 
 module.exports = {
