5a039d9f05edc72067b70b8a24ff836c8651280c
kimyejin <everthanbefore@naver.com>
2022-08-17 15:25:05 +0900

    과제 다해따

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const app = express();
-
+const jsonParser = express.json({strict: false});
 const cors = require('cors');
 const morgan = require('morgan');
+
+// TODO: cors를 적용합니다.
+app.use(cors());
+
+// TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+app.use(jsonParser);
 
 // morgan 미들웨어가 세팅되어 있습니다.
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
 app.use(morgan('tiny'));
 
-// TODO: cors를 적용합니다.
-
-// TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-
-
-const port = 4000;
+const port = 3001;
 const discussionsRouter = require('./router/discussions');
 
 // TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
-
+app.use('/discussions', discussionsRouter);
 
 app.get('/', (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
   res.status(200).send('fe-sprint-my-agora-states-server');
 });
+
 
 const server = app.listen(port, () => {
   console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
@@ -30,3 +32,10 @@ const server = app.listen(port, () => {
 
 module.exports.app = app;
 module.exports.server = server;
+
+
+// my-agora-states-server/app.js
+// 모든 Origin, 경로에 대해 CORS 요청을 허용하게 미들웨어를 적용합니다.
+// POST 요청 등에 포함된 body(payload)를 구조화하기 위한 미들웨어를 적용합니다.
+// 서버 상태 확인을 위해 / 에서 상태 코드 200으로 응답합니다.
+// discussionsRouter 를 이용하여 /discussions 경로로 라우팅합니다.
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,17 +4,29 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    const dataOfTheId = discussionsData.find(data => data.id === Number(id));
+    // 왜 filter는 안되었는가...?
+    if (dataOfTheId) {
+      return res.status(200).json(dataOfTheId)
+    } else {
+      return res.status(404).json('404 Not Found');
+    }
   }
-
+  // return 안 붙여주면 Cannot set headers after they are sent to the client 에러 발생 8ㅅ8
 };
+
 
 module.exports = {
   discussionsController,
 };
+
+
+// my-agora-states-server/controller/index.js
+// discussionsController.findAll
+// discussionsController.findById
