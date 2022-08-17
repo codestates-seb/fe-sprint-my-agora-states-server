cd92fa76a5292a7e33410c5872d1acf41ec5c5c1
tlsrb100 <104765779+tlsrb100@users.noreply.github.com>
2022-08-17 11:06:07 +0900

    Bare Minimum Requirement 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params;
+    const filteredById = discussionsData.filter((discussion)=>{
+      return (discussion.id === Number(id));
+    });
+    console.log(filteredById);
+    if(filteredById.length){
+      res.status(200).send(filteredById[0]);
+    }else{
+      res.status(404).send('데이터가 존재하지 않습니다');
+    }
   }
 
 };
