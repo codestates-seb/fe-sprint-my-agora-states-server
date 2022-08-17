9e5fc62ea96d7c8c43e0f2afa43e8fafc8a705f3
Jongwon <jwp7073@gmail.com>
2022-08-17 11:00:59 +0900

    Bare Minimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const [filteredDiscussion] = discussionsData.filter((discussion) => {
+      return req.params.id === String(discussion.id);
+    });
 
+    if (filteredDiscussion !== undefined) {
+      return res.status(200).json(filteredDiscussion);
+    } else {
+      return res
+        .status(404)
+        .send("id와 일치하는 discussion이 존재하지 않습니다.");
+    }
+  },
 };
 
 module.exports = {
