105c6d3e8dd85a4bec5ec1b571722323941dad42
starplanter93 <starplanter93@gmail.com>
2022-08-17 11:49:11 +0900

    my agora states server index.js까지 테스트 통과

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+
+    const idx = discussionsData.findIndex((el) => {
+      return Number(el.id) === Number(id)
+    });
+
+    if(idx === -1){
+    res.status(404).send('Not Found');
+    }else{
+    const filteredById = discussionsData[idx];
+    res.status(200).send(filteredById);
+    }
   }
 
 };
