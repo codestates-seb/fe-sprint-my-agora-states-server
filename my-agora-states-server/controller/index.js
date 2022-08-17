65f5c5a61090e1e38ca46f42d9e7d541551b2899
anjisu <guajs0818@gmail.com>
2022-08-17 15:25:02 +0900

    테스트 통과

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,17 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const id = req.params.id;
+    const data = discussionsData.filter(el => el.id === Number(id));
+    if(data.length !== 0){
+      res.status(200).json(data[0]);
+    } else {
+      res.status(404).end();
+    }
+  },
 };
-
 module.exports = {
   discussionsController,
 };
