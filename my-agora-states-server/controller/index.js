53028bc0ddf363dc08e0a74d0631d0753664a51b
Helperbot5 <eswirdtheo@gmail.com>
2022-08-17 16:25:44 +0900

    서버 라우터 연결

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    const discussion = discussionsData.filter(data => data.id === +id)[0];
+    // id 앞에 +를 붙여서 number로 형변환
+    if(!discussion) {
+      res.status(404).send('Not Found')
+    }
+  res.status(200).json(discussion);
   }
 
 };
