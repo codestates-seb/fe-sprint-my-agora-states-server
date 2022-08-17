d0b4e2b1b9980bc6553bfca7d9ef591f170e310f
junny1995 <while0818@gmail.com>
2022-08-17 14:58:09 +0900

    Bara Minimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,25 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    let data = discussionsData
+    return res.status(200).json(data);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    if(req.params.id) {
+      let data = {}
+      discussionsData.forEach(el => {
+        if(Number(el.id) === Number(req.params.id)) {
+           data = el
+           return
+        }
+      })
+      if(Object.keys(data).length === 0) {
+        return res.status(404).json(data);
+      }
+      return res.status(200).json(data);
+    }
   }
 
 };
