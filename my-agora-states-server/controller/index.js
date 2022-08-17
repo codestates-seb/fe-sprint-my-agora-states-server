13511e0a4703719d4fc73cf2bc6ba43906f51a3d
Ryu heekyung <beals1129@gmail.com>
2022-08-17 17:32:18 +0900

    문제풀이 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,16 +1,22 @@
+const e = require("express");
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
+
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    let list = discussionsData;
+
+    return res.status(200).json(list)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params;
+
+    const data = discussionsData.filter((el) => el.id === parseInt(id));
+    return data.length ? res.status(200).json(...data) : res.status(404).send();
   }
 
 };
