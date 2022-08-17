a70b4de00163a0978458591fc9efc72081a216ed
junghyunni <rlawjdgus7@gmail.com>
2022-08-17 13:46:06 +0900

    나만의 아고라스테이츠 서버  만들기

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params;
+    if (id) {
+      const data = discussionsData.filter((item) => {
+        return Number(id) === item.id;
+      })
+      if (data.length === 0) {
+        return res.status(404).json("Not Found!");
+      }
+      return res.status(200).send(...data);
+    }
   }
-
 };
 
 module.exports = {
