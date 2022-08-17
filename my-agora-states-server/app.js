eaf0ab8c6b23d8cd1d1ced28d386827052e0f475
Liieonking <100991074+Liieonking@users.noreply.github.com>
2022-08-17 18:09:41 +0900

    Add files via upload

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -4,9 +4,12 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 
+const jsonParser = express.json({strict:false});
+
 // morgan 미들웨어가 세팅되어 있습니다.
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
-app.use(morgan('tiny'));
+app.use(morgan('tiny'), cors(), jsonParser);
+
 
 // TODO: cors를 적용합니다.
 
@@ -17,7 +20,7 @@ const port = 4000;
 const discussionsRouter = require('./router/discussions');
 
 // TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
-
+app.use('/discussions', discussionsRouter)
 
 app.get('/', (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const {id} = req.params;
+    const filterData = discussionsData.filter(el => {
+      return el.id === Number(req.params.id)
+    })
+    if(filterData.length === 0){
+      return res.status(404).send('Not found id')
+    }
     
-    res.send('TODO:')
+    return res.status(200).json(filterData[0])
   }
 
 };
