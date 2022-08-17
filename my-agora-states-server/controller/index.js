a8ac1fbe6bf3d94271f99ad6f3b5edc76705a807
kjs0508 <kss0866@gmail.com>
2022-08-17 15:11:21 +0900

    SEB_FE_40 김지수 과제 제출합니다.

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,16 +4,27 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+
+      return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
+    const {id}=req.params
+    let filtering = discussionsData.filter((item) => {
+      return Number(id)===Number(item.id)
+    });
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    if(filtering.length===0){  
+
+      return res.status(404).send('error'); 
+    }
     
-    res.send('TODO:')
+   return res.json(filtering[0]);
+    
+  }
   }
 
-};
+
 
 module.exports = {
   discussionsController,
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -3,11 +3,11 @@ const { discussionsController } = require('../controller');
 const { findAll, findById } = discussionsController;
 const express = require('express');
 const router = express.Router();
-
+const discussions=require('../repository/discussions')
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
-
+router.get('/',findAll)
+  
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-
+router.get('/:id',findById)
 
 module.exports = router;
