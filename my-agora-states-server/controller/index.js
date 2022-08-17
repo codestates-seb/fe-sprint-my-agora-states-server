79eb7d7dce4413b6a5f051a27e20cf4df474bcfd
ethanJcoding <ethan.95j@gmail.com>
2022-08-17 14:27:21 +0900

    아고라 서버 과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,32 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData)
+
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const {id} = req.params
+
+    let idx = []
+    let list = []
     
-    res.send('TODO:')
+
+    discussionsData.filter((el) => {
+      return idx.push(el.id)
+    })
+    
+    if(idx.includes(Number(id))){
+      discussionsData.filter((discussion) => {
+        if(discussion.id === Number(id)){
+          list.push(discussion)
+          res.status(200).json(list[0])
+        } 
+      })
+    } else {
+      res.status(404).send('WRONG ID')
+    }
   }
 
 };
