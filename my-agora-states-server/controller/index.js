d9c284fa12b5a0daddad62ec9c7258abb37cc76b
ichenny <ichenny.lee@gmail.com>
2022-08-17 14:42:09 +0900

    bareminimum 풀이완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    // id와 일치하는 데이터가 존재하는 경우
+    // id와 일치하는 데이터가 존재하지 않는 경우
+    let filtered = discussionsData.filter(
+      (discussion) => Number(id) === discussion.id
+    )[0];
+
+    if (filtered) return res.status(200).json(filtered);
+    else return res.status(404).json("id에 해당하는 데이터가 없습니다");
+  },
 };
 
 module.exports = {
