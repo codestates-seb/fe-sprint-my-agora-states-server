c5557923093c73e20040a2cdcf6b05bcb2bd20c6
yueunbi <rachelyu.1025@gmail.com>
2022-08-17 16:17:43 +0900

    과제제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,16 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    // req로 받아오는 id가 json 형태 즉,문자열이기 때문에 Number형변환 필요!! 
+    let filtered = discussionsData.filter(el => el.id === Number(id)); 
 
-};
+    if(filtered.length !== 0){
+      //목록이 아닌 '값'을 전달 => id값이 같은 요소만 필터링된 배열 내 한요소만 리턴하도록 인덱스로 접근 
+      return res.status(200).json(filtered[0]);
+    }else{
+      return res.status(404).json('Not Found!');
+  }
+  }}
 
 module.exports = {
   discussionsController,
