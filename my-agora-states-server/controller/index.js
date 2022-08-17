94a3b546c9642a149ad0a133417ee9b1cecacca1
YeryunJung <2bites@naver.com>
2022-08-17 15:24:54 +0900

    submit assignments

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    let filteredData = discussionsData.filter(
+      (data) => Number(req.params.id) === data.id
+    );
+    return filteredData.length
+      ? res.status(200).json(filteredData[0])
+      : res.status(404).send("error");
+  },
 };
 
 module.exports = {
