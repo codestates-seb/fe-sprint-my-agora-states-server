84acf6bdd2e57db32c3ada4a6f081182365ee0a2
nahoyeon <nahy0107@naver.com>
2022-08-17 14:20:52 +0900

    Bare Minimum 과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.send(discussionsData)
+    
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params
+    const filteredData = discussionsData.filter(el => {
+      return el.id === Number(id);
+    })[0]
 
+    if(!filteredData){
+      res.status(404).json(filteredData);
+    }
+    res.status(200).json(filteredData);
+  }
 };
 
 module.exports = {
