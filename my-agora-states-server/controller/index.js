fd811928dfe13fd1c0f8b14041ef855fcf49156e
BemonJoon <ehdfkd2000@naver.com>
2022-08-17 11:55:35 +0900

    test

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,16 +3,26 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // TODO: 모든 discussions 목록을 응답합
+
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    const filterId = discussionsData.filter((discussions) => {
+      if (Number(discussions.id) === Number(id)) {
+        return true;
+      }
+    });
+    if (filterId.length !== 0) {
+      res.status(200).json(filterId[0]);
+    } else {
+      res.status(404).send("일치하는 ID가 없습니다");
+    }
+  },
 };
 
 module.exports = {
