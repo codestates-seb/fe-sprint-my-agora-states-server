a386393a79a7af7870b82d3a08aa09887ee7e0cc
geeneeus <4demo0002@gmail.com>
2022-08-17 16:03:53 +0900

    신현정 1차과제

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,15 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    let data = discussionsData;
+    return res.status(200).json(data);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     
-    res.send('TODO:')
+    const data = discussionsData.filter(ele => Number(req.params.id) === ele.id);
+    return data.length !== 0 ? res.status(200).json(...data) : res.status(404).json('err');
   }
 
 };
