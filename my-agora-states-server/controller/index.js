3d90aad5e1755dd0ac579e929d61c69639608a74
cule97 <wltjd326@naver.com>
2022-08-17 13:09:14 +0900

    아고라스테이츠 서버구현

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    let data= discussionsData;
+    if(req.query !==undefined){
+      return res.status(200).json(data);
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    let data=discussionsData.filter((el)=>{ return el.id===Number(req.params.id)})
+    if(data.length>0){
+      return res.status(200).json(...data)
+    }else{
+      return res.status(404).send('에러')
+    }
   }
 
 };
