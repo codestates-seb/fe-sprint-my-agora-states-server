46927b97bc531e07e7877b479d75be4f204f6b1b
Hyejung Park <hyejj19@naver.com>
2022-08-17 11:01:12 +0900

    Bare Minimum 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,21 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const {agoraStatesDiscussions} = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
+  // [GET] /
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
+  // [GET] /:id
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const {id} = req.params;
+    const discussion = discussionsData.find(data => data.id == id);
+    if (!discussion) res.status(404).send('일치하는 결과가 없습니다.');
+    res.status(200).json(discussion);
+  },
 };
 
 module.exports = {
