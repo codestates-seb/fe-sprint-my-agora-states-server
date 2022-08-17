9d0bc4822d65c6ad66851433862e5c47bed82a1c
MINDA01 <ekdud4639@gmail.com>
2022-08-17 11:40:18 +0900

    feat: 문제풀기

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,16 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const filteredId = discussionsData.filter((data) => data.id === Number(id));
+    if (filteredId.length === 0) return res.status(404).send("Not Found!");
+    return res.json(...filteredId);
+  },
 };
 
 module.exports = {
