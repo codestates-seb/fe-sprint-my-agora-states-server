3b8930d6927616d0bed5d18d2e4ec62018c57bdb
kosmos992 <yechan5870@gmail.com>
2022-08-17 14:56:57 +0900

    fe-sprint-my-agora-states-server

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,20 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const [found] = discussionsData.filter(each => each['id'] === Number(id));
+    found === undefined
+      ? res.status(404).send('Data not found!')
+      : res.status(200).json(found);
+  },
 };
 
 module.exports = {
