db8ea480c8701ceb76d4d76b494f63df0811c195
Eugenius1st <eugenius1st@gmail.com>
2022-08-18 00:22:22 +0900

    [#complete]: 과제제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,16 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
+    // res.send('TODO:')
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    const data = discussionsData.filter((el) => el.id === parseInt(id));
+    return data.length ? res.status(200).json(...data) : res.status(404).send();
+    // res.send('TODO:')
   }
 
 };
