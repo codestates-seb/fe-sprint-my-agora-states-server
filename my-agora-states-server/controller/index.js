caf595d0a39c8fa3af685a4c119bd4fcda7b842a
callmejeje <choije401@naver.com>
2022-08-17 11:40:10 +0900

    SEB_FE_40_최주은 과제 제출합니다

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,34 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData);
+    // console.log(discussionsData[0]);
+    // return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    // let filtered = discussionsData.filter(
+    //   (el) => el.id === Number(req.params.id)
+    // );
+    let found = discussionsData.find((el) => el.id === Number(req.params.id));
 
+    if (found) {
+      return res.status(200).send(
+        // filtered.map((el) => {
+        //   return {
+        //     id: el.id,
+        //     title: el.title,
+        //     url: el.url,
+        //     author: el.author,
+        //     bodyHTML: el.bodyHTML,
+        //     avatarUrl: el.avatarUrl,
+        //   };
+        // })
+        found
+      );
+    } else return res.status(404).send("Not Found!");
+  },
 };
 
 module.exports = {
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,13 +1,17 @@
 // TODO: discussions 라우터를 완성합니다.
-const { discussionsController } = require('../controller');
+const { discussionsController } = require("../controller");
 const { findAll, findById } = discussionsController;
-const express = require('express');
+const express = require("express");
+const { app } = require("../app");
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+// /면 findAll
+router.get("/", findAll);
+// app.use("/", findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-
+// /:id면 findById
+router.get("/:id", findById);
 
 module.exports = router;
