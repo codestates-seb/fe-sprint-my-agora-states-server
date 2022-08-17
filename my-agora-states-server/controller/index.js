de4f82836cea7e4b1dd3cd1f397b2c53e92e861b
daldalchoi <vocolate1990@gmail.com>
2022-08-17 14:20:12 +0900

    왜 되는지 모르겠다

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -10,10 +10,12 @@ const discussionsController = {
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
-    const data = discussionsData.filter((discussion) => discussion.id === id);
-    return data !== -1
-      ? res.status(200).json(data)
-      : res.status(400).send("please type correctly");
+    const filterddiscussion = discussionsData.filter(
+      (data) => data.id === Number(req.params.id)
+    );
+    return filterddiscussion.length === 0
+      ? res.status(404).json("please type correctly")
+      : res.status(200).json(filterddiscussion[0]);
   },
 };
 
