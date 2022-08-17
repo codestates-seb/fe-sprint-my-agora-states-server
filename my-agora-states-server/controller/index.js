6212b7091d7b0b81324d31a032aeed8fcf6dbf2f
haileyport <sugar9935@gmail.com>
2022-08-17 17:09:05 +0900

    과제제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const id = Number(req.params.id);
+    const getById = discussionsData.filter((el) => {
+      return el.id === id;
+    });
+    if (getById.length === 0) return res.status(404).send();
+    return res.status(200).send(getById[0]);
+  },
 };
 
 module.exports = {
