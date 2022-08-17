4d3559ae5b28865d1ea52de0a46fe5e5651cef45
GopOwl <viogarden16@gmail.com>
2022-08-17 14:57:28 +0900

    agra server

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params; //param는 예약된 값 
+    const listId = discussionsData.find(value => value.id === Number(id));
+    if(listId) {
+      return res.status(200).json(listId);
+    }
+    else {
+      return res.status(404).json('failed!!');
+    }
   }
 
 };
