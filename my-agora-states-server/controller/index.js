75657694afb0b80cb1ad5663ea09d6c2476be035
ko9612 <kohana735@gmail.com>
2022-08-17 15:09:07 +0900

    test

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,7 +5,6 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-
     return res.status(200).send(discussionsData)
   },
 
