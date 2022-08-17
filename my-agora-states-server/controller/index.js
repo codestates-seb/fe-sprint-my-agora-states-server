6d31a8d9c4266198251f83db53cc30d0a40d65ff
waymokorea <taw0207@naver.com>
2022-08-17 12:00:02 +0900

    bareminimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,12 +5,21 @@ const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
     
-    return res.status(200).send(agoraStatesDiscussions)
+    return res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
+    const { id } = req.params;
+
+    let existId = discussionsData.filter((item) => {
+      return item.id === Number(id)
+    })
+    if (existId.length !== 0) {
+      return res.status(200).json(...existId);
+    }else {
+      return res.status(404).json("wrong request")
+    }
 
 
     res.send('TODO:')
