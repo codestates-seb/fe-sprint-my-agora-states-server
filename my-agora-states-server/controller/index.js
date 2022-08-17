ab0908f0c21ffd22d55f33575afe5de0ca091794
kongchip <myunghwan0321@gmail.com>
2022-08-17 14:51:52 +0900

    나만의 아고라스테이츠 서버

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    let filterDiscussion = discussionsData.filter((el) =>
+    Number(req.params.id) === el.id);
+    if (filterDiscussion[0]){
+      res.status(200).json(filterDiscussion[0])
+    } else {
+      res.status(404).json("No Data Found");
+    }
+  },
 };
 
 module.exports = {
