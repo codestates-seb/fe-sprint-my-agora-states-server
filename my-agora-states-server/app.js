cbb841d6424d26dd4208dc8790149f1380c9736c
seoyeon0108 <97490053+seoyeon0108@users.noreply.github.com>
2022-08-17 16:45:41 +0900

    최소조건

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -6,18 +6,20 @@ const morgan = require('morgan');
 
 // morgan 미들웨어가 세팅되어 있습니다.
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
-app.use(morgan('tiny'));
+app.use(morgan('tiny')); //  시간요청하는 미들웨어임. 신경안써도됨
 
-// TODO: cors를 적용합니다.
+// TODO: cors를 적용합니다.`
+app.use(cors());
 
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+app.use(express.json({strict: false}))
 
 
 const port = 4000;
 const discussionsRouter = require('./router/discussions');
 
 // TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
-
+app.use('/discussions', discussionsRouter) // 질문? 왜 discussionsRouter를 쓰는거지.. 핸들러로 라우트가 일치할 때 실행되는 함수라는데... https://medium.com/@chullino/require-exports-module-exports-%EA%B3%B5%EC%8B%9D%EB%AC%B8%EC%84%9C%EB%A1%9C-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0-1d024ec5aca3 
 
 app.get('/', (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,16 +1,25 @@
+const e = require("express");
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params;
+    let data = discussionsData.filter((el) =>{
+      return el.id === Number(req.params.id)
+    });
+    if(data.length){ //필터된 값이 있으면 길이가 있을것이고, 그럼 일치하는 값이 있다는거
+    res.status(200).send(...data); //왜 구조분해 할당..?  응답의 body 속성으로 id, title, url, author, bodyHTML, avatarUrl이 있어야 합니다.가 통과됨..
+    //아마 객체로 필터되니까? 배열로 반환되서 구조분해로 쪼개서 보내는거이려나...
+    }else{ //길이가 없으면, 요청으로 들어온 id와 일치하는 discussion이 없다는 거
+     res.status(404).send("bad request");
+    }
   }
 
 };
