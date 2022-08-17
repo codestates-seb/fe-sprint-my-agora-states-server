a0032e12dea298d12f60c487cd959e59b3b61ba3
x-xuhxun <hiihii0402@gmail.com>
2022-08-17 14:32:32 +0900

    bare minimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,17 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+  // res.send()
+    let data = discussionsData.filter(el => Number(req.params.id) === el.id);
     
-    res.send('TODO:')
+    if(data.length > 0) {
+      return res.status(200).json(...data);
+    } else {
+      return res.status(404).send('Sorry cant find that!');
+    }
   }
-
 };
 
 module.exports = {
   discussionsController,
 };
+
