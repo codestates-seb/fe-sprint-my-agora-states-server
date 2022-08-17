5478a06e51913f9db9764be2727f34b0fd0886c0
Ko-Eunseo <koyk0408@naver.com>
2022-08-17 14:12:58 +0900

    아고라스테이츠 서버 과제

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,26 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // get /discussions
+    //id, title, url, author, bodyHTML, avatarUrl
+    // let { id, title, url, author, bodyHTML, avatarUrl } = req.body;
+    console.log(req.body[0]);
+    if (Object.keys(req.query).length === 0) { //쿼리가 없을때
+      return res.status(200).json(discussionsData);
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    let filteredData = discussionsData.filter(discussion => {
+      return discussion.id === Number(id);
+    })
+    if (filteredData.length === 0) {
+      return res.status(404).json('${id} is no found');
+    } else {
+      return res.status(200).json(filteredData[0]);
+    }
   }
 
 };
