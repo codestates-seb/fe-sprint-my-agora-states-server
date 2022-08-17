c6e22f0cc590880c506160778d57876eb60fbbfc
headring <myhakiki@gmail.com>
2022-08-17 16:26:06 +0900

    최소요구조건 완료(서버활성화 - S1 my-agroa-states)

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    console.log('here, sending all')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    console.log('here, sending by id');
+    const { id } = req.params;
+    const haveId = agoraStatesDiscussions.filter(el => el.id === Number(id));
+
+    if(haveId.length >= 1){
+      res.status(200).json(haveId[0]);
+    }else{
+      res.status(404).json('no id');
+    }
   }
 
 };
