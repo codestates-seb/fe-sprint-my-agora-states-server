4099ff37eba3894554301ab6be2f232e9692e634
byungjae lee <104547210+ByungJae-Lee@users.noreply.github.com>
2022-08-17 17:48:38 +0900

    SEB_FE_40_이병재 과제 제출합니다.

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,24 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    // path parameter 
+    const {id} = req.params;
+    // console.log(id);
+    // console.log(typeof id);
+    const found =  discussionsData.find(data => data.id === Number(id));
+    if (found) {
+      res.status(200).send(found);
+    } else {
+      res.status(404).send('Not Found')
+    }
+    // console.log(found);
+    res.send(found);
   }
-
 };
 
 module.exports = {
