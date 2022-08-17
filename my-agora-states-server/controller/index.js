f34f51593d09d0c9d6846c6acac9d4ed47b8b2c7
waymokorea <taw0207@naver.com>
2022-08-17 12:47:25 +0900

    test

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -12,11 +12,11 @@ const discussionsController = {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
 
-    let existId = discussionsData.filter((item) => {
+    let list = discussionsData.filter((item) => {
       return item.id === Number(id)
     })
-    if (existId.length !== 0) {
-      return res.status(200).json(...existId);
+    if (list.length !== 0) {
+      return res.status(200).json(...list);
     }else {
       return res.status(404).json("wrong request")
     }
