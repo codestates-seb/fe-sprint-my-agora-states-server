fa12b146127d84cf8956c5a2a7a0c2f8ba97ed8f
2seb2 <yuo231@naver.com>
2022-08-17 14:08:26 +0900

    bareminimum 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const id = parseInt(req.params.id);
+
+    let filteredDiscussions = discussionsData.filter(el => {
+      return el.id === id;
+    })
+
+    if (filteredDiscussions.length) {
+      return res.status(200).json(...filteredDiscussions);
+    }
+    else {
+      return res.status(404).send('error')
+    }
   }
 
 };
