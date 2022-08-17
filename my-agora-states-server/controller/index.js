4a4a26378b08a9b9fb429909d361fcf7abba9e7a
devminoh <gmerit0116@naver.com>
2022-08-17 13:05:34 +0900

    passed

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const filterId = discussionsData.find(
+      (el) => el.id === Number(req.params.id));
+
+    if(filterId){
+      return res.status(200).json(filterId);
+    }else{
+      return res.status(404).json('Not Found!');
+    }
   }
 
 };
