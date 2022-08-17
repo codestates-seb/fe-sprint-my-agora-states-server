d1a1aeb84baf25977a5d0233fbd7d9bbadbe35af
leeseoshim <leeseoit@gmail.com>
2022-08-17 13:59:02 +0900

    agora-server

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,16 +1,28 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
+// console.log(discussionsData);
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    // console.log("req.params.id");
+    // console.log(req.params.id);
+    let filtered = discussionsData.filter(el =>
+      {return Number(req.params.id) === el.id}
+    );
     
-    res.send('TODO:')
+    if(filtered.length === 0){
+      return res.status(404).json();
+    } else {
+      return res.status(200).json(filtered[0]);
+    }  
+    // res.send('TODO:')
   }
 
 };
