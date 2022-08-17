4e1c982ed55951f0c2d5bbd50cb3df0655867821
skyrain1225 <ks501xx@naver.com>
2022-08-17 13:12:25 +0900

    제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const { id } = req.params;
+
+    const data = discussionsData.filter((item) =>
+    item.id === Number(id)
+    );
     
-    res.send('TODO:')
+    if(data.length) return res.status(200).json(data[0]);
+    else return res.status(404).end();
   }
 
 };
