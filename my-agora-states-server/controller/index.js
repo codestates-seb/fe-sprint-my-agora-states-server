035e69e8c31421e1a9e62e9fe669ef503d3bbb3d
jooyong-boo <qnwndyd159@naver.com>
2022-08-17 12:34:18 +0900

    요청 id와 일치하는 discussion 응답

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,6 +9,13 @@ const discussionsController = {
 
     findById: (req, res) => {
         // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+        const id = req.params.id;
+        let result = discussionsData.filter((data) => data.id === Number(id));
+        if (result?.length) {
+            res.status(200).json(...result);
+        } else {
+            res.status(404).send("bad request");
+        }
     },
 };
 
