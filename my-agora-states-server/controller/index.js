3a4fec83f68256f2e2a139440c7b3a7e10c40307
js-ha <jshaha0911@gmail.com>
2022-08-17 16:14:42 +0900

    완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+  const {id} = req.params;
+  const data = discussionsData.find((data)=> data.id === Number(id));
+  if(data){
+    res.status(200).json(data)
+  } else {
+    res.status(404).send("Not Found");
+  }
   }
 
 };
