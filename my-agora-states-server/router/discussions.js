128db33677a6d5285499dd9438d7615afe1dafb9
JungInHa <juudyy99@gmail.com>
2022-08-17 15:47:17 +0900

    모든 discussion 목록을 조회하는 라우터 작성

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/', findAll)
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
 
