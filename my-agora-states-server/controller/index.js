cd90f9536c9dbac6f64ed8cada699e67dc206df3
ko9612 <kohana735@gmail.com>
2022-08-17 14:57:17 +0900

    1stcommit

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,16 +1,31 @@
+const { filter } = require("../../../../unit10/fe-sprint-statesairline-server/statesairline/repository/airportList");
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+
+    return res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+
+    const {id} = req.params;
     
-    res.send('TODO:')
+    if(id !== undefined){
+      const filteredById = discussionsData.filter((el)=>{
+        return el.id === Number(id);
+      })
+
+      if(filteredById.length === 0){
+        return res.status(404).send('Not found data');
+      }
+      else{
+        return res.status(200).send(...filteredById);
+      }
+    }
   }
 
 };
