2e909a8f513d56739b5c1059f6f045a1cae45396
moonjh9392 <moonjh9392@gmail.com>
2022-08-17 10:48:19 +0900

    테스트 통과

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,28 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
+console.log("controller");
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    console.log("/discussions/findAll");
+    res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    console.log("/discussions/findById");
+    const { id } = req.params;
+    const result = discussionsData.filter((ele) => {
+      return ele.id === Number(id);
+    });
 
+    if (result.length === 0) {
+      res.status(404).json(result);
+    } else {
+      res.json(result[0]);
+    }
+  },
 };
 
 module.exports = {
