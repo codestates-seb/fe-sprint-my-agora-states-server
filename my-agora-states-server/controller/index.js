94be7af5e38a4dfddbb562526d27acf17506e7fa
Heera1 <caecus.regio@gmail.com>
2022-08-17 17:20:21 +0900

    과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    // 파람스로 들어오는 데이터는 str 타입이므로 num 타입으로 바꿔줘야 한다
+    const list = discussionsData.find((item) => item.id === Number(id))
+    if(list){
+      return res.status(200).send(list);
+    } else {
+      return res.status(404).send('Not Found');
+    }
   }
-
 };
 
 module.exports = {
