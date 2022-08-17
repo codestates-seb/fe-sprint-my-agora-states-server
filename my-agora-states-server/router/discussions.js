5b596329a78cbdffacc88564ab7c8e1e231d48a7
billy5982 <kuu0404@naver.com>
2022-08-17 13:28:10 +0900

    서버 구현 완료

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,13 +1,16 @@
 // TODO: discussions 라우터를 완성합니다.
-const { discussionsController } = require('../controller');
+
+const  {discussionsController} = require('../controller');
+// index js에서 객체로 해당 discussionsController을 감싸서 내보내줌 그래서 구조 분해할당으로 얻어낸
+
 const { findAll, findById } = discussionsController;
 const express = require('express');
 const router = express.Router();
-
+console.log(router)
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/',findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-
+router.get('/:id',findById);
 
 module.exports = router;
