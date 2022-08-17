c4de8e8e1e06423fa7aa27f338dad69f39361478
likeprograming1 <hsj2216@naver.com>
2022-08-17 14:57:51 +0900

    베어미니엄

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const {id} = req.params;
+    let FilterId = discussionsData.filter(data=> data.id === Number(id))[0];
+    if(FilterId){
+      res.send(FilterId);
+    }else{
+      res.status(404).send(FilterId);
+    }
     
-    res.send('TODO:')
   }
 
 };
