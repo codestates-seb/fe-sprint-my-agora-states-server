610fe4f7831a23c95a36ace1282ff09b57e8e85d
lactofreemilk <jihwan4743@gmail.com>
2022-08-17 14:25:02 +0900

    첫작동완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params;
+    let position = discussionsData.findIndex((obj)=>obj.id === Number(id));
+    if(position === -1 ) {
+      res.status(404).send()
+    } else {
+      res.status(200).send(discussionsData[position])
+    }
   }
 
 };
