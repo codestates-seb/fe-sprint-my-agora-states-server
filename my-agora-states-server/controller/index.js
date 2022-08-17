1b3fa4cc191542b310e128a68b4b7aeac958122b
devjindev <hyjinny00@gmail.com>
2022-08-17 11:10:10 +0900

    나만의 아고라 스테이츠 서버 완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const filteredDiscussion = discussionsData.filter(
+      (discussion) => discussion.id == req.params.id
+    );
 
+    if (filteredDiscussion[0]) {
+      res.send(filteredDiscussion[0]);
+    } else {
+      res.status(404).json("해당 id와 일치하는 데이터가 존재하지 않습니다.");
+    }
+  },
 };
 
 module.exports = {
