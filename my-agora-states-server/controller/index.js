2d9e2e8a061e31af1c707fac7533efba46bca26c
ssunip <ssunip99@gmail.com>
2022-08-17 16:31:50 +0900

    agora-states-server bare minimum 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(agoraStatesDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    console.log(typeof req.params); // type이 object
+    console.log(id);
 
+    const data = discussionsData.filter((el) => el.id === Number(id));
+    if (data.length !== 0) {
+      res.send(data[0]);
+    } else {
+      res.send(res.status(404).json("Not found!"));
+    }
+  },
 };
 
 module.exports = {
