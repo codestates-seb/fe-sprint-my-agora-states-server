1486d4d940dde218cb1fa2a36d1cc72b9be8aa02
leg1771 <leg1770@naver.com>
2022-10-18 15:49:49 +0900

    최종

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,25 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    if (Object.keys(req.query).length === 0) { 
+      return res.status(200).json(discussionsData);
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const {id} = req.params;
     
-    res.send('TODO:')
+    let a = discussionsData.filter((el)=>{
+      return Number(id) === el.id;
+    })
+    if (a.length === 0) { 
+      return res.status(404).json('${id} dont exist');
+    } else { 
+      return res.status(200).json(a[0]);
+      
+    }
+    
   }
 
 };
