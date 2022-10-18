03048e28369f30201386ea0048873fd3d49c44ce
Haejeong <skhj5822@naver.com>
2022-10-18 11:33:27 +0900

    bare minimum complete

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    if(id){
+      let filteredData = discussionsData.find(discussion => discussion.id===Number(id));
+      filteredData ? res.status(200).send(filteredData) : res.status(404).send('Not Found!');
+    }
+    res.status(404).send('Required params');
+    // res.send('TODO:')
   }
 
 };
