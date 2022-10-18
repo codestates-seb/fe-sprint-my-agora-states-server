2e18cde2cfffbf9721ade3a9d8a53777c8d29bcd
kimtank <sycork@gmail.com>
2022-10-18 13:33:22 +0900

    router 설정

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,9 +5,15 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-
+/** 
+ * https://ryan-han.com/post/translated/pathvariable_queryparam/
+ * Resource가져오는 GET -> Path Variable
+ * sort or filtering -> QueryParams 
+ */
+//id에 맞게 솔팅하는거라서 그런가?
+router.get('/:id', findById);
 
 module.exports = router;
