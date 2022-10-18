708b5783819b99c6c5cc9e964fa940338a1e5ef1
kimdong-hun <kimdonghun426@gmail.com>
2022-10-18 14:34:54 +0900

    feat: 요청으로 들어온 id와 일치하는 discussion 응답

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,7 +9,31 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
+    const {id} = req.params;
+
+    let result = discussionsData.filter((data) => {
+      return data.id === Number(id);
+    })
+
+    if(result.length !== 0) {
+      res.status(200).json(result[0]);
+      // res.status(200).json(result);
+    }
+    else {
+      res.status(404).send('Not Found');
+    }
+
+    // let result = discussionsData.find((data) => {
+    //   return data.id === Number(id);
+    // })
+
+    // if(result) {
+    //   res.status(200).json(result);
+    //   // res.status(200).json(result);
+    // }
+    // else {
+    //   res.status(404).send('Not Found');
+    // }
   }
 };
 
