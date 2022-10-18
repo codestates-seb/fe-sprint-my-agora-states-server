b08788b3a1cbb1f93389162b2801dd8bb43b360b
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-10-18 14:36:38 +0900

    bare minium 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,19 +1,24 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
+
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const result = discussionsData.slice();
+    return res.status(200).json(result)
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params;
+    const result = discussionsData.find(el => el.id === Number(id))
+    if(result){
+      return res.status(200).json(result)
+    } else{
+      return res.status(404).json('error')
+    }
   }
-
-};
+  // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+  }
 
 module.exports = {
   discussionsController,
