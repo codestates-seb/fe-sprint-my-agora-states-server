80114b0905c43b818f35c69b6ada79e1206fc074
Hyunji0911 <hyunji.lee0911@gmail.com>
2022-10-18 12:34:45 +0900

    과제제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -2,19 +2,24 @@ const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
-  findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
-  },
+	findAll: (req, res) => {
+		// TODO: 모든 discussions 목록을 응답합니다.
+		return res.status(200).json(discussionsData);
+	},
 
-  findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+	findById: (req, res) => {
+		// TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+		const { id } = req.params;
+		let filtered = discussionsData.filter((el) => el.id === Number(id));
 
+		if (filtered.length !== 0) {
+			return res.status(200).send(filtered[0]);
+		} else {
+			return res.status(404).send("Sorry!");
+		}
+	},
 };
 
 module.exports = {
-  discussionsController,
+	discussionsController,
 };
