c28a779341618d493a1bfc9df0a7137833e859f3
Lee SeungHyun <tmdgusdlrk2@gmail.com>
2022-10-18 15:24:52 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(agoraStatesDiscussions)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const { id } = req.params;
     
-    res.send('TODO:')
-  }
+    let filteredData = agoraStatesDiscussions.filter((ele) => {
+      return ele.id === Number(id);
+    });
+
+    if (filteredData) {
+      res.status(200).send(...filteredData);
+    }
+    res.status(404).send("Not found");
+  },
 
 };
 
