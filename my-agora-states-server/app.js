9fb338cee73b110b3048482ebaca9508c9bcfeb2
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2022-10-18 14:13:03 +0900

    1.01

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
 const morgan = require('morgan');
-
-// morgan 미들웨어가 세팅되어 있습니다.
-// HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
-app.use(morgan('tiny'));
-
-// TODO: cors를 적용합니다.
-
-// TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-
 
 const port = 4000;
 const discussionsRouter = require('./router/discussions');
 
+// TODO: cors를 적용합니다.
+app.use(cors())
+// TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+app.use(express.json());
+// morgan 미들웨어가 세팅되어 있습니다.
+// HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
+app.use(morgan('tiny'));
+//fetch("http://localhost:4000").then()~)
 // TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
-
+app.use('/discussions', discussionsRouter);
 
 app.get('/', (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const params = req.params.id;
+    const filteredId = discussionsData.filter((key) => key.id === Number(params))
+    if (filteredId.length !== 0) {
+      return res.status(200).json(filteredId[0]);
+    } else {
+      return res.status(404).end();
+    }
   }
 
 };
