9fc7925df5f523de3c62a8ceeb1dec5791464a3a
niv_ad <didekqls93@gmail.com>
2022-10-18 19:57:18 +0900

    SEB_FE_41 양다빈 과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+
+    return res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params;
+    const idList = discussionsData.filter((el)=>{
+      return el.id === Number(id)
+    })
+    if(idList.length !== 0) {
+      return res.status(200).json(idList[0])
+    }else
+    console.log(res.status())
+    return res.status(404).send('오류남')
   }
-
 };
 
 module.exports = {
