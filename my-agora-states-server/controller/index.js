404abed3f9fc6d13e09bdd95563165f5e247b6f8
aemaaeng <haemin120@naver.com>
2022-10-18 11:10:10 +0900

    Bare minimum request: my-agora-states-server 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(agoraStatesDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    // req.params { id: '~~' }
+    let filtered = agoraStatesDiscussions.filter((obj) => {
+      return obj.id === Number(req.params.id);
+    });
+    // if 문으로 분기 / filtered가 있냐 없냐로
+    if (filtered.length !== 0) {
+      return res.status(200).json(filtered[0]);
+    } else {
+      return res.status(404).send("Bad Request");
+    }
+  },
 };
 
 module.exports = {
