1fac1eec3eb78c6f3c378fa3d5c3b80c0c29b2a4
djwjfk <grw07236@naver.com>
2022-10-18 16:17:15 +0900

    아고라스테이츠 서버만들기 완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -2,17 +2,34 @@ const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
+  // GET /discussions
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    //GET /discussions/:id
+    const { id } = req.params; 
+    const findById = discussionsData.find(discussion => discussion.id === Number(id));
+    if(findById) {
+      return res.status(200).json(findById)
+    } else {
+      return res.status(404).send('Not found');
+    }
   }
-
+  
+ //! 실패한 방법
+    // if(req.params !== undefined){
+    //   console.log(req.params);
+    //   const filteredId = discussionsData.filter((discussions)=>{
+    //     return discussions.id === req.params.id
+    //   });
+    //   console.log(filteredId);
+    //   return res.status(200).json(filteredId)
+    // }
+    // return res.status(404).json(discussionsData)
 };
 
 module.exports = {
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -2,12 +2,13 @@
 const { discussionsController } = require('../controller');
 const { findAll, findById } = discussionsController;
 const express = require('express');
+const { app } = require('../app');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/' , findAll)
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-
+router.get('/:id' , findById)
 
 module.exports = router;
