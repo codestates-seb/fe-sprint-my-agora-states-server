1cb32dbc14da373baf3dc1144015086e03528bb8
kanglaeheon <kanglaeheon@gmail.com>
2022-10-18 13:25:05 +0900

    Feat: bare minimum completed

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -1,27 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 
 // morgan 미들웨어가 세팅되어 있습니다.
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-// TODO: cors를 적용합니다.
-
-// TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-
+// TODO: (O) cors를 적용합니다.
+app.use(cors());
+// TODO: (O) Express 내장 미들웨어인 express.json()을 적용합니다.
+app.use(express.json());
 
 const port = 4000;
-const discussionsRouter = require('./router/discussions');
+const discussionsRouter = require("./router/discussions");
 
-// TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
+// TODO: (O) app.use()를 활용하여 /discussions 경로로 라우팅합니다.
+app.use("/discussions", discussionsRouter);
 
+// 캐시 컨트롤
+// app.use((req, res, next) => {
+//   res.set("Cache-control", "no-store");
+//   next();
+// });
 
-app.get('/', (req, res) => {
+// 캐시 disable
+// app.disable("view cache");
+
+app.get("/", (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
-  res.status(200).send('fe-sprint-my-agora-states-server');
+  res.status(200).send("fe-sprint-my-agora-states-server");
 });
 
 const server = app.listen(port, () => {
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,16 +3,48 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // TODO: (O) 모든 discussions 목록을 응답합니다.
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    // TODO: (O) 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const { id } = req.params;
+    const filteredById = discussionsData.filter((item) => {
+      if (item.id === Number(id)) {
+        return true;
+      }
+      return false;
+    });
+    if (filteredById.length === 0) {
+      res.status(404).send("해당 id를 찾을 수 없습니다.");
+    } else {
+      const {
+        id,
+        title,
+        url,
+        author,
+        bodyHTML,
+        avatarUrl,
+        answer,
+        createdAt,
+        updatedAt,
+      } = filteredById[0];
+      const body = {
+        id,
+        title,
+        url,
+        author,
+        bodyHTML,
+        avatarUrl,
+        answer,
+        createdAt,
+        updatedAt,
+      };
+      console.log(body);
+      res.status(200).json(body);
+    }
+  },
 };
 
 module.exports = {
