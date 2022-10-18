bf2f56e81fadffc6ffa619cb2f582884398e23ae
2Zerozero <hawgae@gmail.com>
2022-10-18 14:40:19 +0900

    과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const data = discussionsData.slice();
+    return res.status(200).json(data);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    const list = discussionsData.find( el => el.id === Number(id) )
+    if(list) {
+      return res.status(200).json(list);
+    }
+    else{
+      return res.status(404).json('Incorrect request');
+    }
   }
 
 };
