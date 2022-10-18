b1726f7283b36ad583fde01f3cfdd1ed35a93825
atia <hckr1994@gmail.com>
2022-10-18 15:54:40 +0900

    fix: find 대신 filter 사용하도록 변경

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,9 +9,9 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    const _data = discussionsData.find(el => (el.id === Number(req.params.id)));
-    if (!_data) res.status(404).send('일치하는 아이디가 존재하지 않습니다.');
-    else res.status(200).json(_data);
+    const _data = discussionsData.filter(el => (el.id === Number(req.params.id)));
+    if (_data.length === 0) res.status(404).send('일치하는 아이디가 존재하지 않습니다.');
+    else res.status(200).json(..._data);
   }
 };
 
