606ab6a634e0d23b059442f6f583f61640b39c33
ninefloor <less0805@gmail.com>
2022-10-18 10:50:59 +0900

    Bare Minimum 작업 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(agoraStatesDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params
+    const filtered = agoraStatesDiscussions.filter(obj => {
+      return obj.id === Number(id)
+    })
+    if(filtered.length){
+      res.status(200).json(...filtered);
+    } else{
+      res.status(404).json('Not Found')
+    }
   }
 
 };
