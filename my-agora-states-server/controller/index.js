fe62fa994bedde4425278713bab7bbaf0c675cea
sktjgudals <sktjgudals@naver.com>
2022-10-18 10:07:50 +0900

    SERVER 과제 수정

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const data = discussionsData;
+    const filterById = data.filter((x) => {
+      if (Number(id) === Number(x.id)) return true;
+      return false;
+    });
+    if (filterById.length === 0) {
+      return res.status(404).json(filterById[0]);
+    } else {
+      return res.status(200).json(filterById[0]);
+    }
+  },
 };
 
 module.exports = {
