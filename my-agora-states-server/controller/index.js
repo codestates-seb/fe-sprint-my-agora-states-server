f2e297fd8cdeaffa202634089db8e3d3c60eb9fb
noah-eunsoo <noah.eunsoo0504@gmail.com>
2022-10-18 15:24:09 +0900

    SEB_FE_41_김은수 과제 제출합니다

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,28 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(agoraStatesDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    // TODO:
+    if (id) {
+      let data = discussionsData.filter((el) => {
+        return Number(id) === el.id;
+      });
 
+      if (data.length === 0) {
+        return res.status(404).json({ status: false });
+      } else {
+        return res.status(200).json(...data);
+      }
+    }
+  },
 };
 
 module.exports = {
