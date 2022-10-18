5cc3101d8184f9d0987f11f782c105aa561db874
limdumb <dlaruddls@nate.com>
2022-10-18 11:50:01 +0900

    findAll,findById 작성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const data = discussionsData.slice()
+    return res.status(200).json(data)
+
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params
+    const sameID = discussionsData.filter((data) => {
+      return data.id === Number(id)
+    })
+
+    if(sameID.length === 0) {
+      res.status(404).json("잘못된 Request 입니다.")
+    } else {
+      res.status(200).json(...sameID)
+    }
   }
 
 };
