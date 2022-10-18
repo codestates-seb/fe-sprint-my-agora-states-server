f79ac51706e7229cbf01ade4def440adbe482e91
minseob shim <shim5505@gmail.com>
2022-10-18 17:28:05 +0900

    일부 복구 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send("TODO:");
+    return res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const { id } = req.params;
+    const data = discussionsData.filter(
+      (discussion) => discussion.id === Number(id)
+    );
 
-    res.send("TODO:");
+    if (data) {
+      return res.state(200).json(...data);
+    } else {
+      return res.state(404).send("Not Found");
+    }
   },
 };
 
