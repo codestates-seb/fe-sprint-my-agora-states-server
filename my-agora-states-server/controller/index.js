7ba66e2982d762b1fc992ba765a952a925e55eed
lmimoh <dlalsgns9039@gmail.com>
2022-10-18 16:44:23 +0900

    modified

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,19 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(agoraStatesDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    const result = agoraStatesDiscussions.filter(el => el.id === Number(id));
 
+    if(result.length !== 0) res.json(result[0]);
+    else res.status(404).send('empty');
+  }
 };
 
 module.exports = {
