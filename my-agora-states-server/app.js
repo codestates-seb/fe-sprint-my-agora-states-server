faaddd1adc66cbb7a40641e66b5d3ea52c65fd2d
FE012 <twte96@gmail.com>
2022-10-18 13:58:45 +0900

    first commit

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -3,20 +3,21 @@ const app = express();
 
 const cors = require('cors');
 const morgan = require('morgan');
-
 // morgan 미들웨어가 세팅되어 있습니다.
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
 app.use(morgan('tiny'));
 
-// TODO: cors를 적용합니다.
-
-// TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+// 1. TODO: cors를 적용합니다.
+app.use(cors());
+// 2. TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+app.use(express.json({ strict: false }));
 
 
 const port = 4000;
 const discussionsRouter = require('./router/discussions');
 
-// TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
+// 3. TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
+app.use('/discussions', discussionsRouter)
 
 
 app.get('/', (req, res) => {
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,16 +3,25 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    //1. TODO: 모든 discussions 목록을 응답합니다.
+    if (Object.keys(req.query).length === 0) {
+      return res.status(200).json(discussionsData);
+    }
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    //2. TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const { id } =req.params;
+    let filteredData = discussionsData.filter(discussion => {
+      return discussion.id === Number(id);
+    })
+    if(filteredData.length === 0) {//필터되어 나온 게 없다면
+     return res.status(404).json(`{id} is not found`)
+    }
+    else { //필터되어서 나온게 있다면
+      return res.status(200).json(filteredData[0])
+    }
   }
-
 };
 
 module.exports = {
