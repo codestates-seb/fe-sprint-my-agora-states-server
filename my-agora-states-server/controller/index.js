f66f006e85f00a73e48fa6ccbd875a300a635d47
KISOOJIN <ekf4455@naver.com>
2022-10-18 14:16:15 +0900

    나만의 아고라 스테이츠 서버 index.js 수정

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -11,11 +11,21 @@ const discussionsController = {
     const data = discussionsData.find(
       (discussion) => discussion.id === Number(req.params.id)
     );
+    //find()는 값을 반환하고 filter()는 배열을 반환
+    // find() 이용시
     if (data) {
       return res.status(200).json(data);
     } else {
       return res.status(404).json("Not found");
     }
+
+    // filter() 이용시
+    // if (data.length > 0) {
+    //   return res.status(200).json(...data);
+    // } else {
+    //   return res.status(404).json("Not found");
+    // }
+
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
   },
 };
