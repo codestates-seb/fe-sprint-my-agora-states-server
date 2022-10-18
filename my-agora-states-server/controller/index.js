bd188510aa2622fe5b70a55b027e10b4124ea5b0
SUBINSON <fraisecheris@gmail.com>
2022-10-18 12:01:21 +0900

    feat: index.js - [GET] /discussion/:id 구현

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,8 +9,9 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    const data = discussionsData.filter(discussion => discussion.id === Number(id));
+    return (data.length !== 0 ? res.json(data[0]) : res.status(404).json(data))
   }
 
 };
