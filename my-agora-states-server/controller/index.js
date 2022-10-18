a29d82721244d908f3381ad6c5657e6283c278c0
Sehannnnnnn <stanley710@naver.com>
2022-10-18 16:06:37 +0900

    clear bear minimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,14 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData);
   },
-
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { uuid } = req.params;
+    const selectedDataById = discussionsData.filter((data) => data.id == uuid);
+    if (selectedDataById.length == 0) return res.status(404).send(selectedDataById[0]);
+    else return res.status(200).send(selectedDataById[0]);
   }
 
 };
