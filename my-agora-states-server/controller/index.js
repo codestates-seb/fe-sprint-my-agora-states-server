5b7aeea3e2afd3e5562ca57b0566d87843cf5273
davina708 <davinida96@gmail.com>
2022-10-18 15:38:54 +0900

    과제 코드 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    console.log(req.params);
+    const data = discussionsData.find(
+      (discussion) => discussion.id === Number(id)
+    );
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      return res.status(404).json("Incorrect request");
+    }
+  },
 };
 
 module.exports = {
