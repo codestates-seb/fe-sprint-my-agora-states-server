95f29cbdee4b8fa3fd1de9236b2678af465e899e
ParkHyunSeok <luckseok1@gmail.com>
2022-10-18 11:35:06 +0900

    My Agora States Server 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,25 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
+const { param } = require("../router/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.param;
+    const data = discussionsData.find(
+      (discussion) => discussion.id === Number(id)
+    );
+    if (data) {
+      return res.status(200).json(data);
+    } else {
+      return res.status(404).send("Not found");
+    }
+  },
 };
 
 module.exports = {
