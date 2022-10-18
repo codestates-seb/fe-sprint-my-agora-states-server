6194bae3cf91a68f38c981ea331a955b65dd1bdd
kimhw7 <khw786678@gmail.com>
2022-10-18 14:24:19 +0900

    라우터 생성 및 내용 수정

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    const filteredById = discussionsData.filter(discussion => discussion.id === Number(id));
+    if(filteredById.length === 0) {
+      return res.status(404).json('not a available id')
+    }else {
+      return res.status(200).json(filteredById[0]);
+    }
   }
 
 };
