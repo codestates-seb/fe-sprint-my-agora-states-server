0d9910d5e4342873db33721a4a1ce181e9ff2ab1
sweetyr928 <031694@naver.com>
2022-10-18 11:32:41 +0900

    bareminimum done

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const { id } = req.params;//string
+    let filteredDiscussion = discussionsData.filter((discussion) => {
+      return discussion.id === Number(id);
+    })
+    if(filteredDiscussion.length !== 0){
+      return res.status(200).send(filteredDiscussion[0]);
+    }else{
+      return res.status(404).send('Not Found!');
+    }
     
-    res.send('TODO:')
   }
 
 };
