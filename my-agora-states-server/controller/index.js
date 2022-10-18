d9912fcdb37c4ce434a6ef1fb0553c92b8051919
Haejeong <skhj5822@naver.com>
2022-10-18 16:34:19 +0900

    find/filter에 대한 주석추가

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,18 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.status(200).send(discussionsData);
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
     if(id){
-      let filteredData = discussionsData.find(discussion => discussion.id===Number(id));
-      filteredData ? res.status(200).send(filteredData) : res.status(404).send('Not Found!');
+      // filter 메소드를 쓰고 싶으시면 반환값이 배열이니까 객체로 바꿔서 보내줘야 함.
+      // 1명이 작성한 여러 트윗을 찾아야 하는 경우 -> filter(O), find(X)
+      let filteredData = discussionsData.find(discussion => discussion.id===Number(id)); 
+      filteredData ? res.status(200).send(filteredData) : res.status(404).send('Not Found!'); 
     }
     res.status(404).send('Required params');
-    // res.send('TODO:')
   }
 
 };
