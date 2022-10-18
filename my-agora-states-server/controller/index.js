40bd82b22be162e0dd854159da898ef325e6c063
ssunyk <ssyykk0506@gmail.com>
2022-10-18 15:10:49 +0900

    bare minimum 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,14 +3,18 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // TODO: 모든 discussions 목록을 응답합니다. 전체 반환
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다. req.params
+    const { id } = req.params;
+    const data = discussionsData.find(discussions => discussions.id === Number(id));
+    if(data) {
+      return res.status(200).json(data);
+    }
+    return res.status(404).json("해당 id와 일치하는 데이터가 존재하지 않습니다.");
   }
 
 };
