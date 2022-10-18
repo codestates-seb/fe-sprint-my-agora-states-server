a79364c94771da8e03b49395cf9967f81de42814
seungheonkim <seungheon818@naver.com>
2022-10-18 14:55:30 +0900

    bare_done

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,20 +1,24 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const {agoraStatesDiscussions} = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
-  findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
-  },
+    findAll: (req, res) => {
+        // TODO: 모든 discussions 목록을 응답합니다.
+        return res.status(200).json(discussionsData);
+    },
 
-  findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    findById: (req, res) => {
+        // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+        const {id} = req.params;
+        const id_filter = discussionsData.filter((el) => {
+            return el.id === Number(id);
+        })
+        if (id_filter.length === 0) return res.status(404).json('No discussion found!');
+        return res.status(200).json(id_filter[0]);
+    }
 
 };
 
 module.exports = {
-  discussionsController,
+    discussionsController,
 };
