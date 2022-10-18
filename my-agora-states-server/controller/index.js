c8e74f4ee24dcda7fc4c5fb70e3d27843d54d414
pchan191 <pchan191@gmail.com>
2022-10-18 14:43:20 +0900

    PASS

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,20 +1,24 @@
+const { json } = require("express");
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    const data = discussionsData.filter(discussion => discussion.id === Number(id));
+    if(data.length !== 0){
+      return res.status(200).json(data[0])
+    } else if(data.length === 0){
+      return res.status(404).send('Not found');
+    }
   }
-
-};
-
+}
 module.exports = {
   discussionsController,
 };
