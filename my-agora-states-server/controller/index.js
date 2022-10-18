8440c30dfaef7de7996ec1be907500a90c7b7110
jsh3418 <albert5428@gmail.com>
2022-10-18 13:52:08 +0900

    feat: bare minimum implement

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    const requestDiscussion = discussionsData.filter((disccusion) => disccusion.id === Number(id));
+
+    if (requestDiscussion.length === 0) {
+      res.status(404).send("해당 아이디는 없는 아이디입니다.");
+      return;
+    }
+
+    res.status(200).send(...requestDiscussion);
+  },
 };
 
 module.exports = {
