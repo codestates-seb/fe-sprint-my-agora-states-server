98691d6809edf7a34b5ac24e2c828c70c4859e59
dev-sena <leesena948@gmail.com>
2022-10-18 12:47:16 +0900

    SEB_FE_41_이세나 과제 제출합니다.

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,28 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    const filteredList = discussionsData.filter((data) => {
+      // data.id인 discussions.js의 id는 숫자, req.params.id는 문자열이므로 맞춰줘야 함
+      return data.id === Number(id);
+    });
+
+    // 일치하는 id가 없으면 filteredList의 length가 0이고, 있으면 0이 아님
+    if (filteredList.length === 0) {
+      return res
+        .status(404)
+        .json(`${id}와 일치하는 데이터가 존재하지 않습니다.`);
+    } else {
+      // console.log(filteredList); -> [{}]이고 원하는 형태는 {}
+      return res.status(200).json(filteredList[0]);
+    }
+  },
 };
 
 module.exports = {
