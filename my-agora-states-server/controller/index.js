eb556aaf9838e133eb1f5fdc4b671706f6eb9493
shinnh2 <nahyeon.shin9@gmail.com>
2022-10-18 11:39:27 +0900

    discussion 조회 기능 작성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -2,19 +2,24 @@ const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
-  findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
-  },
+	findAll: (req, res) => {
+		// TODO: 모든 discussions 목록을 응답합니다.
+		res.send(agoraStatesDiscussions);
+	},
 
-  findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+	findById: (req, res) => {
+		// TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+		let filteredDiscussion = [];
+		console.log(req.params.id);
+		if (req.params.id) {
+			filteredDiscussion = discussionsData.filter((el) => {
+				return el.id === Number(req.params.id);
+			});
+		}
+		res.send(filteredDiscussion[0]);
+	},
 };
 
 module.exports = {
-  discussionsController,
+	discussionsController,
 };
