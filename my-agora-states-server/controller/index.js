755d2453055d71ecf66867e944b3fa05a505a5c1
hannaax <hanan941008@gmail.com>
2022-10-18 13:57:26 +0900

    아고라스테이츠서버 과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,14 +3,32 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
+    console.log(req)
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    if (Object.keys(req.query).length === 0) {
+      return res.status(200).json(
+        // 'TODO:'
+        discussionsData)
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const discussions = agoraStatesDiscussions
+    const { id } = req.params
+
+      // if(id !== undefined) {
+        const data = discussionsData.filter((discussion)=> {
+          return discussion.id === Number(id);})
+        // return res.status(200).json(data[0])
+      // }
+
+      if (data.length === 0) {
+        return res.status(404).json('${id} is not found')
+      }
+      else {
+        return res.status(200).json(data[0])
+      }
   }
 
 };
