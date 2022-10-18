33e8c8a1ff2993f86cede4a44e0738513560fd04
sena <senika_@naver.com>
2022-10-18 12:46:39 +0900

    과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,17 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    // req.params 이용
+    const { id } = req.params;
+    const data = discussionsData.filter(
+      (discussion) => discussion.id === parseInt(id)
+    );
+    return data.length ? res.status(200).json(...data) : res.status(404).send();
+  },
 };
-
 module.exports = {
   discussionsController,
 };
