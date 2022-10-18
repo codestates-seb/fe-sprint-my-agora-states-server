18257a8f6b10c1fc0f69afda0962d24d557e9271
bakjonghyo <cutitout52@gmail.com>
2022-10-18 15:57:24 +0900

    SEB_FE_41_박종효 과제 제출
    1. 과제 자체의 방법은 알겠으나 기존에 만든 아고라 스테이츠에 연동하는 방법을 잘 모르곘음
    2. 테스트 하는 방법도 잘모르겠음

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,18 +3,17 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const data = discussionsData;
+    return res.status(200).json(data);
   },
-
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const list = discussionsData.filter((element) => element.id === Number(id));
+    if (list) {
+      return res.status(200).json(list);
+    } else {
+      return res.status(404).json("Incorrect request");
+    }
+  },
 };
-
-module.exports = {
-  discussionsController,
-};
+module.exports = { discussionsController };
