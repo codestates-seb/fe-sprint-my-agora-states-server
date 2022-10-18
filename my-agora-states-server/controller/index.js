85898de04fe73b3b7286e0c64e17ae5f6255d4e1
jwpark <pjwp94@gmail.com>
2022-10-18 10:20:40 +0900

    basic server 구축 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    let result = {}
+    for (el of discussionsData) {
+      if(el.id === Number(req.params.id)) {
+        result = el;
+        res.status(200).json(result);
+      }
+    }
+    res.status(404).json("해당 discussion이 존재하지 않습니다.")
   }
 
 };
