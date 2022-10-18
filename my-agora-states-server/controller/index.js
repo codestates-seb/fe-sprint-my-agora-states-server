c248918b2263ee1e8ab072c5b577a945daa60a43
ahnseoYoo <ahnseo.yoo@gmail.com>
2022-10-18 11:07:38 +0900

    bare minimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    let filteredData;
+    //console.log(typeof(id))//string
+    if(id){
+      filteredData = discussionsData.find(item => item.id === Number(id)); //.find is recommended over filter. Since we're looking for a single element in an array, iterating through all the elements might include unnecessary steps.
+     filteredData ? res.status(200).send(filteredData)  // If data found, returns the data
+     : res.status(404).send("Requested data does not exist"); // If data not found, throw error
+    }
+
+    else{
+      res.status(404).send("Please put all the required parameters. required params: id") // Reqests must be in a proper format. If not, throw error 
+    } 
   }
 
 };
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -4,10 +4,12 @@ const { findAll, findById } = discussionsController;
 const express = require('express');
 const router = express.Router();
 
-// TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
+// router.get('/',(req,res) => res.status(200).send("Welcomd to the main page of discussions router. available endpoints: /findall, /findbyid")) -- this code line is unused. endpoint '/' must return all data
 
+// TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
+router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-
+router.get('/:id',findById);
 
 module.exports = router;
