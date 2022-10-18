b978db9f60c6ac1427fb2addbf3c2427e7a68400
jjeonge97 <cowguswjd@gmail.com>
2022-10-18 13:07:22 +0900

    submit

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,20 +1,26 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
-  findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
-  },
+    findAll: (req, res) => {
+        // TODO:모든 discussions 목록을 응답합니다.
+        res.status(200).send(discussionsData);
+    },
 
-  findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    findById: (req, res) => {
+        // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+        const { id } = req.params;
+        const data = discussionsData.find((discussion) => {
+            return discussion.id === Number(id);
+        });
+        if (data) {
+            return res.status(200).send(data);
+        } else {
+            return res.status(404).send('Not Found');
+        }
+    },
 };
 
 module.exports = {
-  discussionsController,
+    discussionsController,
 };
