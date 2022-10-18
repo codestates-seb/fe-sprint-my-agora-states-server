aa8d23e1a890f0ca267516bdefcd38e57067a606
gkgjswls <gkgjswls842@gmail.com>
2022-10-18 12:38:08 +0900

    구현완료
    완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+
+    return res.status(200).json(discussionsData)
+    
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     
-    res.send('TODO:')
+    const result = discussionsData.filter((obj)=>{return obj.id === Number(req.params.id)})
+    const sending = result[0];
+    return (result.length !== 0? res.status(200).json(sending) : res.status(404).send('Not Found'))
+    
   }
 
 };
