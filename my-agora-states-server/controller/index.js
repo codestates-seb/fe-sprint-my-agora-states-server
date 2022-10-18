738cdbb72342e8da1e6c50c4fa48c9c412848b9f
jjangyeunii <euneun0927@naver.com>
2022-10-18 14:51:14 +0900

    Bare Minimum 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const discussionIddArr = discussionsData.map((discussion) => discussion.id);
+    // console.log(discussionIddArr);
+    if (id in discussionIddArr) {
+      const discussionRequestData = discussionsData.find(
+        (discussion) => discussion.id === Number(id)
+      );
+      return res.status(200).send(discussionRequestData);
+    } else {
+      return res.status(404).send("Not found");
+    }
+  },
 };
 
 module.exports = {
