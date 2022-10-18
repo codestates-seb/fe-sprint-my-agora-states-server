9e257909f038073e77948d25d4b6a2ba382c9b33
eunji <omegle3333@gmail.com>
2022-10-18 22:38:32 +0900

    최종 수정

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const { id } = req.params;
     
-    res.send('TODO:')
+    const data = discussionsData.filter(el => {
+      return el.id === Number(id);
+    });
+    if(data.length === 0) {
+      return res.status(404).send('empty');
+    } else {
+      res.json(data[0]);
+    }
   }
-
 };
 
 module.exports = {
