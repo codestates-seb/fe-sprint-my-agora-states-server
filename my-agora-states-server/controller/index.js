91af464e0ea3a300acaab59ce1bd79c7876785e0
Double.Pos <wks1945@naver.com>
2022-10-18 10:42:43 +0900

    과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,26 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    let resBody = discussionsData;
+    const { limit, pages } = req.query;
+    if (!limit && !pages) {
+      return res.status(200).json(resBody);
+    }
+    return res.status(404).send('Not Found!');
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const found = discussionsData.find((d) => d.id === Number(id));
+    if (found) {
+      return res.status(200).json(found);
+    }
+    return res.status(404).send('Not Found!');
+  },
 };
 
 module.exports = {
