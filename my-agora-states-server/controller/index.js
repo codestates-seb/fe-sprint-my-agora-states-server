06028692005a914e25032a93964942a71f8d0e7a
ashleysheo <ashleysyheo@gmail.com>
2022-10-18 11:57:42 +0900

    complete: bare minimal requirements

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,14 +3,19 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    const filteredDiscussion = discussionsData.find((discussion) => discussion.id === Number(id));
+
+    if (filteredDiscussion) {
+      res.status(200).json(filteredDiscussion);
+    } else {
+      res.status(404).send('Not Found');
+    }
   }
 
 };
