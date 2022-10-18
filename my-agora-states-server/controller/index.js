7db9421275145061c766809a6195e06bad235224
Yeon <yeonhwan619@gmail.com>
2022-10-18 10:56:41 +0900

    bare-minimum passed

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params;
+    const resData = discussionsData.filter((v) => {
+      return v.id === Number(id);
+    });
+
+    if(resData.length) {
+      res.json(resData[0]);
+    } else {
+      res.status(404).send('Bad Request, ID is unknown');
+    }
   }
 
 };
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,6 +5,13 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
+router.get('/', (req, res) => {
+  findAll(req, res);
+});
+
+router.get('/:id', (req, res) => {
+  findById(req, res);
+})
 
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
