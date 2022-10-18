813669ef440f95c582c116f750fdb38c70353e5b
atia <hckr1994@gmail.com>
2022-10-18 13:49:48 +0900

    feat: complete bare minimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,15 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const _data = discussionsData.find(el => (el.id === Number(req.params.id)));
+    if (!_data) res.status(404).send('일치하는 아이디가 존재하지 않습니다.');
+    else res.status(200).json(_data);
   }
-
 };
 
 module.exports = {
