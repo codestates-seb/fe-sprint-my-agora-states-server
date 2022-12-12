634e48f617bc02799d1b4ab16c82c47d3c522a63
tjddmssl <tjddmssl910103@gmail.com>
2022-12-13 00:02:10 +0900

    과제 문제 풀었음

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
+    res.send("TODO:");
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const filtered_data = discussionsData.filter(
+      (item) => item.id === Number(id)
+    );
+    if (filtered_data.length === 0) {
+      return res.status(404).send();
+    } else {
+      return res.status(200).send(filtered_data[0]);
+    }
+    res.send("TODO:");
+  },
 };
 
 module.exports = {
