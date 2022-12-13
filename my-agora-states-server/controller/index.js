566a5876374588c525f79b2fa26fdc2a8df38eaa
SinHyeoonmin <tlsgusals125@naver.com>
2022-12-13 16:03:24 +0900

    test

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const {id} = req.params;
+    let filteredDiscussions = discussionsData.filter( (el) => el.id === Number(id) )
+    
+    if (filteredDiscussions.length === 0) {
+      return res.status(404).send('Not Found');
+    } else {
+      return res.status(200).json(filteredDiscussions[0]);
+    }
+}
 
 };
 
