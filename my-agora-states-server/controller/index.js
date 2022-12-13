5c659f8c6db3b77bf8f4d1d60ee3eaf97fd139aa
soohyuneee <tlstngus95@naver.com>
2022-12-13 14:45:18 +0900

    완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,20 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const {agoraStatesDiscussions} = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const {id} = req.params;
+    const data = discussionsData.find((discussion) => discussion.id === Number(id));
+    if (data) {
+      return res.send(data);
+    } else return res.status(404).send("Incorrect request");
+  },
 };
 
 module.exports = {
