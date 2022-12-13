328ef215d080a38325c187ebbd05eec88b18413b
chanheek <toadfwe1@naver.com>
2022-12-13 15:54:15 +0900

    제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,16 +1,34 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+const { agoraStatesDiscussions } = require("../repository/discussions");   //  = [{...}]
+const discussionsData = agoraStatesDiscussions;   // {[{....}]}
+
+
+
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     
-    res.send('TODO:')
+    const { id } = req.params;
+
+    let data = discussionsData.find((el) => {
+      return el.id === Number(id)
+    })
+
+    // let data = discussionsData.filter((el) =>{
+    //   return req.params.id === el.id
+    // })                                                      // data = 
+
+    if (data) {
+      return res.status(200).json(data)
+    } else  {                                          // 안넘어감
+        return res.status(404).json('xxx');
+      }
+
   }
 
 };
