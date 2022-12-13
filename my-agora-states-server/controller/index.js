1334a514668ac36c9ebd14fbdae30ba7ed0a0428
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2022-12-13 15:38:48 +0900

    feat: 모든 기능 구현 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const data = discussionsData.filter((data) => {
+      return +id === +data.id;
+    });
+    console.log("data", data.length);
+    if (data.length === 0) {
+      return res.status(404).send("일치하는 데이터가 없습니다");
+    }
+    res.status(200).json(data[0]);
+  },
 };
 
 module.exports = {
