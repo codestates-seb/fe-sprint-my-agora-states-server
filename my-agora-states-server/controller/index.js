2b1ed871fae558689a36616f579bcb191b4d0c28
sleepyjoyy <sleepyjoyy@gmail.com>
2022-12-13 11:47:52 +0900

    Bare minimum chk

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,21 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // TODO: 모든 discussionsr 목록을 응답합니다.
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params;
+    let filteredById = discussionsData.find((d)=> d.id === Number(id));
+    if(filteredById){
+      return res.status(200).json(filteredById);
+    }else 
+      return res.status(404).send('Not Found');
   }
-
 };
 
 module.exports = {
