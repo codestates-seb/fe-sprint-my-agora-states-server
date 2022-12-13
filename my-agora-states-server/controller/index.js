045aea38b6932e876eaf4316fd081ce4637b29c1
YUNH7 <yunny126@gmail.com>
2022-12-13 16:32:31 +0900

    서버연결 과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params
+    const filtered = discussionsData.filter((discussion) => discussion.id === Number(id))
 
+    if (filtered.length === 0) {
+      res.status(404).send('일치하는 데이터가 없습니다')
+    } else {
+
+      res.json(...filtered)
+    }
+  }
 };
 
 module.exports = {
