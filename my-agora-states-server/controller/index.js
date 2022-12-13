7018364efa57c5f92c062af80de9fda646773643
minjusong <mindyumandyu@gmail.com>
2022-12-13 14:57:10 +0900

    complete

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,26 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(agoraStatesDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} = req.params;
+    let list = agoraStatesDiscussions;
+    if (id) {
+      list = agoraStatesDiscussions.filter((el)=>{
+        return el.id === Number(id)
+      })
+      return list.length 
+      ? res.status(200).json(list[0])
+      : res.status(404).end()
+  
+    }
+    }
   }
 
-};
+
 
 module.exports = {
   discussionsController,
