767379e592349a92fc736161c07ebefeb72f16cb
hongjungi94 <92861329+hongjungi94@users.noreply.github.com>
2022-12-13 14:44:16 +0900

    SEB_FE_42_홍준기 과제 제출합니다

---
+++ my-agora-states-server/my-agora-states-server/controller/index.js
@@ -0,0 +1,28 @@
+const { agoraStatesDiscussions } = require("../repository/discussions");
+const discussionsData = agoraStatesDiscussions;
+
+const discussionsController = {
+  findAll: (req, res) => {
+    // TODO: 모든 discussions 목록을 응답합니다.
+    res.status(200).json(discussionsData);
+  },
+
+  findById: (req, res) => {
+    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    
+    const { id } = req.params;
+    const findById = discussionsData.find((el) => {
+      return el.id === parseInt(id);
+    });
+    if (findById !== undefined) {
+      res.status(200).json(findById);
+    } else {
+      res.status(404).json({ message: 'Not Found' });
+    }
+  },
+
+};
+
+module.exports = {
+  discussionsController,
+};
