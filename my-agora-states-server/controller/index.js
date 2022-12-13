aee4a0b0e72671c784eb8681e454d31bff76b4e0
mikjk97 <mikjk0530@gmail.com>
2022-12-13 14:24:06 +0900

    SEB_FE_42_김준경 과제 제출합니다

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -13,11 +13,10 @@ const discussionsController = {
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const {id} = req.params;
-    const data = discussionsData.filter(discussion=>{
-      return discussion.id = Number(id);
-    })
+    const data = discussionsData.filter((discussion)=>{
+      return Number(id) === discussion.id });
     if(data.length === 0){
-      return res.status(400).json('Not found')
+      return res.status(404).send('Not found')
     }else{
       return res.status(200).json(data[0])
     }
