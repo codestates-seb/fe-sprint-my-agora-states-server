cf97494b120c23155dc50e42f08f17950d2d7c5b
heyroocoke <heyroocoke@gmail.com>
2022-12-13 13:45:00 +0900

    modify

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,26 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const { id } = req.params; // req.params.id === id
+    // const id = req.params.id --> 얘랑은 틀린말임
+    let list = discussionsData;
+
+    if(id){
+      list = list.filter((e) => e.id === Number(id))
+      if(list.length > 0){
+        return res.status(200).json(list[0])
+      }else{
+        return res.status(404).send()
+      }
     
-    res.send('TODO:')
+      // list = list.filter((e) => e.id !== req.params.id)
+    }
+    
   }
 
 };
