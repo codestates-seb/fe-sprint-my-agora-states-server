43f5be7d403b9f3ace361e9a9ac7ab3e98d14eae
ByeongminBRO <106019251+ByeongminBRO@users.noreply.github.com>
2022-12-13 16:36:41 +0900

    Update index.js

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,12 +3,12 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
+
     return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+
    const {id}= req.params
    const a = discussionsData.find((b) => b.id === Number(id));
    
@@ -18,7 +18,6 @@ const discussionsController = {
     return res.status(404).end();
    }
   }
-
 };
 
 module.exports = {
