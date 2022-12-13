5258603b723221ca6901cbdeaee2571acc30c1bb
sujin <sjij0617@naver.com>
2022-12-13 16:57:59 +0900

    second

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -6,16 +6,16 @@ const discussionsController = {
     // TODO: 모든 discussions 목록을 응답합니다.
     console.log(discussionsData);
     
-    res.json(discussionsData)
+    return res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const {id} = req.params
-    console.log(typeof( id));
-     const data = agoraStatesDiscussions.filter(el=>el.id === parseInt(id))
-     console.log(data);
-    res.json(data)
+    console.log(typeof(id))
+     const data = discussionsData.filter(el=>el.id === parseInt(id))
+  //  console.log(data);
+    return res.status(200).json(data)
   }
 
 };
