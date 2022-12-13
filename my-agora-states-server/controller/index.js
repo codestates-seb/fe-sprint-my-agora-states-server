c841b92366feaefa00ef61ece41fa9d84fb890fb
ok-soosoo <doub22e@gmail.com>
2022-12-13 16:11:53 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,30 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const { id } = req.params;
+    // console.log(typeof id) // string 
     
-    res.send('TODO:')
-  }
+    const filteredid = discussionsData.filter((data) => {
+      // console.log(typeof data.id) // number
+      // console.log(data.id === Number(id)) // number number  > true
+      return data.id === Number(id)})
+      // if (data.id !== Number(id)) { return res.status(404).send(`Invalid id >> ${id}`)}
+      // console.log(filteredid)
+      if (filteredid.length === 0) {
+        return res.status(404).send(`Invalid id >> ${id}`)
+      } else {
+        return res.status(200).send(filteredid[0])}
+      }
+
+          
+      
+
+    
 
 };
 
