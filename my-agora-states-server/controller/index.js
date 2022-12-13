13fa289d998b281eb6ad230e3cafae72f7384a32
jellyjw <wldnzzz@naver.com>
2022-12-13 10:07:28 +0900

    과제완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { uuid } = req.params;
 
+    const filtered_data = discussionsData.filter(
+      (item) => item.id === Number(uuid)
+    );
+    console.log(filtered_data[0]);
+    if (filtered_data.length === 0) {
+      return res.status(404).send("error!!");
+    } else {
+      return res.status(200).json(filtered_data[0]);
+    }
+  },
 };
 
 module.exports = {
