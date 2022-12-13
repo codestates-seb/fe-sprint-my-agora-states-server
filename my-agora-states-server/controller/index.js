936c40dcc3b94fa1471bab9e79b511998d085b40
nayoung <kny_7_@naver.com>
2022-12-13 15:34:20 +0900

    SEB_FE_42_김나영과제제출합니다

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,16 +4,27 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    
+    res.status(200).send(agoraStatesDiscussions)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     
-    res.send('TODO:')
+    
+    const {id}=req.params;
+    let list=agoraStatesDiscussions;
+    if(id){
+      list=agoraStatesDiscussions.filter((el)=>{
+        return el.id===Number(id)})
+        return list.length
+        ? res.status(200).json(list[0])
+        : res.status(404).end()
+      }
+    }
   }
 
-};
+
 
 module.exports = {
   discussionsController,
