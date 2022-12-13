96b785d94dd0c0cf702a7b42b9deab175cd4d963
Andy <andy_0805@naver.com>
2022-12-13 11:31:31 +0900

    '완성'

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,15 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const result = discussionsData.find(el => el.id === Number(id))
+
+    return result ? res.status(200).json(result) : res.status(404).send('Not found')
   }
 
 };
