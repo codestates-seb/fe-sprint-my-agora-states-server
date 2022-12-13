ede3f2a386b14591d9230bd7c38b67395d6fd280
yjb048 <115616389+yjb048@users.noreply.github.com>
2022-12-13 15:33:52 +0900

    테스트 통과

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,24 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+const discussionsData = agoraStatesDiscussions; //배열
 
 const discussionsController = {
+
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const {id} = req.params;
+    let filteredDiscussions = discussionsData.filter( (el) => el.id === Number(id) )
     
-    res.send('TODO:')
-  }
+    if (filteredDiscussions.length === 0) {
+      return res.status(404).send('Not Found');
+    } else {
+      return res.status(200).json(filteredDiscussions[0]);
+    }
+}
 
 };
 
