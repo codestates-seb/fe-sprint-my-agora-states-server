97db613024af854ae7bc66b9f1f2f766a610c11a
lunadein2022 <lunadein2022@gmail.com>
2022-12-13 14:26:52 +0900

    조수빈

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    let resid = discussionsData.filter((el) => {
+      return el.id === Number(id);
+    });
+    if (resid.length !== 0) {
+      return res.status(200).json(resid[0])
+    } else {
+      return res.status(404).json('noooooooo!')
+    }
   }
-
 };
 
 module.exports = {
