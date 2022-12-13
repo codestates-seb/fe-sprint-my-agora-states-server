8d72c2c701e81f486073d28c7dfdf38f4a82c0b8
dding-v <115635503+dding-v@users.noreply.github.com>
2022-12-13 16:18:59 +0900

    Update index.js

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -8,9 +8,15 @@ const discussionsController = {
   },
 
   findById: (req, res) => {
-
+    const { id } = req.params;
+    let data = discussionsData.filter(item => item.id === Number(id));
+    
+    if(data.length !== 0) {
+      return res.status(200).json(data[0]);
+    } else {
+    return res.status(404).end
+    }
   }
-
 };
 
 module.exports = {
