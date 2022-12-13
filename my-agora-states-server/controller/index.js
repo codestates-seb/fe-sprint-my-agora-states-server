a7bf97be5ec1e3fca85da1b412dbda07de934b9d
sienna0715 <5609373@naver.com>
2022-12-13 12:16:30 +0900

    서버 생성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    
+    // find() 주어진 판별 함수를 만족하는 첫 번째로 찾은 요소의 값을 반환한다.
+    // 왜 안될까 고민하다보니 req.params가 문자열로 들어온다는 사실을 발견했다..!
+    const data = discussionsData.find(el => el.id === Number(id));
+    return data ? res.status(200).json(data) : res.status(404).send("Not found.");
+  },
 };
 
 module.exports = {
