c11287ec134ee86a248950e23e1de5f9f902b564
BRWEB <52031484+rockbell89@users.noreply.github.com>
2022-12-13 16:38:39 +0900

    나만의 아고라 스테이츠 서버 만들기 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const data = discussionsData;
+    console.log("findAll", data);
+    return res.status(200).send(data);
+    // res.send('TODO:')
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const data = discussionsData.filter((data) => data.id === id);
+    console.log("findById", data);
+    return res.status(200).send(data);
+    // res.send('TODO:')
+  },
 };
 
 module.exports = {
