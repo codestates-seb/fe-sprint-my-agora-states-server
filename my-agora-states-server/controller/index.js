28aeefc355f2ba95b8f21a5091a22ec8369c4c51
es39 <esj10025@gmail.com>
2022-12-13 13:35:33 +0900

    index.js modify

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -11,10 +11,12 @@ const discussionsController = {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params
     let list = discussionsData.filter((data) => Number(id) === data.id)
-    if(list.length === 0){
-      return res.status(404).json('Not Found') 
-    } else {
-      return res.status(200).json(list[0])
+    if(id){
+      if(list.length === 0){
+        return res.status(404).json('Not Found') 
+      } else {
+        return res.status(200).json(list[0])
+      }
     }
   }
 };
