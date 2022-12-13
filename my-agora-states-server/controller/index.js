25f4a50379194fd7b6ca0ea1ca9bf0ef319d10b1
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2022-12-13 17:57:32 +0900

    Update index.js

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,3 +1,5 @@
+const { agoraStatesDiscussions } = require("../repository/discussions");
+const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
