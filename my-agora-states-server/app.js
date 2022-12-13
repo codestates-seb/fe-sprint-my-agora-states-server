1605e24b0d83dfb48d66a575037b2bd6c055c6d1
EunjiLee1997 <eunjilee1111@gmail>
2022-12-13 13:08:13 +0900

    Bare Minimum Requirement 완료

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -8,15 +8,19 @@ const morgan = require('morgan');
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
 app.use(morgan('tiny'));
 
-// TODO: cors를 적용합니다.
+/// TODO: cors를 적용합니다.
+app.use(cors());
 
-// TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-
+/// TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+app.use(express.json({ strict: false }));
 
 const port = 4000;
 const discussionsRouter = require('./router/discussions');
+const { json } = require('express');
+const { addHook } = require('dompurify');
 
-// TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
+/// TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다.
+app.use('/discussions', discussionsRouter);
 
 
 app.get('/', (req, res) => {
