488257bd860051d1a5d3c897da79dfd2e9438689
KimYeongIm0 <ygim4029@gmail.com>
2022-12-13 12:25:37 +0900

    테스트통과완

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,15 +5,21 @@ const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
     const data = discussionsData.slice();//복사
-    
     return res.status(200).json(data);
     
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const { id } = req.params;
+    const list = discussionsData.find((item) => item.id === Number(id) )
+    if(list) {
+      return res.status(200).json(list);
+    }
+    else{
+      return res.status(404).json('Incorrect request');
+    }
     
-    res.send('TODO:')
   }
 
 };
