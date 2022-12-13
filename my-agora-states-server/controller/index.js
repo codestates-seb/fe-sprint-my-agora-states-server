9839abb87af619c3fe8e7dd795699203d9f47fe7
12ayo22 <12ayo22@gmail.com>
2022-12-13 09:27:59 +0900

    [feat] discussion 라우터 컨트롤러 기능

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,27 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const params = req.params;
+    const { id } = params;
+    const data = discussionsData.find(
+      (discussion) => parseInt(discussion.id, 10) === parseInt(id, 10)
+    );
 
+    // 데이터가 없는 경우
+    if (!data) {
+      res.status(400).send('Bad Request');
+      return;
+    }
+    res.json(data);
+  },
 };
 
 module.exports = {
