db861e3494f28935a45699e5ef2eabcfbf206f12
leeyongwon <33643585+LeeYongWon@users.noreply.github.com>
2022-12-13 14:37:05 +0900

    SEB_FE_42_이용원 과제제출합니다

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    console.log(req)
+    return res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params
+    const result = discussionsData.filter(el=>el.id===Number(id))
+    if(result.length===0){
+      return res.status(404).send()
+    }else {
+      return res.status(200).send(result[0])
+    }
   }
 
 };
