ce39f52e7fee80da816b2d1c2a0b2dfc26de07fd
Rose Jang <jangmi749@gmail.com>
2022-12-13 11:44:29 +0900

    모든 과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,43 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+const { agoraStatesDiscussions } = require('../repository/discussions');
+let discussionsData = [...agoraStatesDiscussions];
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    let filteredDiscussions = [...discussionsData];
+    if (id) {
+      filteredDiscussions = discussionsData.filter((discussion) => {
+        return discussion.id === Number(id);
+      });
+    }
+    if (filteredDiscussions.length === 0) {
+      return res.status(404).json('Not found');
+    } else {
+      return res.status(200).json(filteredDiscussions[0]);
+    }
+  },
 
+  // * (과제 아님) 리액트 아고라 스테이츠 앱에서 새로 질문 등록을 위해 추가
+  create: (req, res) => {
+    const newQuestion = {
+      id: new Date().getTime(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      url: '#',
+      answer: null,
+      avatarUrl: 'https://www.sirarchibald.dev/unnamed.jpg',
+      ...req.body,
+    };
+
+    discussionsData.unshift(newQuestion);
+    return res.status(201).json(discussionsData);
+  },
 };
 
 module.exports = {
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,13 +1,16 @@
 // TODO: discussions 라우터를 완성합니다.
 const { discussionsController } = require('../controller');
-const { findAll, findById } = discussionsController;
+const { findAll, findById, create } = discussionsController;
 const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
+router.get('/:id', findById);
 
+// * (과제 아님) 리액트 아고라 스테이츠 앱에서 새로 질문 등록을 위해 추가
+router.post('/', create);
 
 module.exports = router;
