aef0efcae1c23e0b749943459c90db5d5089df91
shinchangyoung <gojj294@gmail.com>
2022-12-13 14:29:27 +0900

    나만의 아고라츠 서버과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,15 +1,46 @@
+const { identity } = require("underscore");
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    // const {id} = req.params;
+    // let filDisData = discussionsData.filter(discussion => {
+    //   return discussion.id === Number(id);
+    // })
+    // if(filDisData.length === 0){
+    //   return res.status(404).json(`${id} is not found` )
+    // }else{
+    //   return res.status(200).json(filDisData[0])
+    // }
+   
+    const { id } = req.params;
+    // 같은 id 값 걸러준 친구
+    let filDisData = discussionsData.filter((item)=>{
+      return item.id === Number(id) ;
+    })
+    if(filDisData.length === 0){
+      return res.status(404).json(` ${id} is not found`)
+    }else{
+      return res.status(200).json(filDisData[0])
+    }
+    // res.send(discussionsData.filter(()=>{
+    //   let pass = true
+    //   if (pass = pass && (discussionsData === discussionsData.id)){
+    //     return pass 
+    //     console.log(pass)
+        
+    //   }
+    //   if (pass = pass && (discussionsData === undefined)){
+    //     return res.status(404).json('에러')
+    //   }
+    // }))
   }
 
 };
