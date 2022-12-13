7d1a09d85d78504b2697f14ebeb2255a7e0ca6e1
CHOISANAE <tksdo96@DESKTOP-Q6R0VVA.localdomain>
2022-12-13 16:10:12 +0900

    SEB_FE_42_최산애 과제 제출합니다.

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,15 +1,24 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
+const { all } = require("../router/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(agoraStatesDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    let filteredDiscussion =[];
+    console.log(req.params.id);
+    if(req.params.id) {
+      filteredDiscussion = discussionsData.filter((el) => {
+        return el.id === Number(req.params.id);
+      });
+    if (filteredDiscussion.length > 0) res.send(filteredDiscussion[0]);
+    else res.status(404).send("일치하는 데이터가 없습니다.");
+    }
   }
 
 };
