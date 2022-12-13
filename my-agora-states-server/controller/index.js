1c935bc6728ff9713265d79b65662f6db3d69d54
jeongminsang <101001956+jeongminsang@users.noreply.github.com>
2022-12-13 15:58:10 +0900

    과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,31 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
+    // res.send('TODO:')
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} = req.params;
+    const { title, url, author, bodyHTML, avatarUrl } = req.query;
+    // if(id !== undefined){
+    //   const discussionlist = discussionsData.filter(discussion => id === discussion.id)
+    //   return res.status(200).json(discussionlist);
+    // }
+    console.log(typeof id)
+    const discussionlist = discussionsData.filter(discussion => {
+      return Number(id) === discussion.id})
+    if(discussionlist.length === 0){
+      return res.status(404).json('Not Found!');
+    }
+    else{
+      return res.status(200).json(...discussionlist);
+    }
+      
+    
+    
+    // res.send('TODO:')
   }
 
 };
@@ -17,3 +36,4 @@ const discussionsController = {
 module.exports = {
   discussionsController,
 };
+ 
