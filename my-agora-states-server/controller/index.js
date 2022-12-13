9a27824402363e3156ea908d7629bfeee6bdef9d
Jung Su <wjdwjdtn92@naver.com>
2022-12-14 00:40:10 +0900

    feat: findAll add offset,limit query params and api add get count

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,10 +1,23 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+let discussionsData = agoraStatesDiscussions;
+const { v4: uuid } = require("uuid");
 
 const discussionsController = {
   findAll: (req, res) => {
+    const { offset, limit } = req.query;
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.status(200).json(discussionsData);
+    console.log(offset, limit);
+
+    if (offset && limit) {
+      const showData = discussionsData.slice(
+        (offset - 1) * limit,
+        (offset - 1) * limit + Number(limit)
+      );
+
+      res.status(200).json(showData);
+    } else {
+      res.status(200).json(discussionsData);
+    }
   },
 
   findById: (req, res) => {
@@ -20,6 +33,23 @@ const discussionsController = {
       return;
     }
     res.status(200).json(...filteredDiscussions);
+  },
+
+  create: (req, res) => {
+    const id = uuid();
+    const data = req.body;
+    const newDiscussionsData = {
+      id,
+      ...data,
+    };
+
+    discussionsData = [newDiscussionsData, ...discussionsData];
+
+    res.status(201).json(newDiscussionsData);
+  },
+
+  count: (req, res) => {
+    res.status(200).json(discussionsData.length);
   },
 };
 
