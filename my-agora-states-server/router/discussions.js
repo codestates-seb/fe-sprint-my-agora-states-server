5d0ea77219f2fc61ca26ca597ecac63c7347584b
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2022-12-13 17:58:15 +0900

    Update discussions.js

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,3 +1,6 @@
+const { discussionsController } = require('../controller');
+const { findAll, findById } = discussionsController;
+const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
