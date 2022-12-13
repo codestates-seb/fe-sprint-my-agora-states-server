7c2948d2b2e6265cc08fa945902b5fc15c86fa02
ofcpire <ofcpire@gmail.com>
2022-12-13 16:08:01 +0900

    끝

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,15 +1,23 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
+const express = require('express')
+const app = express();
+app.use(express.json());
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id}=req.params;
+    filteredDiscussionsData=discussionsData.filter((discussion)=>{
+      return discussion.id===Number(id);
+    })
+    if(filteredDiscussionsData.length>0)
+    return res.status(200).json(filteredDiscussionsData[0]);
+    else return res.status(404).send();
   }
 
 };
