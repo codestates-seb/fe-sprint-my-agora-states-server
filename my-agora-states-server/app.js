dd5ebde0dd127fafd2449cec95be013c47a25adc
dding-v <115635503+dding-v@users.noreply.github.com>
2022-12-13 16:01:55 +0900

    Update app.js

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -4,7 +4,6 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 
-
 // morgan 미들웨어가 세팅되어 있습니다.
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
 app.use(morgan('tiny'));
@@ -20,7 +19,6 @@ const discussionsRouter = require('./router/discussions');
 
 // TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. //
 app.use('/discussions', discussionsRouter)
-
 
 app.get('/', (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
