3f35e6274d2e943d6bd2cc93adb4b56da971aad8
journey0321 <yjyjh0321@gmail.com>
2022-12-13 22:15:52 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,28 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+const { agoraStatesDiscussions } = require('../repository/discussions');
+let discussionsData = [...agoraStatesDiscussions];
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // res.send('TODO:')
+    return res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    let filteredDiscussions = [...discussionsData];
+    if(id) {
+      filteredDiscussions = discussionsData.filter((discussion) => {
+        return discussion.id === Number(id);
+      });
+    }
+    if (filteredDiscussions.length === 0) {
+      return res.status(404).json('Not found');
+    } else {
+      return res.status(200).json(filteredDiscussions[0]);
+    }
   }
-
 };
 
 module.exports = {
