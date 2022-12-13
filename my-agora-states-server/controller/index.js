a68ef07bb142cc19207f81cf79829eefbe1cf4a0
suming <ipower_overi@naver.com>
2022-12-13 16:19:07 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,22 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    let filteredData = discussionsData.filter((el) => el.id === Number(id));
+    if (filteredData.length !== 0) {
+      res.status(200).send(filteredData[0]);
+    } else {
+      res.status(404).send('id와 일치하는 데이터가 없습니다');
+    }
+  },
 };
 
 module.exports = {
