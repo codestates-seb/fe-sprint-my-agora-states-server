7354e896ca1df733e222b5fd26293e0aa8b0fa22
jinaSE0 <tiffany6771@gmail.com>
2022-12-13 15:43:12 +0900

    fetch제외하고완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,24 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    if(Object.keys(req.query).length===0){
+      return res.status(200).json(discussionsData)
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} = req.params
+    //디스터션데이터에서일치하는것을 필터해서 저장해주고 그것을 리턴해줌
+    let filteredData = discussionsData.filter((el)=>{
+      return el.id === Number(id)
+    })
+    if(filteredData.length===0){
+      return res.status(404).json("찾는 아이디 음슴")
+    }else{
+      return res.status(200).json(filteredData[0])
+    }
+    
   }
 
 };
