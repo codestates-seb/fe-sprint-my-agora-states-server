6cf34ab66c9269b2f91e8af5e8da5d01b8472c11
benidene <dlgusehd09@naver.com>
2022-12-13 13:24:11 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,23 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
+
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const {id} = req.params;
+      const filteredData = discussionsData.filter(el => el.id === Number(id) )
+      if(filteredData.length === 0) {
+        res.status(404).json("Not found")
+      }else{
+        res.status(200).json(filteredData[0])
+    }
   }
-
 };
 
 module.exports = {
