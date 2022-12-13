63e33d82a69be38ce7b3b4dac8f9b7eb485a79f1
jetty8013 <jetty8013@gmail.com>
2022-12-13 15:50:40 +0900

    과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params
+    const filterById = discussionsData.filter(data => {
+      if(Number(id) === data.id) return data
+    })
+    if(filterById.length === 0){
+      res.status(404).send('Not found')
+    }
+    else {
+      res.status(200).json(...filterById)
+    }
   }
 
 };
