10df8d3c422d88ab4eb5cba97b8283c29a54bcce
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2022-12-13 16:32:29 +0900

    미완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,11 +4,12 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    console.log(req.param)
+    let filteredData = discussionsData.filter()
     res.send('TODO:')
   }
 
