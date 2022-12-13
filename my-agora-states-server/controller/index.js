472f19482c00ba1837ad0bdd91ba469ec6104823
yeojin15 <k.yeojin15@gmail.com>
2022-12-13 13:00:39 +0900

    과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const { id } = req.params;
+    const filtered = discussionsData.filter((item)=> item.id===parseInt(id))
+    if(filtered.length === 0){
+      return res.status(404).send('error') // ???? 404를 왜 먼저 써줘야 하지???
+    }else{
+      return res.status(200).send(filtered[0])
+    }
+
   }
 
 };
