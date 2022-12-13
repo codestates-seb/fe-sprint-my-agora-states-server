b812890e93e3e5f4c0a26d4bd542686a91ec1625
김지아 <gimjia@gimjiaui-MacBookAir.local>
2022-12-13 22:05:22 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,26 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // res.send('TODO:')
+    //return res.state(200).json(agoraStatesDiscussions);
+    res.send(agoraStatesDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    //res.send('TODO:')
+    const { id } = req.params;
+    if(id) {
+      let date = discussionsData.filter((el)=> {
+        return Number(id) === el.id;
+      });
+      if(date.length===0) {
+      return res.statu(200).josn({state: false});
+      }else {
+        return res.statu.josn(...date)
+      }
+    }
+  },
 };
 
 module.exports = {
