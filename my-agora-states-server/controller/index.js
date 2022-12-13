5698ccbd5937e64e35bb2c36494725a22e9bdfd7
Yoonable <yyw241@gmail.com>
2022-12-13 14:33:46 +0900

    과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    
-    res.send('TODO:')
+    const{ id } = req.params;
+    const filtered = discussionsData.filter((ele) => {
+      return ele.id === Number(id)
+    })
+    if(filtered.length === 0){
+      return res.status(404).json({})
+    } else {
+      return res.json(filtered[0])
+    }
   }
-
 };
 
 module.exports = {
