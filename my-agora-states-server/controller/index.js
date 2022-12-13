5128234a96a17ec7e2a1f93203b3afb4541bda34
josephgs-koo <josephgs.koo@gmail.com>
2022-12-13 16:39:04 +0900

    완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -11,11 +11,11 @@ const discussionsController = {
     findById: (req, res, next) => {
         // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
         const { id } = req.params;
-        const resBody = discussionsData.filter((x) => x.id === Number(id));
-        if (resBody.length === 0) {
+        const resBody = discussionsData.find((x) => x.id === Number(id));
+        if (!resBody) {
             return next();
         }
-        return res.status(200).json(resBody[0]);
+        return res.status(200).json(resBody);
     },
 };
 
