b538dba1d4b96d99ce8fd8a138058528ba4743db
jiyongnoh <115959559+jiyongnoh@users.noreply.github.com>
2022-12-13 16:42:06 +0900

    my-agora-states-server ver 1.0

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,16 +1,26 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
-
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const {id} = req.params;
     
-    res.send('TODO:')
+    let fiteredIndex = discussionsData.findIndex(discussion => {
+      return Number(id) === discussion.id;
+    })
+    
+    if(fiteredIndex !== -1){
+      return res.status(200).json(discussionsData[fiteredIndex]);
+    }
+    else{
+      return res.status(404).json();
+    }
+    
   }
 
 };
