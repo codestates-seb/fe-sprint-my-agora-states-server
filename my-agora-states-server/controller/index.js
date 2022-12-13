c737efe7cb13e2a85078742f6fd491d2e666c078
leechiyun <wychis28@gmail.com>
2022-12-13 12:06:37 +0900

    my agora server

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    let { id } = req.params;
+    id = Number(id);
+
+    if (id) {
+      const finded = discussionsData.find((item) => {
+        return id === item.id;
+      })
+      if (finded) {
+        return res.status(200).json(finded);
+      }
+      return res.status(404).send('Not Found!');
+    }
   }
 
 };
