bfc2b2d832fbff5d27546cc11cd9ce09c3b4092b
chae030 <ychae1997@gmail.com>
2023-02-08 13:12:41 +0900

    feat: 요청받은 id 가진 discussion 출력

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,6 +9,10 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const id = Number(req.params.id)
+    const [ discussion ] = discussionsData.filter(data => data.id === id)
+    if(discussion) res.status(200).json(discussion);
+    else res.status(404).end();
   }
 };
 
