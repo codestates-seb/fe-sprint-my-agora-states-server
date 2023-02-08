d8049fe8e5922837b93064177a06fa550573d526
cheesepizza453 <earlykim1209@gmail.com>
2023-02-09 01:10:10 +0900

    베어미니멈 완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    //res.send('TODO:')
+    const { id } = req.params;
+    const data = discussionsData.find((a) => a.id === Number(id));
 
+    //성공일 때 상태코드 200, 실패 404 보내주고
+    if (data) return res.status(200).json(data);
+    else return res.status(404).json("404!");
+  },
 };
 
 module.exports = {
