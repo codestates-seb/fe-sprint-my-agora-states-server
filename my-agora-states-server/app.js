e16acab3e221adc4474082af722b76a6cb92841e
hongyunsu <dbstn9@hanmail.net>
2023-02-09 00:13:10 +0900

    bareminimum success

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -1,31 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 
 // morgan 미들웨어가 세팅되어 있습니다.
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 // TODO: cors를 적용합니다.
+app.use(cors());
 
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-
+app.use(express.json({ strict: false })); // fetch할때 body에 데이터넣는데 json은 문자열로만들어진형태 , 객체랑 데이터는 따옴표안넣는데 json이 // 배열이나 객체형태로 보내면 언제나 파싱되는데, 문자열형태로 보내면 json파일인데 스트릭트가 true가 되어있으면 body데이터가 아니라고 하는것.
 
 const port = 4000;
-const discussionsRouter = require('./router/discussions');
+const discussionsRouter = require("./router/discussions");
 
-// TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
+// TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다.
+app.use("/discussions", discussionsRouter);
 
-
-app.get('/', (req, res) => {
-  // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
-  res.status(200).send('fe-sprint-my-agora-states-server');
+app.get("/", (req, res) => {
+	// 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
+	res.status(200).send("fe-sprint-my-agora-states-server");
 });
 
 const server = app.listen(port, () => {
-  console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
+	console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
 });
 
 module.exports.app = app;
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -2,18 +2,39 @@ const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
-  findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
-  },
+	findAll: (req, res) => {
+		// TODO: 모든 discussions 목록을 응답합니다.
+		res.send(discussionsData); //??? 이게 맞나 여기 TODO:
+	},
 
-  findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+	findById: (req, res) => {
+		// TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+		// res.send("TODO:");
 
+		// discussions.js id가져오려면 params사용하면 된다.
+		// api/posts/:id라는 라우터경로가 있을때, id는 req.params.id로 불러올 수 있다.
+		// req객체에 parameter라는 프로미터가 있고, 그 프로퍼티의 id라는 프로퍼티로 접근해 요청을 보낼 수 있는 것
+		const { id } = req.params;
+
+		// find 새로운 변수 선언 >> 데이터가 있는 discussionsData안에서 같은 id를 find로 찾기
+		const data = discussionsData.find((a) => a.id === Number(id));
+
+		// 성공 상태코드 200, 모든 목록을 담은 repository 폴더 안의 discussions 목록을 담은 discussionsData를 그대로 return한다.
+		if (data) return res.status(200).json(data);
+		else return res.status(404).json("Not found");
+		// 실패 상태코드 404
+
+		// 이것도 통과된다
+		// const data = discussionsData.filter((a) => a.id === Number(id));
+
+		// if (data.length === 0) {
+		// 	return res.status(404).json("Not found");
+		// } else {
+		// 	return res.status(200).json(data[0]);
+		// }
+	},
 };
 
 module.exports = {
-  discussionsController,
+	discussionsController,
 };
