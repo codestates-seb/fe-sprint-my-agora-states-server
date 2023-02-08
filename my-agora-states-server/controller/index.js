87e4735b7ab1f2e01d9cf9325189de2c64829493
hihijin <117073214+hihijin@users.noreply.github.com>
2023-02-08 19:17:27 +0900

    과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,43 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    const idData = discussionsData.filter(v => String(v.id) === String(id)); // id가 숫자이거나 문자일 수 있으므로 둘다 number타입으로 변경시킨다.
+    if(idData.length>0) res.status(200).json(idData[0]);
+    else res.status(404).send("Not Found!");
+  },
+/*
+  create: (req, res) => {
+    const newQuestion = {
+      id: new Date().getTime(), //랜덤 숫자로 고유한 id 설정
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      url: '#',
+      answer: null,
+      avatarUrl: "https://play-lh.googleusercontent.com/38AGKCqmbjZ9OuWx4YjssAz3Y0DTWbiM5HB0ove1pNBq_o9mtWfGszjZNxZdwt_vgHo=s200",
+      ...req.body,
+    };
 
+    discussionsData.unshift(newQuestion); //새로운 객체를 discussiondata 배열 제일 앞에 붙인다.
+    return res.status(201).json(discussionsData);
+    //discussionsData.unshift(req.body);
+    //res.status(301).json(req.body);
+  },
+
+  deleteById: (req, res) => {
+    const {id} = req.params;
+    const idx = discussionsData.findIndex((v) => {
+      return v.id === Number(id)
+    });
+    discussionsData.splice(idx,1);
+    return res.json(discussionsData);
+  },
+  */
 };
 
 module.exports = {
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,9 +5,18 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
+router.get('/:id', findById);
 
 
+/*
+//새로운 질문 등록을 위한 라우트 작성 (과제 X)
+router.post('/', create);
+
+//질문 삭제를 위한 라우트 작성 (과제 X)
+router.delete('/:id', deleteById);
+
+*/
 module.exports = router;
