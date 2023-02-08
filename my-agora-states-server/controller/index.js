194459c87d707545b35b625d943894ed02f5f615
crowwan <crowwan@naver.com>
2023-02-09 00:16:14 +0900

    feat: 라우터 처리 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,77 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+let discussionsData = agoraStatesDiscussions;
+
+const compareByKey = (target, key) => (a) => a[key]?.toString() === target[key];
+
+const calculatePages = (item) => Math.ceil(item.length / 10) || 0;
+
+const filterItem = (item) => (f) => {
+  const discussion = item.filter((a) => f(a));
+  return discussion;
+}; //O(n);
+
+const filterDiscussions = filterItem(discussionsData);
+
+const findById = (req, res) => {
+  // discussionsData.find(a => a.id === +req.params); // O(n)
+  // 고차함수
+  const discussion = filterDiscussions(compareByKey(req.params, "id"));
+  discussion.length
+    ? res.json(discussion[0])
+    : res.status(404).json(discussion);
+};
+
+const findByTag = (target) => filterDiscussions(compareByKey(target, "tag"));
+
+const findByPage = (res, page, data) => {
+  const start = (page - 1) * 10;
+  const discussion = data.slice(start, start + 10);
+  const pages = calculatePages(data);
+  res.json({ discussion, pages });
+};
+
+const findUnanswered = (data) => {
+  return filterItem(data)((a) => !Boolean(a.answer));
+};
+
+const appendData = (req, res) => {
+  discussionsData.unshift(req.body);
+  res.send("ok");
+};
+const patchData = (req, res) => {
+  discussionsData.find(compareByKey(req.params, "id")).answer = {
+    ...req.body,
+  };
+  res.send("ok");
+};
+const deleteData = (req, res) => {
+  discussionsData = discussionsData.filter(
+    (a) => !compareByKey(req.params, "id")(a)
+  );
+  res.send("deleted");
+};
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    if (!Object.keys(req.query).length) {
+      res.json(discussionsData);
+      return;
+    }
+
+    const tagData = req.query.tag ? findByTag(req.query) : discussionsData;
+
+    const unansweredData = req.query.unanswered
+      ? findUnanswered(tagData)
+      : tagData;
+
+    req.query.page
+      ? findByPage(res, req.query.page, unansweredData)
+      : findByPage(res, 1, unansweredData);
   },
-
-  findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+  findById,
+  appendData,
+  patchData,
+  deleteData,
 };
 
 module.exports = {
