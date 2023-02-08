32756afbdd46f00c4e415673a725bf81863518cc
Latemarch <73418661+Latemarch@users.noreply.github.com>
2023-02-08 18:16:59 +0900

    Done

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,19 +1,21 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+const { agoraStatesDiscussions } = require('../repository/discussions')
+const discussionsData = agoraStatesDiscussions
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
-};
+    const { id } = req.params
+    let result = discussionsData.filter((obj) => obj.id === +id).pop()
+    if (result) return res.send(result)
+    else return res.status(404).send('Not Found')
+  },
+}
 
 module.exports = {
   discussionsController,
-};
+}
