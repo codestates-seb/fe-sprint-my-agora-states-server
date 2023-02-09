ecebdb868f81c816e925798afe2409ac32d8800a
Kadesti <50021232+Kadesti@users.noreply.github.com>
2023-02-09 13:50:41 +0900

    Agora_khu

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,16 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    let filtered = discussionsData.find((e) => e.id === Number(id));
+    if(!filtered) return res.status(404).send(filtered);
+    
+    // res.send('TODO:')
+    return res.status(200).json(filtered);
+  },
 };
 
-module.exports = {
-  discussionsController,
-};
+module.exports = { discussionsController };
