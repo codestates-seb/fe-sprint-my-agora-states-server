54c9ff6dc99222ebdfa3b923a12d50d335597221
shdomi8599 <shdomi@naver.com>
2023-02-09 10:46:09 +0900

    서버 완성

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -7,17 +7,14 @@ const morgan = require('morgan');
 // morgan 미들웨어가 세팅되어 있습니다.
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
 app.use(morgan('tiny'));
-
-// TODO: cors를 적용합니다.
-
-// TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-
+app.use(cors());
+app.use(express.json());
 
 const port = 4000;
 const discussionsRouter = require('./router/discussions');
 
 // TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
-
+app.use('/discussions', discussionsRouter);
 
 app.get('/', (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,16 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    if (discussionsData.filter((x) => x.id === Number(id)).length === 0)
+      return res.status(404).send('Not found');
+    else
+      return res.status(200).json(discussionsData.filter((x) => x.id === Number(id))[0]);
   }
-
 };
-
 module.exports = {
   discussionsController,
 };
