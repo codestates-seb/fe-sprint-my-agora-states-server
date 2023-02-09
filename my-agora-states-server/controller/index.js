9c5d2ffd2025c9988816ae98ea37a631e345ef8a
sung <sunga.jlh@gmail.com>
2023-02-09 11:26:03 +0900

    bare req finished

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,15 +3,22 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // TODO: 모든 discussions 목록을 응답합니다
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
+    const { id } = req.params;
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    console.log('id: ', id);
+    const idx = discussionsData.findIndex(el => el.id === +id);
+    console.log('idx: ', idx);
+    if (idx === -1){
+      return res.status(404).json('The id number does not exist');
+    }else{
+      return res.status(200).json(discussionsData[idx]);
+    }
   }
-
 };
 
 module.exports = {
