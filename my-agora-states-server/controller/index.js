cb70f2278a6e0e740a1b2bd16a3c33d912c0d747
yunhwan <sky10240107@naver.com>
2023-02-09 10:17:42 +0900

    과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    //idx가 조건을 만족 못한다면 -1
+    const idx = discussionsData.findIndex((data) => data.id === +id);
+
+    if (idx >= 0) {
+      res.status(200).json(discussionsData[idx]);
+    } else {
+      res.status(404).json("Not Found!");
+    }
+  },
 };
 
 module.exports = {
