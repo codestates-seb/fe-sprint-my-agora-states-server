6df60a2d88a62fe4335770e9bdd8ebb55cd495d9
Leejungmin211 <jungmin910211@gmail.com>
2023-02-09 11:30:49 +0900

    아고라 스테이츠 서버 작성

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -6,18 +6,19 @@ const morgan = require('morgan');
 
 // morgan 미들웨어가 세팅되어 있습니다.
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
+//요청과 응답에 대한 정보를 콘솔에 기록, tiny 최소한된 로그를 출력
 app.use(morgan('tiny'));
 
 // TODO: cors를 적용합니다.
-
+app.use(cors())
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-
+app.use(express.json())
 
 const port = 4000;
 const discussionsRouter = require('./router/discussions');
 
 // TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
-
+app.use('/discussions', discussionsRouter)
 
 app.get('/', (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,16 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    //id와 일치하는 데이터가 존재하지 않을 경우, 404 오류 메세지
+    const id= Number(req.params.id);
+    //console.log(typeof id) -> string이므로 Number타입으로 변경
+    const [listId] = discussionsData.filter((item) => item.id === id)
+    if(listId) return res.status(200).json(listId);
+    return res.status(404).send('Not Found!');
+  },
 };
-
 module.exports = {
   discussionsController,
 };
