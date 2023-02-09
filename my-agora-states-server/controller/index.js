ce7b7e5de52c67202178be42dfb3b2353e2f2ae2
taeyoung1012 <dlaxodud12@naver.com>
2023-02-09 15:10:58 +0900

    discussion id 조회기능

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -10,8 +10,20 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send()
-  }
+    let filtered = [] 
+    
+    if(req.params.id){
+      filtered = discussionsData.filter((el)=> {
+        return el.id === Number(req.params.id)
+      })
+    }
+
+    if(filtered.length > 0){
+      res.send(filtered[0])
+    } else {
+      res.status(404).send("일치하는 데이터 없는데요?")
+    }
+    }
   
 };
 
