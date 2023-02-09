fbd4dc8b958616b18bc391caaf2f8975c2ed8c80
elee-88 <120184637+elee-88@users.noreply.github.com>
2023-02-09 14:24:35 +0900

    index.js 수정

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,15 +3,24 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // TODO: 모든 discussions 목록을 응답합니다. 
+    return res.json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.    
+    // id를 기준으로 데이터를 찾아야 함 -> 요청 파라미터 지정
+    const { id } = req.params;
+    // 모든 데이터가 있는 discussionData 안에서 같은 id를 filter로 찾음
+    // discussionData의 id는 문자열이 아니고 number이기 때문에 변환
+    let filtered = discussionsData.filter((el) => el.id === Number(id));
+    // 데이터가 없는 경우
+    if(filtered.length === 0){
+      return res.status(404).json('Not found');
+    } else {
+      return res.status(200).json('filtered[0]');
+    }
   }
-
 };
 
 module.exports = {
