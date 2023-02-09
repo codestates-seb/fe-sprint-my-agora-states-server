f69eeb93501e5d82886b48f0204095ab1ab53989
chochojj <csgy114@gmail.com>
2023-02-09 15:09:52 +0900

    Update index.js

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,7 +9,15 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} = req.params;
+    if(req.params.id){
+      let resdisc = discussionsData.filter((el)=> Number(req.params.id) === el.id);
+      if(resdisc.length === 0 ){
+        return res.status(404).json(resdisc);
+      }else{
+        return res.status(200).json(resdisc[0]);
+      }
+    }
   }
 
 };
