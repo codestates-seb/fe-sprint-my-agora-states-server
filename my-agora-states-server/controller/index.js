7ab188575ff5f17696b5594740b7852dcde97bd2
장혜림 <heylj522@gmail.com>
2023-02-09 11:11:18 +0900

    기능구현완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,14 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const data = discussionsData.filter(discussion => discussion.id === Number(id));
+    return data.length ? res.status(200).json(data[0]) : res.status(404).json('Not Found!');
   }
 
 };
