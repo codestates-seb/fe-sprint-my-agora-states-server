343ff2a22d03d90a73cf721fc591aeeef6dbdd4e
ock2023 <dmsdhr2605@gmail.com>
2023-02-09 12:07:44 +0900

    끝

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(agoraStatesDiscussions)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const list = discussionsData.filter(el=>{
+      return el.id === Number(id);
+    });
+    
+    if(list.length !== 0) {res.status(200).json(list[0]);}
+    else {res.status(404).send("NOT FOUND!")}
+  
+    }
 };
 
 module.exports = {
