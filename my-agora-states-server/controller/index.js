b569dfb68b43501732e764d1789fb08c88eafd7c
JHJFE <ghwo9611@gmail.com>
2023-02-09 11:17:15 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+     const { id } = req.params
+     let list;
+ 
+     list = discussionsData.filter((data) => {
+       return data.id === parseInt(id)
+     })
+     if (list.length === 0) {
+       return res.status(404).json('Not Found');
+     } else {
+       return res.status(200).json(...list);
+     }
   }
 
 };
