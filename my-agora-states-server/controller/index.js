ef893d69978dbb1971c296e675086946f2a7b126
raccoon0814 <yjh44242168@gmail.com>
2023-02-09 11:12:15 +0900

    Bare Minium Requirement

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params // 구조분해할당
+    let list = discussionsData.filter((item) => { // 걸러내고
+      return item.id === Number(id)
+    })
+    if(list.length !== 0){ // 걸러낸 길이가 0이 아니면
+      return res.status(200).send(list[0]) // 객체가 담긴 배열로 들어오니까 [{...}] 
+    } else { //아니면=길이가 0이면
+      return res.status(404).send('Not found') //404
+    }
   }
 
 };
