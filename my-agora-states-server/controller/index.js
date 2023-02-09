0516e42d03ccb7b7d0389ed46503048f7f074a03
mscojl24 <mscojl24@naver.com>
2023-02-09 09:57:19 +0900

    Bare Minimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,18 +1,49 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
+
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params
+    let list = discussionsData.filter(el => { return el.id === Number(id) });
+    list.length ? res.status(200).json(list[0]) : res.status(404).json(list);
+  },
 
-};
+  
+  // // [PUT]요청 된 uuid 값과 동일한 uuid 값을 가진 데이터를 요쳥 된 Body 데이터로 수정합니다.
+  // update: (req, res) => {
+
+  //   const { id } = req.params; 
+  //   const bodyData = req.body; 
+  //   let indexNum; 
+  //     for (i = 0; i < flights.length; i++) {
+  //     if (discussionsData[i].id === Number(id)) {
+  //       indexNum = i; 
+  //     }
+  //   }
+  //   discussionsData[indexNum] = { ...discussionsData[indexNum], ...bodyData };
+  //   return res.status(200).json(discussionsData[findIndex]); 
+  // },
+
+  // // [POST] 요청 된 데이터를 저장합니다.
+  // create: (req, res) => { 
+  //   const discussionsData_id = discussionsData.length; 
+  //   req.body.id = discussionsData_id; 
+
+  //   discussionsData.push(req.body);
+  //   return res.status(200).json(discussionsData); 
+
+  // },
+
+
+
+}
 
 module.exports = {
   discussionsController,
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,9 +5,14 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
+router.get('/:id', findById);
 
+// // 추가 POST(수정) , DEL(삭제)
+// router.post('/:id', create);
+
+// router.put('/:id', update);
 
 module.exports = router;
