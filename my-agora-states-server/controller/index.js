32a08bdc687b2feadb37ac808885bff47b77d8a2
MinhyungEdward <rmaalsgud@gmail.com>
2023-02-09 13:05:53 +0900

    finish

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const found = discussionsData.find((d) => d.id === Number(id));
+    if (found) {
+      return res.status(200).json(found);
+    } else {
+      return res.status(404).send("Not found");
+    }
   }
-
 };
 
 module.exports = {
