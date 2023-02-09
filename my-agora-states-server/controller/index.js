48bec332b051ea876c66ad3bf0a4335b13536a09
1-blue <1-blue98@naver.com>
2023-02-09 16:08:41 +0900

    complete

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,32 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+
+    res.json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const targetId = +req.params.id;
 
+    const exDiscussion = discussionsData.find(
+      (discussion) => discussion.id === targetId
+    );
+
+    if (!exDiscussion) return res.status(404).json("일치하는 질문이 없습니다.");
+
+    return res.json(exDiscussion);
+  },
+
+  create: (req, res) => {
+    const { discussion } = req.body;
+
+    discussionsData.unshift(discussion);
+
+    res.json({
+      message: "질문 생성 완료!",
+      createdDiscussion: discussion,
+    });
+  },
 };
 
 module.exports = {
