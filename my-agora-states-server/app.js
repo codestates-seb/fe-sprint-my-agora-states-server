9ee1dcf76215ab66900335b9389f0be4663464fa
오현종 <ohj@ohyeonjong-ui-MacBookAir.local>
2023-02-09 15:03:35 +0900

    과제

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -11,7 +11,7 @@ app.use(morgan('tiny'));
 // TODO: cors를 적용합니다.
 app.use(cors());
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-app.use(express.json({strict: false}));
+app.use(express.json());
 
 const port = 4000;
 const discussionsRouter = require('./router/discussions');
