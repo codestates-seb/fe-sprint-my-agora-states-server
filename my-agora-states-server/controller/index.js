d0424324de71c0cb24b7221de71731f014d52629
Hyun Jin, Lee <nagi5924@gmail.com>
2023-02-09 14:16:21 +0900

    Bare Minimum Requirement Clear

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,15 +1,28 @@
+const e = require("express");
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params
+
+    const filtered = discussionsData.filter((el) => el.id === Number(id))
+
+    console.log(`filtered length : ${filtered.length}`)
+
+    if(!filtered.length){  // filtered 가 빈 배열(filtered.length===0)이라면 status(404) 출력
+      return res.status(404).send({
+        message: 'Data Not Found!'
+      })
+    }
+    // filtered = [{id : id, cratedAt: ...}] 형태로 나와서 ...로 전개해줬음
+    return res.status(200).json(...filtered);
   }
 
 };
