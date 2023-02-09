aea6c8e723c5a017c33f0da94ac50e6afc6967f7
zi0ne <jiwon0331j@gmail.com>
2023-02-09 13:12:44 +0900

    Bare Minimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,17 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    //res.send('TODO:')
+    const { id } = req.params;
+    const filtered = discussionsData.filter(el => el.id === Number(id));
+    filtered.length ? res.status(200).json(filtered[0]): res.status(404).json(filtered);
+
+    },
 
 };
 
