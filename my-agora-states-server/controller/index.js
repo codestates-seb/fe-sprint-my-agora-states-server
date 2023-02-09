e16c73e7f7959dfb2f30acc493072d259258be23
SEB_FE_43_차재현 <119926151+CHA710617@users.noreply.github.com>
2023-02-09 17:30:56 +0900

    test

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,16 +3,18 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
-  },
+   console.log(typeof discussionsData[0].id)
+    res.status(404).json(discussionsData);
+    },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const {id}=req.params
+    const idx=discussionsData.findIndex((el)=>el.id===parseInt(id));// TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const filteredId=discussionsData.fulter((el)=>el.id===parseInt(id))
+    console.loglength!==0
 
-};
+  }
+}
 
 module.exports = {
   discussionsController,
