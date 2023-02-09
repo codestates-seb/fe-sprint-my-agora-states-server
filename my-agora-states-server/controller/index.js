42afb3367c50a9678287959df8eeef5c87c063c1
shin ga young <rkdud12547@gmail.com>
2023-02-09 13:07:11 +0900

    Bare Minimum Requirement

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,15 +1,24 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
+// console.log(discussionsData)
+
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} = req.params;
+    // console.log (id) // 2, 1239018
+    const discussion = discussionsData.find(data => data.id === Number(id))
+    console.log(discussion)
+    if(discussion) {
+      return res.status(200).json(discussion);
+    } else return res.status(404).json('Not Found');
+    
   }
 
 };
