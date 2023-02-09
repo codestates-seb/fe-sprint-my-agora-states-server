9b0f02786a981749ce87ca69c3ac201baa00fc8c
윤종협 <yjh5799@naver.com>
2023-02-09 14:44:45 +0900

    bareMinimum 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,22 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+const discussionsData = agoraStatesDiscussions; // 불러온 데이터 재할당
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params; // id 요청
+    let list = discussionsData.filter(el => Number(id) === el.id); // 요청 id와 일치하는 id를 찾아 필터링한 새로운 배열을 list에 할당
+    if (list.length > 0) {
+      return res.status(200).json(list[0]); // 일치할 경우 json으로 응답
+    } else {
+      return res.status(404).json('404 Not Found');
+    }
   }
-
 };
 
 module.exports = {
