9efb4f0030ab6cf9b37637801ac3ee93b1c2bda3
주재민 <jujaemin@jujaemin-ui-MacBookAir.local>
2023-02-09 10:26:54 +0900

    complete

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,30 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
+const okCode = 200;
+const notFoundCode = 404;
+
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(okCode).json(agoraStatesDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    let { id } = req.params;
+    id = Number(id);
 
+    const result = agoraStatesDiscussions.find(
+      (discussion) => discussion.id === id
+    );
+
+    if (result) {
+      return res.status(okCode).json(result);
+    } else {
+      return res.status(notFoundCode).end();
+    }
+  },
 };
 
 module.exports = {
