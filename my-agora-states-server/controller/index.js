a1ec3dcc6558831f84b66dc36457427d892c9110
Sangbeom Heo <mansaout@gmail.com>
2023-02-09 14:48:50 +0900

    feat: create 구현 - 새로운 아이템을 디스커션 목록에 추가

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,4 +1,5 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { v4: uuid } = require('uuid');
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
@@ -18,6 +19,36 @@ const discussionsController = {
     }
   },
 
+  create: (req, res) => {
+    const { author, title, content } = req.body;
+    const createdId = uuid();
+    const createdDate = new Date().toJSON();
+
+    const newItem = {
+      author,
+      title,
+      content,
+      id: uuid(),
+      createdAt: createdDate,
+      updatedAt: createdDate,
+      url: '/',
+      answer: null,
+      avatarUrl:
+        'https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4',
+    };
+    discussionsData.unshift(newItem);
+    res.status(200).json(createdId);
+  },
+};
+
+const filterFunc = {
+  All: () => true,
+  Answered: item => !!item.answer,
+  Unanswered: item => !item.answer,
+};
+
+const getFilteredItems = (items, filter) => {
+  return items.filter(filterFunc[filter]);
 };
 
 module.exports = {
