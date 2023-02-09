4246df511cca0c6abefea5d461345c55f931edca
Zeereoreo <wltjrcjf12@naver.com>
2023-02-09 10:52:33 +0900

    베어미니멈 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    let fillterd = discussionsData.filter(el => Number(id) === el.id);
+    if (fillterd.length === 0) {
+      return res.status(404).json("err");
+    } else {
+      return res.status(200).json(fillterd[0]);
+    }
   }
 
 };
