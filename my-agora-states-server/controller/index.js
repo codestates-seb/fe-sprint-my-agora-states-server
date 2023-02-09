a7c17a436a84ecae502777cf41208bdd3f9b7ad8
정제우 <wpdn3825@naver.com>
2023-02-09 16:25:18 +0900

    bare minimum submit

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,15 +4,29 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+      console.log('discussions success')
+      return res.status(200).json(discussionsData);
+
+      
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    // TODO: path parameter id를 가진 discussion을 응답합니다.
+    // console.log(typeof(req.params.id)) // -> string
+    // console.log(Number(req.params.id)) // -> 20
+    // 클라이언트가 요청한 id의 값을 필터로만들어 discussionData를 걸러내자.
 
-};
+    const filtered = discussionsData.filter((list) => list.id === Number(req.params.id));
+
+    // 걸러낸 discussionData에 데이터가 있으면 200
+    if (filtered.length !== 0) {
+      return res.status(200).json(filtered[0]);
+    // 걸러낸 discussionData에 데이터가 없으면 404
+    } else {
+      return res.status(404).end(`죄송합니다람쥐`);
+    }
+  }
+}
 
 module.exports = {
   discussionsController,
