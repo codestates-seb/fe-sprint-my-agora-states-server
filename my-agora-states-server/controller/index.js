01ac67d13c9bf2881536212f13e23e2952b282a5
gjy0605 <gjy0605@gmail.com>
2023-02-09 11:15:30 +0900

    index.js TODO 조건 작성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // res.send(discussionsData) 보다 아래 코드가 더 적절한 이유.
+    // 응답코드 200 제공하여 성공적으로 처리 알림, JSON으로 반환하여 클라이언트에서 더 쉽게 파싱하고 처리 가능.
+    res.state(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    let { id } = req.params;
+    const sameData = agoraStatesDiscussions.find(
+      (same) => same.id === Number(id)
+    );
+    if (sameData) return res.status(200).json(sameData);
+    else return res.status(404).end(); 
+  },
 };
 
 module.exports = {
