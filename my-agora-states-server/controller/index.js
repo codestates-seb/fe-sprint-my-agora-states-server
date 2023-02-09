b3d94d8252080114d1e7c85be57a3d8c1a2131fc
dohyun98 <kch9554@naver.com>
2023-02-09 13:10:43 +0900

    done

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // res.send(discussionsData);
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    let list = discussionsData.filter((el) => el.id === Number(id)); // req.pqeams는 string타입이라서
+    console.log(list);
+    if (list.length === 0) {
+      return res.status(404).json("Not FSound");
+    } else {
+      return res.status(200).json(list[0]);
+    }
+  },
 };
 
 module.exports = {
