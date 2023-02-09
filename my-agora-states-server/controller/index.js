aa0adf264764b3844677fb6ebe58f970695837dd
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-02-09 15:04:32 +0900

    아고라서버

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const data = discussionsData.filter((dis) => {
+      return dis.id == Number(id);
+    });
+    if (data[0]) {
+      return res.status(200).json(data[0]);
+    } else {
+      return res.status(404).send("없어용🫥");
+    }
+  },
 };
 
 module.exports = {
