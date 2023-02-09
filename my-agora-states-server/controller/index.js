56b7c84878f26269f22ce4d619c601dcd3919788
Taehyun <dbsxogus2331@naver.com>
2023-02-09 10:49:39 +0900

    feat: add server completed

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,24 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const discussion = discussionsData.filter((item) => {
+      return item.id === Number(id); // 에러 처리 --> id를 받아올 때 숫자 형태가 아니라 변환 필요
+    });
+    if (discussion.length === 0) {
+      res.status(404).json({ Error: 'No Discussion Found' });
+    } else {
+      res.status(200).json(discussion[0]);
+    }
+  },
 };
 
 module.exports = {
