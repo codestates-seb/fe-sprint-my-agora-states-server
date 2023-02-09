f7dfc8afdf74f906798226623a494afce231be44
UseonJ <naul630@gmail.com>
2023-02-09 16:57:29 +0900

    put
    fxxxking

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,15 +1,32 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+let discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    
+    return res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    // const bodyData = req.body;
+    let filtered = discussionsData.filter(x => x.id === Number(id))    
+    if(filtered.length === 0){
+      return res.status(404).json('Not found')
+    }
+    else{
+    return res.status(200).json(...filtered)
+    }
+  },
+  newPost: (req, res) => {    
+    const bodyData = req.body; // body에 객체를 넘겨주어야 함.
+    
+    const newPost = [ bodyData, ...discussionsData ]; //body가 추가된 post
+    discussionsData = newPost;
+    console.log(discussionsData)
+    return res.status(200).json(discussionsData);    
   }
 
 };
