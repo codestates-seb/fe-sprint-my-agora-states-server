fda09d905a772663e649678553d690cb190e9a14
leekoby <abc2752@naver.com>
2023-02-09 13:23:23 +0900

    create server

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,16 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const filtered = discussionsData.find((item) => +(id) === item.id);
+
+    if (filtered) return res.status(200).send(filtered);
+    return res.status(404).send('No Discussion');
   }
 
 };
