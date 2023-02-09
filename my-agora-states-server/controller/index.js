acec0374720a85f91d38c47c36e4b5ac1ceeb701
Hyojin <hyojin.ju@gmx.net>
2023-02-09 12:20:55 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,13 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    const filteredDiscussions = discussionsData.filter((discussion) => discussion.id === Number(id));
+    console.log(filteredDiscussions);
+    console.log(filteredDiscussions[0])
+    return filteredDiscussions.length ? 
+            res.status(200).json(filteredDiscussions[0]) : 
+            res.status(404).json('No Match ID');
+  },
 
 };
 
