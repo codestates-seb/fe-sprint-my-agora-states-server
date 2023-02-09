3adcf2494ffe150e6e2fe2064db96e07872c531b
rsuubinn <rsuubinn@gmail.com>
2023-02-09 15:03:56 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,17 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    if(id) {
+      const filterd = discussionsData.filter((discussion) => discussion.id === Number(id));
+      if(filterd.length === 0) return res.status(404).json(filterd[0]);
+      else return res.status(200).json(filterd[0]);
+    } 
   }
 
 };
