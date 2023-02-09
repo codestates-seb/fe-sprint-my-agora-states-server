62b76ad31521df5f8659458fee90cf0acf76f6e3
dongwook98 <ehddnr8563@gmail.com>
2023-02-09 11:22:00 +0900

    모든 discussion 목록을 조회하는 라우터 작성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,17 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const data = [...discussionsData]; // 원본 유지하면서 복사
+    return res.status(200).json(data);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    res.send('TODO:');
+  },
 };
 
 module.exports = {
