c8472809ff09e0dc0615b5c34fe779821481289e
938938 <938kjh@gmail.com>
2023-02-09 11:26:57 +0900

    ::Bare Minimum Requirement 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,24 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    console.log('????');
+    res.status(200).json(discussionsData);
   },
-
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    console.log('?');
+    const { id } = req.params;
+    const list = discussionsData.filter((ele) => ele.id === Number(id));
+    if (list.length === 0) {
+      res.status(404).send();
+    } else {
+      res.status(200).json(list[0]);
+    }
+    // res.status(200).send(list);
+  },
 };
 
 module.exports = {
