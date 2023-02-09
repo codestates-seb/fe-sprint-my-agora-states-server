90c645c7e9cb73115bfa20500b7577cc1d4f940e
dev-Jaewon <app235naver@gmail.com>
2023-02-09 15:15:17 +0900

    과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -2,18 +2,21 @@ const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
-  findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
-  },
+    findAll: (req, res) => {
+        // TODO: 모든 discussions 목록을 응답합니다.
+        // res.send('TODO:')
+        return res.status(200).json(discussionsData);
+    },
 
-  findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    findById: (req, res) => {
+        const { id } = req.params;
+        const data = discussionsData.filter((v) => v.id === +id);
 
+        if (data.length > 0) return res.status(200).json(data[0]);
+        else return res.status(404).json();
+    },
 };
 
 module.exports = {
-  discussionsController,
+    discussionsController,
 };
