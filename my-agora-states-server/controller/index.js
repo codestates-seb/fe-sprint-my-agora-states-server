60774fc21e715d94cad24fac66a4a58277156f64
조민수 <choalstn@gmail.com>
2023-02-09 11:08:59 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    console.log(typeof id);
+    const filter = discussionsData.filter((el) => el.id === Number(id));
 
+    filter.length > 0
+      ? res.status(200).json(...filter)
+      : res.status(404).send("404 err");
+  },
 };
 
 module.exports = {
