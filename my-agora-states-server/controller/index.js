f18059f088c3e02fb22da976ed7a1cecbae24512
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-02-09 11:46:56 +0900

    :sparkles: feat(controller): 요청으로 들어온 id와 일치하는 디스커션에 대한 응답 메서드 구현 id와 일치하는 디스커션이 있다면 200응답에 해당 디스커션 데이터를 보낸다. id와 일치하는 디스커션이 없다면 404응답을 보낸다.

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,7 +9,11 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const findDiscussion = discussionsData.find((item) => Number(id) === item.id);
+
+    if (findDiscussion) return res.status(200).send(findDiscussion);
+    return res.status(404).send('No Discussion');
   }
 
 };
