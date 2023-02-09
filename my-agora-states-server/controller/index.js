01b6dda68f2d6bee22aa83abe930dcb07fbc2da7
Zeereoreo <wltjrcjf12@naver.com>
2023-02-09 14:39:10 +0900

    id res 부분 수정

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -11,10 +11,10 @@ const discussionsController = {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
     let fillterd = discussionsData.filter(el => Number(id) === el.id);
-    if (fillterd.length === 0) {
-      return res.status(404).json("err");
+    if (fillterd) {
+      return res.status(200).json(fillterd);
     } else {
-      return res.status(200).json(fillterd[0]);
+      return res.status(404).json("err");
     }
   }
 
