5e378d0eca13c67047b8e6e728d7527a253e3226
elee-88 <120184637+elee-88@users.noreply.github.com>
2023-02-09 14:41:30 +0900

    discussions.js 수정

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,9 +5,11 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+// 서버에 있는 목록을 가져 옴 -> get으로 라우팅
+// 모든 목록을 조회 -> '/'
+router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-
+router.get('/:id', findById);
 
 module.exports = router;
