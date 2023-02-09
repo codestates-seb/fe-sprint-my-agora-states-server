1d851dbf834de62c599e12af90583a386bb2502f
Aroma-oh <on002way@gmail.com>
2023-02-09 11:17:39 +0900

    Bare Minimum

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,15 +1,17 @@
+const e = require('express');
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const filteredID = discussionsData.filter((el) => el.id === Number(id));
+    filteredID.length ? res.status(200).json(filteredID[0]) : res.status(404).json("Not found")
   }
 
 };
