db239594635f7f71936989d3d93fb02016ad5db5
sineTlsl <limsieun0313@gmail.com>
2023-02-09 11:08:07 +0900

    findAll, findById 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,25 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    let filterdData = discussionsData.filter((discussion) => {
+      return Number(id) === discussion.id;
+    });
+
+    let result = Object.assign({}, filterdData[0]);
+
+    if (result.id) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json("NOT FOUND!");
+    }
+  },
 };
 
 module.exports = {
