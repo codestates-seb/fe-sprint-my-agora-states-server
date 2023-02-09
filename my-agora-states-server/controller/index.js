7714e131719fba67bed04b6532a13c96e065f36e
Hyemm_720 <zzxxc070@gmail.com>
2023-02-09 11:12:38 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(agoraStatesDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    let filteredData = agoraStatesDiscussions.filter((ele) => {
+      return ele.id === Number(id); // id 문자열들 넘버타입으로 바꿔주기
+    });
+
+    if (filteredData.length !== 0) {
+      res.status(200).json(filteredData[0]);
+    } else {
+      res.status(404).send("Not Found");
+    }
+  },
 };
 
 module.exports = {
