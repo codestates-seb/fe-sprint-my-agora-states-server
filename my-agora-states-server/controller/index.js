7c23007479ae1e2ceaa47407fd1e99c33da26bd2
hajin1015 <10hajin15@gmail.com>
2023-02-09 09:35:11 +0900

    과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,16 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.send(discussionsData)
   },
+
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    let filteredData = discussionsData.filter((el) => el.id === Number(id))
+    if (!filteredData.length) return res.status(404).send()
+    return res.status(200).json(...filteredData)
   }
 
 };
