75d344530061d4111fbabec4b21c5bfcbeb84533
chochojj <csgy114@gmail.com>
2023-02-09 14:19:13 +0900

    update app.js

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,7 +4,7 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
