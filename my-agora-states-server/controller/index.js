1d3c6b795716dbd66481f6a835c8e41560cda1df
조민수 <choalstn@gmail.com>
2023-03-03 00:17:01 +0900

    delete 기능 구현 (lodash 사용)

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,5 +1,6 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
+const _ = require("lodash");
 
 const discussionsController = {
   findAll: (req, res) => {
@@ -10,12 +11,25 @@ const discussionsController = {
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
-    console.log(typeof id);
     const filter = discussionsData.filter((el) => el.id === Number(id));
 
     filter.length > 0
       ? res.status(200).json(...filter)
       : res.status(404).send("404 err");
+  },
+
+  create: (req, res) => {
+    discussionsData.unshift(req.body);
+    return res.status(201).json(discussionsData[0]);
+  },
+
+  deleteDiscussion: (req, res) => {
+    const { title } = req.params;
+    console.log(title);
+
+    // discussionsData = discussionsData.filter((el) => el.title !== title);
+    let result = _.remove(discussionsData, { title: title });
+    return res.json(result);
   },
 };
 
