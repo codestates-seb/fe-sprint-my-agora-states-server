54ba223d9333deb3f2e0151d7e784d40b68e19b0
g4dalcom <g4dalcom@gmail.com>
2023-04-06 15:55:08 +0900

    bare minimum 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,19 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const id = parseInt(req.params.id);
+    const data = discussionsData.filter((el) => el.id === id);
 
+    if (data.length > 0) {
+      return res.status(200).json(data[0]);
+    } else return res.status(404).json('데이터가 존재하지 않습니다.');
+  },
 };
 
 module.exports = {
