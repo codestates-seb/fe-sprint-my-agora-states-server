ef51dd3fdd14d94887304f4b8a250f05882598e5
jihoon2819 <parkjihoon98>
2023-04-07 15:54:36 +0900

    asdasd

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,13 +3,27 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // TODO: 모든 discussions 목록을 응답합니다.  
+    
+      return res.status(200).json(discussionsData);
+    
+    
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id}=req.params;
+    let filteredData = discussionsData.filter(discussion => {
+      //req.params.id가 문자열임
+      return discussion.id === Number(id);
+    })
+    if (filteredData.length === 0) { //필터되어 나온 게 없다면
+      return res.status(404).json(`${id} is not found`).end();
+    } else { //필터되어서 나온 게 있다면
+      return res.status(200).json(filteredData[0]);
+      //[0]으로 뽑아와야만 출력이 된다.
+    }
+  
   }
 
 };
