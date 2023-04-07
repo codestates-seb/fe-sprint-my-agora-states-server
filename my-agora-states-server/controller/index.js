e4ba5bfba310919f25d04d52b7dc9d2ef6264e09
thewronghand <penfreak77@gmail.com>
2023-04-07 09:17:11 +0900

    feat: discussions.js 라우터 구현

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,14 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.status(200).send(discussionsData);
+    return res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
     let discussion = discussionsData.filter((item) => item.id === id)[0];
-    res.status(200).send(discussion);
+    return res.status(200).send(discussion);
   },
 };
 
