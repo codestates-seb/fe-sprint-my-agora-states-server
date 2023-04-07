7c1639b09526b4e8beae92ad2c83b8550e8c1583
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-04-07 12:28:27 +0900

    feat: Complete Bare Minimum Requirements

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -2,16 +2,28 @@ const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
+  // TODO: 모든 discussions 목록을 응답합니다.
+  // GET /discussions
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    if(Object.keys(req.query).length === 0) {
+      return res.status(200).json(discussionsData);
+    }
   },
-
+  
+  // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+  // GET /discussions/:id
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const filteredData = discussionsData.filter(discussion => 
+      {return discussion.id === Number(id)});
+    // discussionsData에 해당 id와 일치하는 데이터가 존재하는 경우,
+    if(filteredData.length !== 0) {
+      return res.status(200).json(filteredData[0]);
+    // discussionsData에 해당 id와 일치하는 데이터가 존재하지 않는 경우,
+    } else {
+      return res.status(404).json('Please confirm your ${id}');
+    }
   }
-
 };
 
 module.exports = {
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -3,11 +3,12 @@ const { discussionsController } = require('../controller');
 const { findAll, findById } = discussionsController;
 const express = require('express');
 const router = express.Router();
+const { app } = require('../app');
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-
+router.get('/:id', findById); // findAll 아니고 *findById 주의
 
 module.exports = router;
