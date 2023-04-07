76602bf4a43ccad2faff4c0898ec363d9f8b8e67
22yuu <22yuu@naver.com>
2023-04-07 14:01:15 +0900

    feat: BareMinum Done...

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const filtered = discussionsData.filter((item) => item.id === Number(id));
+
+    if(filtered.length === 0) {
+      res.status(404).send('Not Found....!');
+    } else {
+      res.status(200).send(filtered[0]);
+    }
   }
 
 };
