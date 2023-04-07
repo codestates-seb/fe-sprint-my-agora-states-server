ed9cf3dffa186b598e200601cc9694ef6f1621f9
lhs9602 <lhs2490@gmail.com>
2023-04-07 10:54:09 +0900

    bareminum 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,15 +1,29 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+let discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id}=req.params;
+    let list=discussionsData;
+    if (req.params) {
+      
+    
+    list = list.filter((items)=>{
+     return items.id===Number(id);
+    })
+    if(list.length===0){
+      return res.status(404).send('존재하지 않는 id입니다');
+     }
+    }
+    
+
+    return res.status(200).send(list[0]);
   }
 
 };
