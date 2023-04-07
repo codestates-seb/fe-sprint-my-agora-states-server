8208216e1ddea074b7440b21f47f3aaaa84d0fb3
kimhyejoo <rnrmfzl2659@gmail.com>
2023-04-07 11:18:29 +0900

    bareminimum 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    //const {id, title, url, author, bodyHTML, avatarUrl} = discussionsData
+    res.status(200).json(discussionsData)
+    //console.log(req.body)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const foundByIdDiscussion = discussionsData.find(item => +id === item.id)
+    console.log(foundByIdDiscussion)
+    return foundByIdDiscussion 
+      ? res.status(200).json(foundByIdDiscussion) 
+      : res.status(404).json(`${id} is not found`)
   }
 
 };
