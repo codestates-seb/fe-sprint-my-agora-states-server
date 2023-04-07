778b0f7af1eb4c3767f4f7ad3f972bb19aae4636
HChanWoo <cksdn1st@naver.com>
2023-04-07 10:13:00 +0900

    Update app.js

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -9,10 +9,11 @@ const morgan = require('morgan');
 app.use(morgan('tiny'));
 
 // TODO: cors를 적용합니다.
-app.unsubscribe(cors());
+// app.unsubscribe(cors());
+app.use(cors());
 
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-app.use(express.json());
+app.use(express.json({ strict: false }));
 
 const port = 4000;
 const discussionsRouter = require('./router/discussions');
