b5360f8e928a0dbc5dbc2d3a5d4282a878f03e27
yyjjvv <jiwonyoo66@gmail.com>
2023-04-07 11:48:26 +0900

    🐛 bare minimum clear

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -2,18 +2,26 @@ const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
-  findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
-  },
+    findAll: (req, res) => {
+        // TODO: 모든 discussions 목록을 응답합니다.
+        res.status(200).json(discussionsData);
+    },
 
-  findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    findById: (req, res) => {
+        // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+        const { id } = req.params;
+        const filteredItem = discussionsData.filter(
+            (item) => item.id === Number(id)
+        );
+        let result = Object.assign({}, filteredItem[0]);
+        if (result.id) {
+            res.status(200).json(result);
+        } else {
+            res.status(404).json("검색 결과가 없습니다.");
+        }
+    },
 };
 
 module.exports = {
-  discussionsController,
+    discussionsController,
 };
