1eb612234bf60e75214eed8881254b922c8e1cde
jeongjwon <zlzlsksk111@gmail.com>
2023-04-07 15:19:13 +0900

    Bare Minimum complete

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,26 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다. 
+    const { id } = req.params;
+
+    let filteredData = discussionsData;
+
+    filteredData = filteredData.filter((data) => {  
+      return data.id === Number(id);
+    });
+
+    
+    //객체 형태로 전달해야하는데, filteredData 는 배열이다.
+    if(filteredData.length > 0){
+      return res.status(200).json(filteredData[0]);
+    }else{
+      return res.status(404).send(null);
+    }
   }
 
 };
