9172f7e90cb02e333e71ac3818206716546a0641
hahagarden <tingyuan413@gmail.com>
2023-04-07 16:09:11 +0900

    add error

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -11,6 +11,9 @@ const discussionsController = {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
     targetData = discussionsData.find((data) => data.id === Number(id));
+    if (!targetData) {
+      res.sendStatus(404);
+    }
     res.send(targetData);
   },
 };
