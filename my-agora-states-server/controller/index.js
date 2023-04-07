8a7cb4a2558f5d93e6cd0934767b936981d2f9ce
thejsw <jswtncapetnc@gmail.com>
2023-04-07 16:09:02 +0900

    my-agora-states-server

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,24 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // res.send('TODO:')
+    if (Object.keys(req.query).length === 0) {
+      return res.status(200).json(discussionsData)
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    // res.send('TODO:')
+    const { id } = req.params;
+    let filteredData = discussionsData.filter(discussion => {
+      return discussion.id === Number(id)
+    })
+    if (filteredData.length === 0) {
+      return res.status(404).json(`${id} is not found`)
+    } else {
+      return res.status(200).json(filteredData[0])
+    }
   }
 
 };
