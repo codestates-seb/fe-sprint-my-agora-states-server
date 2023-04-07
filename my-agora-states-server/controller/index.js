5834d61ac9f709436441ce07986375f04127f87d
thewronghand <penfreak77@gmail.com>
2023-04-07 09:23:17 +0900

    feat: discussionsController 구현 내용 수정
    findAll, findById 메서드의 구현 내용을 수정한다.

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    return res.status(200).send(discussionsData);
+    if (Object.keys(req.query).length === 0) {
+      return res.status(200).send(discussionsData);
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
-    let discussion = discussionsData.filter((item) => item.id === id)[0];
-    return res.status(200).send(discussion);
+    let discussion = discussionsData.find((item) => item.id === id);
+    if (discussion) {
+      return res.status(200).send(discussion);
+    }
+    return res.status(404).send("Not Found");
   },
 };
 
