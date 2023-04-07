84f93ee9563adab06c9be60e1936f93bff04107b
yooni <jiyoon0616@naver.com>
2023-04-07 17:05:48 +0900

    feat (index) : 응답 기능 구현 코드 추가

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,24 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    // 요청으로 들어온 id와 일치하는 discussion을 찾습니다.
+    const { id } = req.params;
 
+    const discussion = discussionsData.find((el) => el.id === Number(id));
+
+    if (!discussion) {
+      return res.status(404).json({ error: 'Discussion not found' });
+    }
+
+    return res.status(200).json(discussion);
+  },
 };
 
 module.exports = {
