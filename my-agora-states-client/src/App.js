c9aeae2dcb0803af3ab5c959ded542b60d61d362
sungjihyun <jhsung23@gmail.com>
2023-04-07 16:26:53 +0900

    feat: aisde를 SideBar 컴포넌트로 분리

--- my-agora-states-client/src/App.js
+++ my-agora-states-client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import NoItem from './components/NoItem/NoItem';
 import DiscussionItem from './components/DiscussionItem/DiscussionItem';
 import DiscussionForm from './components/DiscussionForm/DiscussionForm';
+import SideBar from './components/SideBar/SideBar';
 
 function App() {
   const [datas, setDatas] = useState([]);
@@ -101,38 +102,7 @@ function App() {
           </div>
         </section>
 
-        <aside className="side-bar__container">
-          <div className="filter--container">
-            <h5>Filter</h5>
-            <div className="option--wrapper">
-              <input type="checkbox" name="filter" id="option-solved" checked />
-              <label for="option-solved">답변 완료</label>
-            </div>
-            <div className="option--wrapper">
-              <input type="checkbox" name="filter" id="option-unsolved" checked />
-              <label for="option-unsolved">답변 대기중</label>
-            </div>
-            <div className="option--wrapper">
-              <input type="checkbox" name="filter" id="option-notice" checked />
-              <label for="option-notice">공지</label>
-            </div>
-          </div>
-          <div className="sort--container">
-            <h5>Sort</h5>
-            <div className="option--wrapper">
-              <input type="radio" name="sort" id="option-recent" checked />
-              <label for="option-recent">최근 등록 순</label>
-            </div>
-            <div className="option--wrapper">
-              <input type="radio" name="sort" id="option-old" />
-              <label for="option-old">오래된 등록 순</label>
-            </div>
-            <div className="option--wrapper">
-              <input type="radio" name="sort" id="option-dictionary" />
-              <label for="option-dictionary">제목 가나다 순</label>
-            </div>
-          </div>
-        </aside>
+        <SideBar />
       </main>
     </>
   );
