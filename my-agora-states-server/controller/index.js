3db4f976e2a4745371146b18579d5af78449bb6e
KingGyeongHoo <fmaxgo@naver.com>
2023-04-07 13:37:48 +0900

    bare minimum clear

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    let data = discussionsData
+    return res.status(200).json(data)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    let filteredData = discussionsData.find(item => item.id === Number(req.params.id))
+    console.log(filteredData)
+    if(filteredData){
+      return res.status(200).json(filteredData)
+    } else {
+      return res.status(404).json('Data Not Found')
+    }
+    
   }
 
 };
