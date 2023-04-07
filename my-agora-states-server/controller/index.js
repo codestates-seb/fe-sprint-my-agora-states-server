2c4d306e43fe491696b7ced92d23c70df0aae75e
jungo <mkk90645@gmail.com>
2023-04-07 14:34:40 +0900

    findById

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,8 +9,12 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    // !!! req.parmas.id가 string형태로 들어오기 때문에 Number로 변환해주어야 한다.
+    const data = discussionsData.find((el) => el.id === Number(req.params.id));
+    if (!data) return res.status(404).send("Not found");
+    return res.status(200).json(data);
+  },
+
 
 };
 
