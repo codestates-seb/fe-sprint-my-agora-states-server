944e7dc5588096b761edc1f8d9f3276c7622edd2
sungjihyun <jhsung23@gmail.com>
2023-04-07 10:13:25 +0900

    feat: /discussions 경로 라우팅

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -4,6 +4,8 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 
+const discussions = require('./router/discussions');
+
 // morgan 미들웨어가 세팅되어 있습니다.
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
 app.use(morgan('tiny'));
@@ -18,6 +20,7 @@ const port = 4000;
 const discussionsRouter = require('./router/discussions');
 
 // TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다.
+app.use('/discussions', discussions);
 
 app.get('/', (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
