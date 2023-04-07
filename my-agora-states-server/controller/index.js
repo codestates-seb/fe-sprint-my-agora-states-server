e3de913b99f08514b39f96083e65b0a29b591f1a
soobbb <soob8437@gmail.com>
2023-04-07 14:50:17 +0900

    All pass

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,26 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    console.log("findById");
+    const { id } = req.params;
+    let list = discussionsData;
 
+    if (list) {
+      list = list.filter((data) => {
+        return data.id === Number(id);
+      });
+      if (list.length !== 0) {
+        return res.status(200).json(list[0]);
+      } else {
+        return res.status(404).send("Not Found!!!!!!!!!!!");
+      }
+    }
+  },
 };
 
 module.exports = {
