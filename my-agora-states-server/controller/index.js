6f2d2a75b6eaa256b17d4a3635362cd370fb5a7f
LimJaeSub <liso_o@naver.com>
2023-04-07 20:31:45 +0900

    arr[0]
    [0]

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    // params로 들어옴
+    if (req.params.id !== undefined) {
+      let filterData = discussionsData.filter((it) => {
+        return it.id == req.params.id;
+      });
+      if (filterData.length == 0) {
+        res.status(404).end();
+      }
+      res.status(200).json(filterData[0]);
+    }
+  },
 };
 
 module.exports = {
