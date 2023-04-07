1547ebc0fb89afd780d5675a439960000b0bf1ef
wwwwswe <rotey1016@gmail.com>
2023-04-07 18:36:46 +0900

    test-error

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -9,15 +9,15 @@ const morgan = require('morgan');
 app.use(morgan('tiny'));
 
 // TODO: cors를 적용합니다.
-
+app.use(cors());
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-
+app.use(express.json());
 
 const port = 4000;
 const discussionsRouter = require('./router/discussions');
 
 // TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
-
+app.use("/discussions", discussionsRouter);
 
 app.get('/', (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const discussion = discussionsData.find(discussion => discussion.id===Number(id))
+    if (discussion) {
+      return res.status('200').json(discussion);
+    } else {
+      return res.status('404').send('NOT FOUND');
+    }
   }
-
 };
 
 module.exports = {
