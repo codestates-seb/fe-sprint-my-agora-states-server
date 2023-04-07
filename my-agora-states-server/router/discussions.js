ea293ded12f5f70103f5347a99f1e55a893e859d
NonFungibleCode <saechorome99@naver.com>
2023-04-07 17:14:27 +0900

    과제 제출

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -4,9 +4,22 @@ const { findAll, findById } = discussionsController;
 const express = require('express');
 const router = express.Router();
 
+router.get('/', findAll);
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
 
-
+router.get('/:id', async (req, res) => {
+    try {
+      const id = req.params.id;
+      const discussion = await findById(id);
+      if (!discussion) {
+        return res.status(404).json({ message: 'Discussion not found' });
+      }
+      res.status(200).json(discussion);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
 
 
