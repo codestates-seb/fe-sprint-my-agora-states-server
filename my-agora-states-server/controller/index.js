e23b52f63607710331befd7cd171cf67f176d405
thewronghand <penfreak77@gmail.com>
2023-04-07 09:51:18 +0900

    fix: 테스트 통과를 위한 잘못된 코드 수정

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -12,9 +12,9 @@ const discussionsController = {
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
-    let discussion = discussionsData.find((item) => item.id === id);
+    let discussion = discussionsData.find((item) => item.id === Number(id)); //id값이 문자열 형태로 수신되므로 숫자로 바꿔주어야 한다.
     if (discussion) {
-      return res.status(200).send(discussion);
+      return res.status(200).json(discussion);
     }
     return res.status(404).send("Not Found");
   },
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,13 +5,9 @@ const express = require("express");
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-router.get("/", (req, res) => {
-  discussionsController.findAll();
-});
+router.get("/", findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-router.get("/", (req, res) => {
-  discussionsController.findById();
-});
+router.get("/:id", findById);
 
 module.exports = router;
