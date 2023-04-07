d104c97c4d29034a9fb615e8667ee38635c2a2a1
zheekr <gimmhee5@naver.com>
2023-04-07 16:59:32 +0900

    1차 제출

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
 const morgan = require('morgan');
 
@@ -9,14 +8,17 @@ const morgan = require('morgan');
 app.use(morgan('tiny'));
 
 // TODO: cors를 적용합니다.
+app.use(cors());
 
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+app.use(express.json({ strict: false }));
 
 
 const port = 4000;
 const discussionsRouter = require('./router/discussions');
 
 // TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
+app.use('/discussions', discussionsRouter)
 
 
 app.get('/', (req, res) => {
