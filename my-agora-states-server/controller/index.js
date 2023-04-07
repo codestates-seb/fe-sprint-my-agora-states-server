1abec4653d3f1ac7549d1278dcf2bf7c3a28dd65
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-04-07 12:25:54 +0900

    bare minimum 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    console.log("findAll")
+    return res.json(discussionsData);
   },
 
   findById: (req, res) => {
+    console.log("findById")
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    let filteredDiscussion = discussionsData.filter((discussion) =>{
+        return discussion.id === Number(req.params.id);
+    })
+
+    if(filteredDiscussion.length > 0)
+      return res.status(200).json(...filteredDiscussion);
+    else
+      return res.status(404).send('존재하지 않습니다.');
   }
 
 };
