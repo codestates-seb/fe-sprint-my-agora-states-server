41545237a57dede428813b6cf5459a3b3876e98b
eunheeeee <124559999+eunheeeee@users.noreply.github.com>
2023-04-07 16:18:27 +0900

    완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    if(Object.keys(req.query).length===0){
+      return res.status(200).json(discussionsData);
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} = req.params;
+    const found = discussionsDate.find((d)=>d.id===Number(id));
+    if(found){
+      return res.status(200).json(found);
+    }
+    else{
+      return res.status(404).send("Not found");
+    }
   }
 
 };
