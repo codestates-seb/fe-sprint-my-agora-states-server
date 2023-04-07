28402adaee572aa0c721970393d60c43e0351ccd
Seunghyo <shyo0218@naver.com>
2023-04-07 15:21:22 +0900

    crud 구현

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,5 +1,6 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
+const { v4: uuid } = require('uuid');
 
 const discussionsController = {
   findAll: (req, res) => {
@@ -17,6 +18,33 @@ const discussionsController = {
       return res.status(200).json(data);
     }else{
       return res.status(404).send('Not found');
+    }
+  },
+
+
+  create : (req, res) => {
+    const id = uuid();
+    const { title, author, bodyHTML } = req.body;
+    const newDiscussion = {
+      id,
+      createdAt:new Date().toISOString(),
+      title,
+      author,
+      bodyHTML
+    }
+    discussionsData.push(newDiscussion);
+    return res.status(201).json(newDiscussion);
+  },
+
+  deleteData : (req,res) => {
+    const {id} = req.params;
+    const data = discussionsData.find(discussion => discussion.id === Number(id));
+    const index = discussionsData.indexOf(data);
+    if(data){
+      discussionsData.splice(index, 1);
+      return res.status(200).send("deleted");
+    }else{
+      return res.status(404).send('The course with the given ID was not found');
     }
   }
 
