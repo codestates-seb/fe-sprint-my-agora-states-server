d22cb7676507b0ee6830cc5154af54dc1f018ffb
hdwmumu <keumhe0110@gmail.com>
2023-04-07 16:42:42 +0900

    get

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
 
+    const params = req.params.id;
+    const list = discussionsData.filter((data) => data.id === Number(params));
+    if (list.length !== 0) {
+      return res.status(200).json(list[0]);
+    } else {
+      return res.status(404).json("n");
+    }
+  },
 };
 
 module.exports = {
