c98868ba19a1a5b15110103b7dea45d3e4df531c
FE_Dev <124567984+zop1234@users.noreply.github.com>
2023-04-07 16:43:10 +0900

    Add files via upload

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,27 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
+    // res.send('TODO:')
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const {id} = req.params;
+    let result = discussionsData.filter((x) => x.id === Number(id));
+    if (result.length === 0) {
+      return res.status(404).json(result);
+    }
+    else {
+      result.map((x) => res.status(200).json(x));
+    }
+  },
 
+  create: (req, res) => {
+    discussionsData.unshift(req.body);
+    console.log(req.body);
+    return res.status(201).json(discussionsData[0]);
+  }
 };
 
 module.exports = {
