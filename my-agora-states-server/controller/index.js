9bfabe47860f507758c96d97bd4d7203a3e59bf2
djavoa99 <djavoa99@naver.com>
2023-04-07 16:42:10 +0900

    아고라 스테이츠 연동

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,14 +9,14 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    const { id } = req.params;
+    const { id } = req.params; // filter & find 차이점 a = [1, 2, 3, 4, 5]  => 짝수filter = [2, 4], 짝수find = 2, 4
     let discussion_id = discussionsData.find(discussion => discussion.id === Number(id)) // id가 string으로 나와서 숫자로 형변환 해줘야함
     if(discussion_id) {
       return res.status(200).json(discussion_id);
     } else {
       return res.status(404).send("404");
     }
-  }
+  },
 };
 
 module.exports = {
