3d9c965ea528c42b9c9488c0dc726df4a8ef32cb
yeon0226 <w01086248649@gmail.com>
2023-04-07 16:13:21 +0900

    filter 주석

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -19,7 +19,17 @@ const discussionsController = {
       return res.status(404).send('Not found');
     }
   }
-
+//   const { id } = req.params;
+//   let filteredData = discussionsData.filter(discussion => {//req.params.id가 string 형태로 들어온다. Number로 변환
+//     return discussion.id === Number(id);
+//   })
+//   if (filteredData.length === 0) { //필터되어 나온 게 없다면
+//     return res.status(404).json('${id} is not found');
+//   } else { //필터되서 나오면
+//     return res.status(200).json(filteredData[0]);
+//     //[0]으로 해야 출력됨
+//   }
+// }
 };
 
 module.exports = {
