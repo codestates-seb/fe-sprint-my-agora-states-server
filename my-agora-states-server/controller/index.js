0dd9ae0135d342d78d289a8e47d3a68a29c1a211
djavoa99 <djavoa99@naver.com>
2023-04-07 15:21:54 +0900

    findById 실패

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    let discussion_id = discussionsData.find(discussion => discussion.id === id)
+    if(discussion_id) {
+      return res.status(200).json(discussion_id);
+    } else {
+      return res.status(404).send("404");
+    }
   }
-
 };
 
 module.exports = {
