1563ceffc1688ba53cd9eeff1ccf7e09da232b0d
wwwwswe <rotey1016@gmail.com>
2023-04-07 16:51:53 +0900

    Bare Minimum-ok

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -4,10 +4,15 @@ const { findAll, findById } = discussionsController;
 const express = require('express');
 const router = express.Router();
 
-// TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
 
+// findAll 메소드와 findById 메소드를 discussionsController에서 불러와 해당 메소드들이 모든 discussions 목록을 조회하는 라우터와 :id에 맞는 discussion을 조회하는 라우터에 연결되도록 작성
+
+// TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
+//모든 discussions 목록을 조회하는 라우터는 GET 메소드를 사용하여 / 경로로 요청이 들어오면 findAll 메소드를 호출하도록 설정
+router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-
+// :id에 맞는 discussion을 조회하는 라우터는 GET 메소드를 사용하여 /:id 경로로 요청이 들어오면 findById 메소드를 호출하도록 설정
+router.get('/:id', findAll);
 
 module.exports = router;
