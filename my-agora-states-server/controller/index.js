22b55bcba2615ef808dfd0686f1b7434479da8d2
nosenal27 <nosenal27@gmail.com>
2023-04-07 16:09:37 +0900

    index.js completed

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -10,15 +10,15 @@ const discussionsController = {
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
-    const filteredData = discussionsData.filter((discussion) => {
-      return discussion.id === Number(id);
-    });
-    // discussionsData에 해당 id와 일치하는 데이터가 존재하는 경우,
-    if (filteredData.length !== 0) {
-      return res.status(200).json(filteredData[0]);
-      // discussionsData에 해당 id와 일치하는 데이터가 존재하지 않는 경우,
+    // discussionsData 안에서 같은 id를 찾는다  Number로 지정
+    const data = discussionsData.find(
+      (discussion) => discussion.id === Number(id)
+    );
+    if (data) {
+      console.log(data);
+      return res.status(200).json(data);
     } else {
-      return res.status(404).json("${id} does not match");
+      return res.status(404).send("Not Found");
     }
   },
 };
