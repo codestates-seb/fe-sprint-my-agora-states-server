9ed5eb72d793b055d5fece834e638770229f0229
yeon0226 <w01086248649@gmail.com>
2023-04-07 14:52:55 +0900

    express 추가

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -12,7 +12,7 @@ app.use(morgan('tiny'));
 app.use(cors());
 
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-
+app.use(express.json({strict:false}))
 
 const port = 4000;
 const discussionsRouter = require('./router/discussions');
