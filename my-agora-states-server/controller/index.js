2ed67c46762b5c95c0342109a00faeb928c25e08
nosenal27 <nosenal27@gmail.com>
2023-04-07 15:48:38 +0900

    index.js completed

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,26 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+let discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const filteredData = discussionsData.filter((discussion) => {
+      return discussion.id === Number(id);
+    });
+    // discussionsData에 해당 id와 일치하는 데이터가 존재하는 경우,
+    if (filteredData.length !== 0) {
+      return res.status(200).json(filteredData[0]);
+      // discussionsData에 해당 id와 일치하는 데이터가 존재하지 않는 경우,
+    } else {
+      return res.status(404).json("${id} does not match");
+    }
+  },
 };
 
 module.exports = {
