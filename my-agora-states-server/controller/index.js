f2c5989029d30e773a2347ad2592f312e0318e67
Raros17 <sookmeoyhj@gmail.com>
2023-04-07 16:29:47 +0900

    finale

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,15 +1,36 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
-
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
+    //성공했다는 상태코드 200과 함께 discussinData(목록)전체를 돌려줌.
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    //req.params로 받아야겠군.
+    const {id} = req.params;
+    //params로 들어오면 string 타입.
+      let filtered = discussionsData.filter(list=> list.id ===Number(id));
+      //꼭 number로 바꿔줘야 한다. 
+      if(filtered.length !== 0 ){
+        return res.status(200).json(filtered[0]);
+        //0번째를 추출하면 되고 아니면 안되네 무슨 차이지...        
+      } else {
+        return res.status(404).json('Incorrect request')
+      }
+  },
+
+  findByName: (req, res) => {
+    // TODO: 요청으로 들어온 name와 일치하는 discussion을 응답합니다.
+    const {name} = req.params;
+      let filtered = discussionsData.filter(list=> list.author ===name);
+      if(filtered.length !== 0 ){
+        return res.status(200).json(filtered[0]);    
+      } else {
+        return res.status(404).json('Incorrect request')
+      }
   }
 
 };
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,13 +1,15 @@
 // TODO: discussions 라우터를 완성합니다.
 const { discussionsController } = require('../controller');
-const { findAll, findById } = discussionsController;
+const { findAll, findById, findByName } = discussionsController;
 const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/', findAll);
+//흠...이정도면 모든 목록을 조회하는 라우터인가?
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-
-
+router.get('/:id', findById)
+//흠...이게 아이디에 맞는 라우터인가?
+router.get('posts/:name', findByName)
 module.exports = router;
