8c38d19463a6015f3ea16cad827a2d1e42f3a874
heowan <101536766+heowan@users.noreply.github.com>
2023-04-07 15:20:25 +0900

    SEB_FE_44_허완 아고라스테이프 서버 만들기 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,16 +1,35 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+const discussionsData = [...agoraStatesDiscussions];
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    const filterId = discussionsData.find(el => {
+      return el.id === Number(id);
+    });
+
+    if(filterId){
+      return res.status(200).json(filterId);
+    } else {
+      return res.status(404).send('404 Not Found');
+    }
+    
+  },
+
+  update: (req, res) => {
+    const newTweet = {};
+    newTweet.id = (discussionsData[0].id*1) + 1;
+    Object.assign(newTweet, req.body);
+    discussionsData.unshift(newTweet);
+
+    return res.status(201).json(newTweet);
+  } 
 
 };
 
