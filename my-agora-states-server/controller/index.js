ad2932d6c6dc42b90069a7321889fa6615db0a1a
lhs9602 <lhs2490@gmail.com>
2023-05-02 16:19:52 +0900

    완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,7 +4,7 @@ let discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    return res.status(200).send(discussionsData);
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
@@ -23,7 +23,7 @@ const discussionsController = {
     }
     
 
-    return res.status(200).send(...list);
+    return res.status(200).json(...list);
   },
 
 
