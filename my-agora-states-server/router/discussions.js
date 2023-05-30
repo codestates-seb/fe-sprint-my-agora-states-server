5cac9ad3253ed4b9d36a89a661aba58e4da4b421
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2023-05-30 16:06:59 +0900

    Add files via upload

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,9 +5,18 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
+router.get('/', function (req, res) {
+    if(Object.keys(req.params).length == 0){
+        findAll(req,res)
+    }
+})
 
-
+router.get('/:id', function (req,res) {
+    findById(req,res)
+})
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
+
+
 
 
 module.exports = router;
