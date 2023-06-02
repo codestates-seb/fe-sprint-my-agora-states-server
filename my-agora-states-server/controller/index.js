0747f2913c8c9f709274076039d050f2e872f304
LeeJongBum <qja965@gmail.com>
2023-06-02 19:44:24 +0900

    done

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -6,13 +6,16 @@ const discussionsController = {
     // TODO: 모든 discussions 목록을 응답합니다.
     return res.status(200).send(discussionsData);
   },
-
+  add: (req, res) => {
+    discussionsData.unshift(req.body);
+    return res.status(201).send(discussionsData);
+  },
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
-    const result = discussionsData.find((el) => el.id === parseInt(id));
+    const result = discussionsData.find((el) => el.id === Number(id));
     if (result) res.status(200).json(result);
-    else res.status(404).send("일치안함");
+    else res.status(404).send("아이디없음 ㅋ");
   },
 };
 
