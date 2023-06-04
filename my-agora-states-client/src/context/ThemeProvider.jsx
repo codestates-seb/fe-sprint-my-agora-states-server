d6bd730c32d211d513cab89c0cd41efe0afef855
nalsae <nalsae0207@naver.com>
2023-06-04 21:12:34 +0900

    [FEAT] 모든 컴포넌트에 theme을 공급하는 커스텀 ThemeProvider 구현

---
+++ my-agora-states-client/src/context/ThemeProvider.jsx
@@ -0,0 +1,25 @@
+import { createContext } from 'react';
+import useTheme from '../hooks/useTheme.js';
+import { light, dark } from '../styles/theme.js';
+import { ThemeProvider as CustomThemeProvider } from 'styled-components';
+
+const defaultTheme = {
+  theme: 'light',
+  onChangeTheme: () => {},
+};
+
+export const ThemeContext = createContext(defaultTheme);
+
+function ThemeProvider({ children }) {
+  const themeProps = useTheme();
+
+  return (
+    <ThemeContext.Provider value={themeProps}>
+      <CustomThemeProvider theme={themeProps.theme === 'light' ? light : dark}>
+        {children}
+      </CustomThemeProvider>
+    </ThemeContext.Provider>
+  );
+}
+
+export default ThemeProvider;
