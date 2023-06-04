73d79e2966be1c18b8960c6ce2873044dabd0c5d
nalsae <nalsae0207@naver.com>
2023-06-04 21:33:03 +0900

    [FEAT] 유틸 함수 caclulatePageValue 구현
    - currenPage와 totalPage를 전달 받아 Pagination 컴포넌트 렌더링에 필요한 값을 객체로 반환하는 함수

---
+++ my-agora-states-client/src/utils/functions/calculatePageValue.js
@@ -0,0 +1,17 @@
+const PAGE_COUNT = 5;
+
+export const calculatePageValue = (currentPage, totalPage) => {
+  const pageGroup = Math.ceil(currentPage / PAGE_COUNT);
+
+  let lastPage = pageGroup * PAGE_COUNT;
+  if (lastPage > totalPage) lastPage = totalPage;
+
+  let firstPage =
+    lastPage - (PAGE_COUNT - 1) < 1 ? 1 : lastPage - (PAGE_COUNT - 1);
+  if (totalPage < pageGroup * PAGE_COUNT)
+    firstPage = pageGroup * PAGE_COUNT - (PAGE_COUNT - 1);
+
+  const pageCount = lastPage - firstPage + 1;
+
+  return { firstPage, lastPage, pageCount };
+};
