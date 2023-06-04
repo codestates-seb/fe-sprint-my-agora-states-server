a0a3bbaa75e75c57df153566097914bb0cc0989f
nalsae <nalsae0207@naver.com>
2023-06-04 21:30:29 +0900

    [FEAT] Alert 컴포넌트 구현
    - 사용자가 질문을 등록하면 3.5초 동안 성공 메시지를 상단에 렌더링하는 컴포넌트

---
+++ my-agora-states-client/src/components/Alert.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { motion, AnimatePresence } from 'framer-motion';
+import { ALERT_VALUE } from '../utils/contants/motionValue.js';
+
+function Alert() {
+  const { initial, animate, transition } = ALERT_VALUE;
+
+  return (
+    <AnimatePresence>
+      <SuccessMessage
+        initial={initial}
+        animate={animate}
+        transition={transition}
+      >
+        🎉&nbsp;&nbsp;질문 등록이 완료되었습니다&nbsp;&nbsp;🎉
+      </SuccessMessage>
+    </AnimatePresence>
+  );
+}
+
+const SuccessMessage = styled(motion.p)`
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 5rem;
+  font-size: 1.25rem;
+  font-weight: 600;
+  color: #ffffff;
+  ${({ theme }) => theme.alert};
+`;
+
+export default Alert;
