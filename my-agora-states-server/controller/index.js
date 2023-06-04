8dd0d540f4b41c10dea20f80bedc22947178947a
nalsae <nalsae0207@naver.com>
2023-06-04 21:06:13 +0900

    [FEAT] discussion 라우터에 사용할 로직 구현
    - 모든 데이터를 조회하는 findAll 메서드
    - 특정 데이터를 조회하는 findById 메서드
    - 데이터를 추가하는 addDiscussion 메서드

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,42 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
+const { agoraStatesDiscussions } = require('../repository/discussions');
+
+let discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
-  findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
-  },
+  findAll: (req, res) => res.status(200).send(discussionsData),
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    const targetDiscussion = discussionsData.find(
+      (discussion) => +discussion.id === +id
+    );
+
+    return targetDiscussion
+      ? res.status(200).send(targetDiscussion)
+      : res.status(404).send('해당 ID의 discussion이 존재하지 않습니다!');
+  },
+
+  addDiscussion: (req, res) => {
+    if (!req.body) return res.status(404).send('질문 등록에 실패했습니다.');
+
+    const { id, title, story } = req.body;
+
+    const newDiscussion = {
+      id,
+      createdAt: new Date().toLocaleString(),
+      title,
+      url: '#',
+      author: id,
+      answer: null,
+      avatarUrl: `https://github.com/${id}.png`,
+      bodyHTML: story,
+    };
+
+    discussionsData = [newDiscussion, ...discussionsData];
+
+    return res.status(201).send(newDiscussion);
+  },
 };
 
 module.exports = {
