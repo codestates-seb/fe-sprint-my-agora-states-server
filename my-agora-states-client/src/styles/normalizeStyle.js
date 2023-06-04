e1bba0192810e06034e0bfd3cae5aeaea1caa9e7
nalsae <nalsae0207@naver.com>
2023-06-04 21:11:16 +0900

    [FEAT] 전역에 적용할 Normalize CSS 작성

---
+++ my-agora-states-client/src/styles/normalizeStyle.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const NormalizeStyle = createGlobalStyle/*css*/ `
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  textarea {
+    width: 0;
+  }
+
+  input,
+  button {
+    border: none;
+  }
+
+  button {
+    background: transparent;
+  }
+`;
