a463460d515d2ebb21d62df864ebfbf5ebda3c69
lsm0084 <heonmoo0@gmail.com>
2023-06-05 19:31:30 +0900

    임시

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const r = discussionsData.filter(i => i.id === parseInt(id, 10));
+    if (r.length > 0) {
+      const discussion = r[0];
+      return res.status(200).json(discussion);
+    } else {
+      return res.status(404).send('errer');
+    }
   }
-
 };
 
 module.exports = {
