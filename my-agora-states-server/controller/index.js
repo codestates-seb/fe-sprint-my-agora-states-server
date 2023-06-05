95609b4c527f96ff1cf0ee33ae2709706e67f3f8
Minsu Lee <dlalstn5789@gmail.com>
2023-06-06 00:12:52 +0900

    test 통과

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,32 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(
+      discussionsData.map((discussion) => {
+        return discussion;
+      })
+    );
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    let discussions = discussionsData.map((discussion) => {
+      return discussion;
+    });
+
+    let discussion = [];
+    discussion = discussions.filter((discussion) => {
+      // id를 number타입으로!
+      return discussion.id === +id;
+    });
+
+    if (discussion.length === 0) {
+      res.send(404);
+    }
+    res.send(discussion[0]);
+  },
 };
 
 module.exports = {
