93c12343ab95dd0f6998f53bdb1079b88d086821
HelloWorldDogs223 <yonghyeogi377@gmail.com>
2023-06-05 12:19:49 +0900

    first

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,15 +3,21 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    console.log(id);
+    const data = discussionsData.filter((elem) => {
+      return elem.id.toString() === id;
+    });
+    if (data.length === 0) {
+      return res.status(404).send("ERROR");
+    } else {
+      return res.status(200).json(data[0]);
+    }
+  },
 };
 
 module.exports = {
