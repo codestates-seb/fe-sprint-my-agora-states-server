a2e6db404ae12442de80a0cf5ea05486a31a6798
Yun sun moon <129922886+ysm990926@users.noreply.github.com>
2023-06-05 11:18:48 +0900

    Add files via upload

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    const filtered = discussionsData.find((el) => {
+      return el.id === parseInt(id);
+    });
+  
+    if (filtered) {
+      res.send(filtered);
+    } else {
+      res.status(404).send('err');
+    }
   }
 
 };
