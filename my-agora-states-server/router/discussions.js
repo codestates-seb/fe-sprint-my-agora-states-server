57f1d92458d3bc8fb2f169b9bac26bdbfc9f6e23
novice1993 <novice1993@gmail.com>
2023-06-06 23:12:19 +0900

    아고라스테이츠 과제 완료

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,13 +1,15 @@
-// TODO: discussions 라우터를 완성합니다.
 const { discussionsController } = require('../controller');
-const { findAll, findById } = discussionsController;
+const { findAll, findById, postContent } = discussionsController;
 const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
+router.get('/:id', findById);
 
+// client에서 post 요청을 보냈을 때
+router.post('/', postContent);
 
 module.exports = router;
