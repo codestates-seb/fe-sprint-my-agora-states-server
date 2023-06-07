f0be2920f05d5525bea529c039452b038abb9e4b
Explorers12 <gva05134@naver.com>
2023-06-07 11:07:18 +0900

    일단

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const p = req.params.id;
+    let i = discussionsData.filter((item) => item.id === Number(p));
 
+    if (i.length !== 0) {
+      return res.status(200).json(i[0]);
+    } else {
+      return res.status(404).json(n);
+    }
+  },
 };
 
 module.exports = {
