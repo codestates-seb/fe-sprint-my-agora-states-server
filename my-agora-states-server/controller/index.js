37e17eaec2698a87bba100c6815525c6376f3ff3
김병현 <gimbyeonghyeon@gimbyeonghyeon-ui-MacBookAir.local>
2023-06-07 15:22:19 +0900

    complete

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,29 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
+
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // 1. TODO: 모든 discussions 목록 응답
+    // get /discussions
+    if (Object.keys(req.query).length === 0) { 
+      return res.status(200).json(discussionsData);
+    }
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    // 2. TODO: 요청으로 들어온 id와 일치하는 discussion 응답
+    const { id } = req.params;
+    let filteredData = discussionsData.filter(discussion => {
+      return discussion.id === Number(id);
+    })
+    if (filteredData.length === 0) { 
+      return res.status(404).json('${id} is not found');
+    } else { 
+      return res.status(200).json(filteredData[0]);
+     
+    }
   }
-
 };
 
 module.exports = {
