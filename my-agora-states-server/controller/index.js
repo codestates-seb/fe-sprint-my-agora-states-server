0f6240e6e52481c8465eda1ee6e7f32c058a3348
mingming45 <hko0810@gmail.com>
2023-06-07 11:45:56 +0900

    1

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    //const { author } = req.query;
+    //if (author) {
+    //  res.json(discussionsData.filter({item} => item.author === author))
+    //} else
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const{ id } = req.params;
+    const filtered = discussionsData.filter(discussion => discussion.id === Number(id));
+    if (filtered.length > 0 ) {
+      return res.send(filtered[0])
+    } else {
+      return res.status(404).send('Bad Request'); 
+    }  
   }
-
 };
 
 module.exports = {
