1f11900a4eabb7df8bed692e6862e494eb1e63cd
hyeonjin <jhjin0630@gmail.com>
2023-06-07 13:03:56 +0900

    bareminimum 완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+
+    return res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const list = discussionsData.find((item) => {
+      return item.id === Number(req.params.id);
+    });
+    if (list) {
+      res.status(200).send(list);
+    } else {
+      res.status(404).end();
+    }
+  },
 };
 
 module.exports = {
