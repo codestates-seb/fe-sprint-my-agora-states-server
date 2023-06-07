ed442b24382f82b5ffb09d8748fa3c2df8d6dd8e
JIAHLEE0213 <ja7943@naver.com>
2023-06-07 13:25:08 +0900

    server 만들기 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,24 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} = req.params;
+    // list에는 filter메서드로 discsussion에 있는 id와 요청으로 들어온 id 같을 경우로 걸러줌
+    // req.params.id는 string으로 들어오기때문에 비교를 하기 위해서 Number로 바꿔야함
+    const list = discussionsData.filter((el) => el.id === Number(id))
+    if(list.length !== 0) {
+      // 왜 [0]써줘야하지..?
+      return res.status(200).send(list[0])
+    }
+    // 그렇지 않으면 에러
+    else {
+      return res.status(404).send("Not Found!");
+    }
   }
-
 };
 
 module.exports = {
