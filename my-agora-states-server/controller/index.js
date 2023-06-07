efa11175b07472171712f26e6a996eb37675dd2d
toa <gimhyoungsoo@gmail.com>
2023-06-07 15:53:06 +0900

    서버 코드 TC통과

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,15 +1,20 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
+
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // console.log(discussionsData)
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params
+    const filterdData = discussionsData.filter(el => el.id === Number(id))
+    if (filterdData.length === 0) res.status(404).send('id가 일치하는 데이터가 존재하지 않습니다.')
+    else res.send(...filterdData)
   }
 
 };
