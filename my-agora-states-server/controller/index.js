86389c02df4913c90bfb5a6cea90e527411b0b80
k-impossible <kth3349@gmail.com>
2023-06-07 16:57:19 +0900

    get,post,delete 작업

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,60 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
+const { v4: uuid } = require('uuid');
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData)
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    
+    const target = discussionsData.find((el)=>{
+      console.log(typeof id);
+      // if(typeof id === "string"){
+      //   return el.id === id
+      // }else{
+      // }
+      return el.id === Number(id)
+    })
 
+    return res.json(target)
+  },
+
+  create: (req, res) => {
+    let data = discussionsData;
+    console.log(req.body);
+
+    const newObj = {
+      id : req.body.id,
+      createdAt: req.body.createdAt,
+      title: req.body.title,
+      answer: null,
+      url: req.body.url,
+      bodyHTML: req.body.bodyHTML,
+      avatarUrl: req.body.avatarUrl,
+      author: req.body.author,
+      updatedAt: req.body.updatedAt
+    }
+    data.push(newObj)
+
+    return res.status(201).json(newObj)
+  },
+
+  deleteById: (req, res) => {
+    const id = req.params;
+    // TODO:
+
+    const deleteIdx = discussionsData.indexOf(el => {
+      return el.id === id
+    })
+
+    discussionsData.splice(deleteIdx,1)
+
+    return res.status(201).json("delete success!");
+  }
 };
 
 module.exports = {
