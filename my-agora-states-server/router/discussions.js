38c0fee96d4a4ed36462a28ef65c9ff6e8680a08
friscii <friscii61@gmail.com>
2023-06-07 13:46:57 +0900

    J.B.Lee A accomplished!

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,13 +1,20 @@
 // TODO: discussions 라우터를 완성합니다.
 const { discussionsController } = require('../controller');
-const { findAll, findById } = discussionsController;
+const { findAll, findById, createOne, updateById, deleteById } = discussionsController;
 const express = require('express');
 const router = express.Router();
 
-// TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
+// TODO: 모든 discussions 목록을 조회하는 라우터
+router.get('/', findAll);
 
-
-// TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
+// TODO: :id에 맞는 discussion을 조회하는 라우터
+router.get('/:id', findById);
+router.get('/:id', createOne); 
+// ADVANCED: discussion 하나를 생성하는 라우터를 작성합니다.
+router.get('/:id', updateById);
+// ADVANCED: discussion 하나를 수정하는 라우터를 작성합니다.
+router.get('/:id',deleteById);
+//ADVANCED: discussion 하나를 삭제하는 라우터를 작성합니다.
 
 
 module.exports = router;
