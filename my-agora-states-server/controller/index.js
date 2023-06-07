faf24b5a0eac2a889fc15497a68e02e2dec5041d
Juseong-Yu <aaa4591@naver.com>
2023-06-07 16:15:53 +0900

    post 구현

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,15 +9,21 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    const { id } = req.params;
     const filteredData = discussionsData.filter((data) => data.id === parseInt(id));
     if(filteredData.length > 0){
       res.json(filteredData[0]);
     }else {
       res.status(404).json('not Found');
     }
+  },
+  postDiscussions: (req ,res) => {
+    const id = discussionsData.length + 6;
+    let reqData = req.body;
+    reqData.id = id;
+    discussionsData.unshift(reqData);
+    res.status(200).json(reqData);
+    console.log(reqData)
   }
-
 };
 
 module.exports = {
