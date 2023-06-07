3bd0ccde9e11339c878af6a04fbe1a36ed27ce42
mason <fntldks09@gmail.com>
2023-06-07 15:12:22 +0900

    과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const filteredData = discussionsData.filter(
+      (data) => data.id === parseInt(id)
+    );
+    if (filteredData.length > 0) {
+      res.json(filteredData[0]);
+    } else {
+      res.status(404).json("not Found");
+    }
+  },
 };
 
 module.exports = {
