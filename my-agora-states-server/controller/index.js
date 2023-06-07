49674f1b077230a7f225f824e73b22efec26b176
ColmiismaL <colruck32@gmail.com>
2023-06-07 10:10:42 +0900

    까먹었던 test 실행 및 에러메세지 추가 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -5,7 +5,15 @@ const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
 
-    res.status(200).json(discussionsData);
+    const {author} = req.query
+
+    if (author) {
+      res.status(200).json(discussionsData.filter((item) => item.author === author));
+    } else {
+      res.status(200).json(discussionsData);
+    }
+
+    
   },
 
   findById: (req, res) => {
@@ -14,6 +22,10 @@ const discussionsController = {
     const { id } = req.params;
 
     const findIdDiscussionsData = discussionsData.find((discussion) => discussion.id === Number(id));
+
+    if (!findIdDiscussionsData) {
+      return res.status(404).json({ error: "discussion 없음" });
+    }
     
     res.status(200).json(findIdDiscussionsData)
   }
