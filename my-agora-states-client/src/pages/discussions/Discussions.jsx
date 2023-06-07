70d23ae94c9a7e5588bece9f355cad5d3d41ac20
심도연 <shimdokite@gmail.com>
2023-06-07 15:27:45 +0900

    [Feat] Discussions.jsx 답변 보기 구현
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.css
    	modified:   src/component/header/Header.scss
    	modified:   src/pages/discussions/Discussions.jsx
    	modified:   src/pages/discussions/Discussions.scss
    	new file:   src/pages/newDiscussion/NewDiscussion.jsx
    	new file:   src/pages/newDiscussion/NewDiscussion.scss
    
    Changes not staged for commit:
    	modified:   ../my-agora-states-server/repository/discussions.js
    	modified:   ../package.json

--- my-agora-states-client/src/pages/discussions/Discussions.jsx
+++ my-agora-states-client/src/pages/discussions/Discussions.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { getDiscussion } from "../../api/discussions";
 
+import Collapse from "@kunukn/react-collapse";
+
 import "./Discussions.scss";
 
 const Discussions = () => {
   const [discussions, setDiscussions] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -25,6 +28,7 @@ const Discussions = () => {
   }, []);
 
   const toggleShowDetail = (idx) => {
+    setIsOpen(true);
     setDiscussions((prev) => {
       const update = [...prev];
       console.log(update[idx].isDetailShow);
@@ -34,6 +38,7 @@ const Discussions = () => {
   };
 
   const toggleClosedDetail = (idx) => {
+    setIsOpen(false);
     setDiscussions((prev) => {
       const update = [...prev];
       console.log(update[idx].isDetailShow);
@@ -45,50 +50,87 @@ const Discussions = () => {
   return (
     <div id="Discussions">
       {discussions.map((cur, idx) => (
-        <div className="discussions" key={`${cur.id}${idx}`}>
-          <div className="discussion_avatar--wrapper">
-            <img
-              src={cur.avatar}
-              alt={`avatar of ${cur.author}`}
-              className="discussion_avatar--img"
-            />
-          </div>
-          <div className="discussion_contents">
-            <div className="discussion_title">
-              <div>
-                <a href={cur.url}>{cur.title}</a>
+        <>
+          <div className="discussions" key={`${cur.id}${idx}`}>
+            <div className="discussion_avatar--wrapper">
+              <img
+                src={cur.avatar}
+                alt={`avatar of ${cur.author}`}
+                className="discussion_avatar--img"
+              />
+            </div>
+            <div className="discussion_contents">
+              <div className="discussion_title">
+                <div>
+                  <a href={cur.url}>{cur.title}</a>
+                </div>
+              </div>
+              <div className="discussion_infomation">
+                <div className="info">{`${cur.author} / ${cur.createdAt}`}</div>
+              </div>
+
+              <div className="discussion_answer--contents">
+                {cur.isDetailShow ? (
+                  <button
+                    className="closed"
+                    onClick={() => toggleClosedDetail(idx)}
+                  >
+                    Closed
+                  </button>
+                ) : (
+                  <button
+                    className="showDetail"
+                    onClick={() => toggleShowDetail(idx)}
+                  >
+                    Show Detail
+                  </button>
+                )}
               </div>
             </div>
-            <div className="discussion_infomation">
-              <div className="info">{`${cur.author} / ${cur.createdAt}`}</div>
-            </div>
-          </div>
-          {cur.answer ? (
-            <div className="discussion_answer">☑</div>
-          ) : (
-            <div className="discussion_non-answer">❏</div>
-          )}
-          <div className="discussion_answer--contents">
-            {cur.isDetailShow ? (
-              <button
-                className="showDetail"
-                onClick={() => toggleClosedDetail(idx)}
-              >
-                Closed
-              </button>
+            {cur.answer ? (
+              <div className="discussion_answer">✔︎</div>
             ) : (
-              <button
-                className="showDetail"
-                onClick={() => toggleShowDetail(idx)}
-              >
-                Show Detail
-              </button>
+              <div className="discussion_non-answer">❏</div>
             )}
           </div>
-        </div>
+
+          {cur.isDetailShow && cur.answer && (
+            <div className="discussion_answer--content">
+              <div className="answers">
+                <Collapse isOpen={isOpen}>
+                  <div className="answer_avatar">
+                    <img
+                      src={cur.answer.avatarUrl}
+                      alt={`avatar of ${cur.answer.author}`}
+                      className="answer_avatar--img"
+                    />
+                    <div className="answer_infomation">
+                      <div className="info">{`${cur.answer.author} / ${cur.answer.createdAt}`}</div>
+                    </div>
+                  </div>
+                  <div className="answer_contents">
+                    <div className="answer_title">
+                      <div>
+                        <a href={cur.answer.url}>{cur.answer.title}</a>
+                        <div
+                          className="answer_content"
+                          style={{
+                            whiteSpace: "pre-wrap",
+                            lineHeight: "1.5",
+                          }}
+                        >
+                          {cur.answer.bodyHTML.replace(/<[^>]+>/g, "")}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Collapse>
+              </div>
+            </div>
+          )}
+        </>
       ))}
     </div>
-    // <div>app</div>
   );
 };
 
