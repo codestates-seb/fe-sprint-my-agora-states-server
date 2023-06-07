03fd7cc1fdabab9e95646dd9fd38b7f2b50d3b39
hanbinchoi <wkghskak@naver.com>
2023-06-07 16:55:55 +0900

    수정

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -11,8 +11,8 @@ const discussionsController = {
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const { id } = req.params;
-    const response = discussionsData.filter((ele) => ele.id === Number(id));
-    if (response.length === 0) res.status(404).json();
+    const response = discussionsData.find((ele) => ele.id === Number(id));
+    if (response === undefined) res.status(404).json();
     else res.status(200).json(response);
   },
 
