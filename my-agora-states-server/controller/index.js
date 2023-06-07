5eb0a75b6281e4b678e047ba047b57850ad08e6e
YongILTree1 <kyi4698@LAPTOP-8U5AUD4I>
2023-06-07 14:45:15 +0900

    아고라스테이츠 서버 베어미니멈 기능 구현 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -2,16 +2,32 @@ const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
+  // TODO: 모든 discussions 목록을 응답합니다.
+  // 쿼리스트링 만드는건 req.query 메서드 사용
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const { author } = req.query;
+    let filterAuthor = discussionsData;
+    if (req.query.author) {
+      filterAuthor = filterAuthor.filter((discussion) => {
+        return discussion.author === req.query.author;
+      });
+      return res.status(200).json(filterAuthor);
+    } else {
+      res.status(200).json(discussionsData);
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    let filteredDiscussions = discussionsData.filter((discussion) => discussion.id === parseInt(id));
 
+    if (filteredDiscussions.length !== 0) {
+      return res.status(200).json(filteredDiscussions[0]);
+    } else {
+      return res.status(404).json("NOT FOUND");
+    }
+  },
 };
 
 module.exports = {
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,13 +1,32 @@
 // TODO: discussions 라우터를 완성합니다.
-const { discussionsController } = require('../controller');
+const { discussionsController } = require("../controller");
 const { findAll, findById } = discussionsController;
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+// discussions로 들어오는 요청의 종류를 알아야 라우트를 할 수 있어
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
+// 디스커션과 컨트롤러를 연결
+// 전체 디스커션 데이터 조회 => controller 파일에 함수 저장되어 있어 -> 연결만 해주면 돼
+router.get("/", findAll);
+// 특정 디스커션 데이터 조회
+router.get("/:id", findById);
 
-
+//discussion 추가
+//router.post()
+//혹은 이미 존재하는 특정 디스커션 데이터를 수정
+//router.put()
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
 
-
 module.exports = router;
+
+//더 해볼 수 있는 기능들
+//서버
+//디스커션 데이터 추가
+//특정 디스커션 데이터 수정
+
+//클라이언트
+//질문 제목 클릭 시 질문 내용과 답변 내용 볼 수 있는
+//답변이 달렸는지 여부에 따라 렌더링이 달라지도록(e.g. 체크이모지)
+//서버 데이터를 수정할 수 있는 인터페이스 구현(e.g. 제목 옆 연필 이모지 누르면 제목 수정)
+//특정 데이터를 삭제할 수 있는 인터페이스 구현(e.g. 디스커션 옆 휴지통 이모지)
