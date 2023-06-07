486d884bf981179468e2ede58c899409db80f582
나의 사용자 이름 <  >
2023-06-07 16:14:53 +0900

    완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,18 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} =req.params;
+    const target = discussionsData.find((item) => item.id === Number(id))
+    if(!target) {
+      res.status(404).send('Not Found')
+    }else {
+      res.json(target)
+    }
   }
 
 };
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,9 +5,9 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/', findAll) // 모든 목록 조회라우터 , '/'는 전체의 뜻
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
-
+router.get('/:id',findById) // id에 맞는 조회
 
 module.exports = router;
