65d302d9d19482f0b45e8bb7b5ff068e7c418b23
TaeYoungPar <tpark@oswego.edu>
2023-06-07 10:56:42 +0900

    SEB_45_박태영

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,32 @@
+const { query } = require("express");
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const { author } = req.query;
+    if (author) {
+      const filterDiscussionData = discussionsData.filter(
+        (item) => item.author === author
+      );
+      res.status(200).json(filterDiscussionData);
+    } else {
+      res.status(200).json(discussionsData);
+    } // res.send("TODO:");
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const { id } = req.params;
+    const findId = discussionsData.find((item) => item.id === Number(id));
+    if (findId) {
+      res.status(200).json(findId);
+    } else {
+      res.status(404).send();
+    }
+    // res.send("TODO:id");
+  },
 };
 
 module.exports = {
