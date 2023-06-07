82418ae47a866ac9d10923128d24e75414122a41
김우주 <https://github.com/ghvhdh333>
2023-06-07 14:43:01 +0900

    first commit

--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,9 +5,14 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
+router.get('/:id', findById);
 
+
+// 디스커션을 추가
+
+// 이미 존재하는 특정 디스커션 데이터를 수정
 
 module.exports = router;
