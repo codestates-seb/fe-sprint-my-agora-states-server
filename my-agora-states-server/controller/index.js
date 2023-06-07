3891e5fb6bf0007dc0eaab709884b45d62fd4c89
백아름 <bkhrm@baeg-aleum-ui-iMac.local>
2023-06-07 15:05:59 +0900

    server

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,16 +4,36 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const { author } = req.query;
+    if (author) {
+      res.json(discussionsData.filter((item) => item.author === author));
+    } else {
+      res.json(discussionsData);
+    }
+    res.send();
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    const target = discussionsData.find((item) => item.id === Number(id));
+    if (!target) {
+      res.status(404).send("Not Found");
+    } else {
+      res.json(target);
+    }
+  },
 };
 
 module.exports = {
   discussionsController,
 };
+
+// findById: (req, res) => {
+//   // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+//   const agoraId = discussionsData.filter(
+//     (agoraId) => agoraId.id === Number(req.params.id)
+//   );
+//   res.send(agoraId);
+// },
