52a29eee0636c75c470674529e1df4c562793a2a
심도연 <shimdokite@gmail.com>
2023-06-07 10:09:31 +0900

    [Feat] index.js 에서 데이터 조회 메서드 구현
    Changes to be committed:
    	modified:   my-agora-states-server/controller/index.js

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    // console.log(typeof id); id가 string 이기때문에 타입을 맞춰줘야 한다
+    const target = discussionsData.find((cur) => +cur.id === +id);
+
+    return target
+      ? res.status(200).send(target)
+      : res.status(404).send("Discussion Not Found");
+  },
 };
 
 module.exports = {
