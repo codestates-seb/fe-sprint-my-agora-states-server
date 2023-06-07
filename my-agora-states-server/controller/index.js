5c97c72c16a21817af8a75688cb756de85f7d3fe
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-06-07 13:12:51 +0900

    🔥 feat: 아고라 스테이츠 서버 구현

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,27 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const { author } = req.query;
+    if (author) {
+      res.json(discussionsData.filter((item) => item.author === author));
+    } else {
+      res.json(discussionsData);
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
 
+    if (id) {
+      const discussion = discussionsData.find((item) => item.id === Number(id));
+      if (discussion) {
+        res.json(discussion);
+      } else {
+        res.status(404).json({ error: "Discussion not found." });
+      }
+    }
+  },
 };
 
 module.exports = {
