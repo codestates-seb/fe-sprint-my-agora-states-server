abc8d077b891154c619393b41fa6517b052c0d76
JB0129 <rltkqja1@gmail.com>
2023-06-07 16:53:20 +0900

    post put delete 라우터 추가

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,8 +3,19 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
+
+    // author로 검색
+    const { author } = req.query;
+
+    if (author) {
+      let authorFilter = discussionsData.filter((obj) => {
+        return obj.author === author;
+      })
+      return res.status(200).json(authorFilter)
+    }
+
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.status(200).json(discussionsData)
+    return res.status(200).json(discussionsData)
   },
 
   findById: (req, res) => {
@@ -15,22 +26,68 @@ const discussionsController = {
       return obj.id === Number(id);
     });
 
-    // let maped = filtered.map((obj) => {
-    //   let changedBody = {
-    //     id: obj.id,
-    //     title: obj.title,
-    //     url: obj.url,
-    //     author: obj.author,
-    //     bodyHTML: obj.bodyHTML,
-    //     avatarUrl: obj.avatarUrl
-    //   }
-    //   return changedBody
-    // });
-
     if (filtered.length) {
       return res.status(200).json(filtered[0])
     } else {
       return res.status(404).json("해당 id는 없습니다.")
+    }
+
+  },
+
+  createDate: (req, res) => {
+    // 요청으로 들어온 body 데이터를 discussions 목록에 추가한다.
+    const { title, url, author, bodyHTML, avatarUrl } = req.body;
+
+    let newData = {
+      id: discussionsData[0].id + 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      title: title,
+      url: url,
+      author: author,
+      answer: null,
+      bodyHTML: bodyHTML,
+      avatarUrl: avatarUrl
+    };
+
+    discussionsData.unshift(newData);
+
+    res.status(201).json(discussionsData);
+
+  },
+
+  updateList: (req, res) => {
+    const { id } = req.params;
+    const { title, url, author, bodyHTML, avatarUrl } = req.body;
+
+    let updateData = {
+      updatedAt: new Date(),
+      title: title,
+      url: url,
+      author: author,
+      bodyHTML: bodyHTML,
+      avatarUrl: avatarUrl
+    }
+
+    if (id) {
+      let filterd = discussionsData.filter((obj) => { return obj.id === Number(id) })
+      let obj = Object.assign(filterd[0], updateData)
+      res.status(200).json(obj);
+    } else {
+      res.status(404).json("해당 내용을 찾을 수 없습니다.")
+    }
+
+  },
+
+  deleteList: (req, res) => {
+    const { id } = req.params;
+
+    if (id) {
+      let num = discussionsData.map((obj) => { return obj.id }).indexOf(id);
+      discussionsData.splice(num, 1);
+      res.status(200).json(discussionsData);
+    } else {
+      res.status(404).json("이미 삭제된 내용이거나 찾을 수 없습니다.")
     }
 
   }
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -1,7 +1,7 @@
 // TODO: discussions 라우터를 완성합니다.
 const { app } = require('../app');
 const { discussionsController } = require('../controller');
-const { findAll, findById } = discussionsController;
+const { findAll, findById, createDate, updateList, deleteList } = discussionsController;
 const express = require('express');
 const router = express.Router();
 
@@ -10,5 +10,14 @@ router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
 router.get('/:id', findById);
+
+// discussions 목록에 새로운 질문을 추가한다.
+router.post('/', createDate);
+
+// discussions에서 특정 하나의 리스트 내용을 수정한다.
+router.put('/:id', updateList)
+
+// discussions에서 특정 하나의 리스트 내용을 삭제한다.
+router.delete('/:id', deleteList)
 
 module.exports = router;
