de6b33c260ea9c0092fba61442943eec324f9944
박지민 <jumpm9239@gmail.com>
2023-06-07 16:29:32 +0900

    클리어

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,31 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const { author } = req.query; // 쓴 사람 확인하는 query string 확인
+    // 작성자 쿼리스트링 있으면 그에 해당하는 데이터만 보내도록 해보자
+    //데이터 전송 확인 console.log({ author, query: req.query});
+    if (author) {
+      res.json(discussionsData.filter((item) => item.author === author ))
+    } else {
+      res.json(discussionsData);
+    }
+    
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    // const { id } = req.query;
+    // if (id) {
+    //   res.json(discussionsData.filter((item) => item.id === id))
+    // }
+    const { id } = req.params;
+    const list = discussionsData.find((item) => item.id === Number(id) )
+    if(list) {
+      return res.status(200).json(list);
+    }
+    else{
+      return res.status(404).json('Incorrect request');
+    }
   }
 
 };
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,9 +5,13 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
+router.get('/', findAll);
 
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
+router.get('/:id', findById);
 
-
+// 디스커션 추가
+router.post
+// 혹은 이미 존재하는 디스커션 데이터를 수정 POST
 module.exports = router;
