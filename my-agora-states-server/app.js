3e96bf811b3ed0d15574da5292d43851df2a3dbc
say_yes1024 <yjiee27289@gmail.com>
2023-06-07 16:05:05 +0900

    작업 완료

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -10,7 +10,11 @@ app.use(morgan('tiny'));
 
 // TODO: cors를 적용합니다.
 
+app.use(cors());
+
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+
+app.use(express.json({strict:false}));
 
 
 const port = 4000;
@@ -18,6 +22,7 @@ const discussionsRouter = require('./router/discussions');
 
 // TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
 
+app.use('/discussions', discussionsRouter)
 
 app.get('/', (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status('200').send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    
+    const { id } = req.params;
+    const data = discussionsData.find(item => item.id === Number(id))
+
+    if (data) {
+      return res.status('200').json(data);
+    } else {
+      return res.status('404').send('유효하지 않은 id입니다.');
+    }
   }
 
 };
