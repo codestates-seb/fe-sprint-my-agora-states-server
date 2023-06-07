d0146d71093c93633efa5abe1c148ea169875d4e
Jihee Byun <jh910805jh@gmail.com>
2023-06-07 14:41:08 +0900

    45 과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,25 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    if (Object.keys(req.query).length === 0) { 
+      console.log("dd")
+      return res.status(200).json(discussionsData);
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    const { id } = req.params;
+    let filteredData = discussionsData.filter(discussion => {// 배열안에 있는 객체 형식의 데이터를 id값으로 sort
+      return discussion.id === Number(id);
+    }) 
 
+    if (filteredData.length === 0) { //sort 된 데이터가 배열안에 객체형태로 없을경우, 
+      return res.status(404).json('${id} is not found');
+    } else { //하나의 id로 sort 된 데이터가 배열안에 객체형태로 한개 들어가 있을경우, 배열안에 첫번째 객체  return 
+      return res.status(200).json(filteredData[0]);
+    }
+  }
 };
 
 module.exports = {
