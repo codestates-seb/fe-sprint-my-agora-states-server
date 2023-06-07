66a0d162c1e644637b576033c406bc4de9e551b5
amberjackman <feverhong@vivaldi.net>
2023-06-07 16:38:08 +0900

    6/7

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -1,32 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 
 // morgan 미들웨어가 세팅되어 있습니다.
 // HTTP 요청 logger를 편리하게 사용할 수 있는 미들웨어 입니다.
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
+app.use(cors());
 // TODO: cors를 적용합니다.
 
+app.use(express.json({ strict: false }));
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
 
-
 const port = 4000;
-const discussionsRouter = require('./router/discussions');
 
-// TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다. 
+const discussionsRouter = require("./router/discussions");
 
+// TODO: app.use()를 활용하여 /discussions 경로로 라우팅합니다.
 
-app.get('/', (req, res) => {
+app.use("/discussions", discussionsRouter);
+
+app.get("/", (req, res) => {
   // 서버 상태 확인을 위해 상태 코드 200과 함께 응답을 보냅니다.
-  res.status(200).send('fe-sprint-my-agora-states-server');
+  res.status(200).send("fe-sprint-my-agora-states-server");
 });
 
 const server = app.listen(port, () => {
   console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 module.exports.app = app;
 module.exports.server = server;
