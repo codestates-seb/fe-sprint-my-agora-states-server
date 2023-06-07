73ef8c8bcde328c2dc29d7c2709f179ed59a6929
Juseong-Yu <aaa4591@naver.com>
2023-06-07 16:49:03 +0900

    오류수정

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,6 +9,7 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
+    const { id } =req.params;
     const filteredData = discussionsData.filter((data) => data.id === parseInt(id));
     if(filteredData.length > 0){
       res.json(filteredData[0]);
