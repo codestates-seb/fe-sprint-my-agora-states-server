724755f94c98ae281900880f0322e95d42550efc
rlaxotn2300 <rlaxotn2300@naver.com>
2023-06-07 13:10:14 +0900

    최소과제 완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const { author } = req.query;
+    if(author){
+      res.json(discussionsData.filter((item) => item.author === author))
+    }else{
+      res.json(discussionsData)
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} = req.params
+    const filterDiscusstinsData = discussionsData.filter(el => el.id === Number(id))
+    if(filterDiscusstinsData.length === 0){
+      res.status(404).json(`nonono`)
+    }else{
+      res.status(200).json(filterDiscusstinsData[0])
+    }
   }
 
 };
