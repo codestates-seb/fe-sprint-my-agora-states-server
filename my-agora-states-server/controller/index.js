139efb4397cc197c4ada0679e150ce732d4d0d56
da hae park <eg1377@naver.com>
2023-06-07 14:02:26 +0900

    과제 코드 수정 _ 오류 해결

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,22 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('discussionsData')
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
     const {id} = req.params;
-    const discussion = discussionsData.find((discussion)=> discussion.id === id);
 
-    if(discussion){
-      res.json(discussion);
-    }
-    else {
-      res.status(404).json({error:"Discussion not found"});
-    }
+    const discussion = discussionsData.find((discussion) => discussion.id === Number(id));
 
-    // res.send('TODO:')
+    if (discussion) {
+      const { id, title, url, author, bodyHTML, avatarUrl } = discussion;
+      return res.status(200).json(discussion);
+    } else {
+      return res.status(404).json({ message: 'Discussion not found' });
+    }
   }
 
 };
