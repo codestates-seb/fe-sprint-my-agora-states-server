acc7a72ff39f4585f974a63051824dab528c417c
da hae park <eg1377@naver.com>
2023-06-07 13:17:43 +0900

    FE45_박다해

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send('discussionsData')
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} = req.params;
+    const discussion = discussionsData.find((discussion)=> discussion.id === id);
+
+    if(discussion){
+      res.json(discussion);
+    }
+    else {
+      res.status(404).json({error:"Discussion not found"});
+    }
+
+    // res.send('TODO:')
   }
 
 };
