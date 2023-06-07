ecd9757b91abe01c490f1ae90df3280aee6a15d6
dongyunkim96 <dongyunkim96@gmail.com>
2023-06-07 00:11:31 -0400

    final draft

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,17 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const filteredData = discussionsData.filter((discussion) => discussion.id === Number(req.params.id));
+    
+    if (filteredData.length === 0) {
+      res.status(404).send();
+    }
+    res.send(...filteredData);
   }
 
 };
