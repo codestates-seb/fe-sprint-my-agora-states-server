2f18a97b2ec0161fe71c39405f074480e4ced3e7
ddussi <tmqdyfud1@gmail.com>
2023-06-07 14:07:02 +0900

    23/06/07

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,21 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    let list = discussionsData.filter((x)=>{return x.id === Number(req.params["id"])});
+    let resData;
+    let newList = list.map((x)=>{resData = x;});
+    if(list.length === 0){
+      res.status(404).send("Blank");
+    }else{
+      console.log();
+      res.status(200).send(resData);
+    }
+
   }
 
 };
