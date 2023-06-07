277bf2fbd773b4e3decc44a859e0c1c768c98d92
Jiwon Lee <giwon.yi339@gmail.com>
2023-06-07 15:41:28 +0900

    bare minimal passed

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,28 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // res.send("TODO:");
+
+    //declare the data into allDiscussions
+    const allDiscussions = discussionsData;
+    res.json(allDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
+    // res.send("TODO:");
+    const { id } = req.params; // Rename discussionId to id
 
+    const findDiscussion = discussionsData.find(
+      (item) => item.id === Number(id)
+    );
+
+    if (findDiscussion) {
+      return res.status(200).json(findDiscussion);
+    } else {
+      return res.status(404).json({ error: "Discussion not found" });
+    }
+  },
 };
 
 module.exports = {
