46d9041d72e6be96c9af77350538b230b0a640a3
DongHun, Kim <donghun.kdh@gmail.com>
2023-06-07 09:33:02 +0900

    Bare Minimums Requirements

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,22 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData);
   },
 
   findById: (req, res) => {
+    const filterdData = discussionsData.filter(
+      (discussion) => discussion.id === Number(req.params.id)
+    );
+    if (filterdData.length === 0) {
+      res.status(404).send();
+    }
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    res.send(...filterdData);
+  },
 };
 
 module.exports = {
