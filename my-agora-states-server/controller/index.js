4d121a9d1ab83f29b028690d6cc3aacd59b71135
Explorers12 <gva05134@naver.com>
2023-06-07 11:32:57 +0900

    일단2

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,7 +4,7 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    return res.status(200).json(discussionsData);
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
@@ -15,7 +15,7 @@ const discussionsController = {
     if (i.length !== 0) {
       return res.status(200).json(i[0]);
     } else {
-      return res.status(404).json(n);
+      return res.status(404).json(i);
     }
   },
 };
