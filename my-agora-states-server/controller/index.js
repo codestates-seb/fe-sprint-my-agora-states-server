21e5e399fe0dfb472cc3f5fd7a9e24a23ebb37f2
MS_btff <whdghks845@gmail.com>
2023-06-07 13:04:50 +0900

    agora

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,18 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const {agoraStatesDiscussions} = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    const result = discussionsData.find((el) => el.id === Number(id));
+    if (result) res.status(200).json(result);
+    else res.status(404).send('오류입니다.');
+  },
 };
 
 module.exports = {
