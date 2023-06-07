10698c3f9cc703c34afe35dbc7be230f6ef9764d
심도연 <shimdokite@gmail.com>
2023-06-07 14:01:28 +0900

    [Feat] Discussions.jsx 답변 보기/닫기 버튼 구현
    Changes not staged for commit:
    	modified:   ../my-agora-states-server/repository/discussions.js
    	modified:   ../package.json

--- my-agora-states-client/src/pages/discussions/Discussions.jsx
+++ my-agora-states-client/src/pages/discussions/Discussions.jsx
@@ -16,11 +16,31 @@ const Discussions = () => {
         author: discussion.author,
         title: discussion.title,
         avatar: discussion.avatarUrl,
+        answer: discussion.answer,
+        isDetailShow: false, // 각 항목의 상세 정보 표시 여부 상태값
       }));
       setDiscussions(formattedDiscussions);
-      console.log(res);
+      console.log(formattedDiscussions);
     })();
   }, []);
+
+  const toggleShowDetail = (idx) => {
+    setDiscussions((prev) => {
+      const update = [...prev];
+      console.log(update[idx].isDetailShow);
+      update[idx].isDetailShow = true;
+      return update;
+    });
+  };
+
+  const toggleClosedDetail = (idx) => {
+    setDiscussions((prev) => {
+      const update = [...prev];
+      console.log(update[idx].isDetailShow);
+      update[idx].isDetailShow = false;
+      return update;
+    });
+  };
 
   return (
     <div id="Discussions">
@@ -35,15 +55,36 @@ const Discussions = () => {
           </div>
           <div className="discussion_contents">
             <div className="discussion_title">
-              <h2>
+              <div>
                 <a href={cur.url}>{cur.title}</a>
-              </h2>
+              </div>
+            </div>
+            <div className="discussion_infomation">
+              <div className="info">{`${cur.author} / ${cur.createdAt}`}</div>
             </div>
           </div>
-          <div className="discussion_infomation">
-            <div className="info">{`${cur.author} / ${cur.createdAt}`}</div>
+          {cur.answer ? (
+            <div className="discussion_answer">☑</div>
+          ) : (
+            <div className="discussion_non-answer">❏</div>
+          )}
+          <div className="discussion_answer--contents">
+            {cur.isDetailShow ? (
+              <button
+                className="showDetail"
+                onClick={() => toggleClosedDetail(idx)}
+              >
+                Closed
+              </button>
+            ) : (
+              <button
+                className="showDetail"
+                onClick={() => toggleShowDetail(idx)}
+              >
+                Show Detail
+              </button>
+            )}
           </div>
-          <div className="discussion_answer">{cur.answer}</div>
         </div>
       ))}
     </div>
