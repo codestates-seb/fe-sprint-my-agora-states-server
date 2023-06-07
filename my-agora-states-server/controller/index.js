f5cdf08cbe9603fe2f2a63d081208d4f1137a6c5
Wunhae Kim 김은혜 <66160243+kimwunhae@users.noreply.github.com>
2023-06-07 10:04:09 +0900

    서버 기능 구현

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -1,17 +1,22 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
+const { agoraStatesDiscussions } = require('../repository/discussions');
 const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    const { author } = req.query;
+    if (author) {
+      res.json(discussionsData.filter((item) => item.author === author));
+    } else {
+      res.json(discussionsData);
+    }
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    res.send('TODO:');
+  },
+  addDiscussion: (req, res) => {},
 };
 
 module.exports = {
