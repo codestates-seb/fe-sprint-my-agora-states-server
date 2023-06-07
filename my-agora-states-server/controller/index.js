a96e96033994113dd101123c23bb698017c4b555
sowon_m1 <bsw2377@gmail.com>
2023-06-07 16:12:02 +0900

    과제제출용

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,20 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const { id } = req.params;
+    
+    const filteredData = discussionsData.filter((data) => data.id === Number(id));
+
+    if (filteredData.length) {
+      res.status(200).json(filteredData[0]);
+    } else {
+      res.status(404).json("Not Found");
+    }
   }
 
 };
--- my-agora-states-server/router/discussions.js
+++ my-agora-states-server/router/discussions.js
@@ -5,9 +5,15 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: 모든 discussions 목록을 조회하는 라우터를 작성합니다.
-
+router.get('/', findAll);
 
 // TODO: :id에 맞는 discussion을 조회하는 라우터를 작성합니다.
+router.get('/:id', findById);
 
+
+// 🔥 추가할거!
+// 디스커션 추가
+// router.post()
+// 이미 존재하는 특정 디스커션 데이터를 수정
 
 module.exports = router;
