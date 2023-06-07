d0cb56e6cb5cc279e619e81de090435a1bdcdab5
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-06-07 11:25:27 +0900

    나만의 아고라스테이츠 서버 과제 제출

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.json(agoraStatesDiscussions);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} = req.params;
+    const 검색한데이터
+       = agoraStatesDiscussions.filter((e)=>{
+        return e.id===Number(id)
+      });
+    if(검색한데이터.length===0){
+      res.status(404).send('Not Found!');
+    }else {
+      res.status(200).json(검색한데이터[0]);
+    }
   }
-
 };
 
 module.exports = {
