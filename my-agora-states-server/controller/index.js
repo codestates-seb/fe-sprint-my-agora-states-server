05403db4f9caaeb8c5978759e186d4a32cc05ed5
KimYMin <minmin4439@naver.com>
2023-06-07 13:06:03 +0900

    과제 제출입니다

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,23 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    return res.send(200, discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    //res.send('TODO:')
+    
+    const { id } = req.params;
+    const data = discussionsData.filter((el) => {
+      return el.id === Number(id)
+    })    
+    if(data.length === 0){
+      return res.status(404).json('fail')
+    }
+    else{
+      return res.status(200).json(data[0])
+    }
   }
 
 };
