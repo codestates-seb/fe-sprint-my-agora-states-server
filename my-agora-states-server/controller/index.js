78a6245138fd2d549d5c1bfcde80d7962f63704d
Jun Seong <drawbeat0112@gmail.com>
2023-06-07 14:19:40 +0900

    Bare Minimums done

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // const {author} = req.query;
+    // if(author){
+    //   res.json(discussionsData.filter((item) => item.author === author))
+    // }else{
+    //   res.json(discussionsData);
+    // }
+    res.status(200).json(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+
+    const filterData = discussionsData.filter((discussion) => discussion.id === Number(req.params.id))
+    if(filterData.length === 0){
+      res.status(404).send('Not Found');
+    }
+    res.send(...filterData);
   }
 
 };
