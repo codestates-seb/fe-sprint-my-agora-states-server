c55928eb509aa6424cbdfeebec2374789a4808fb
MS_btff <whdghks845@gmail.com>
2023-06-07 13:17:21 +0900

    agora

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -9,9 +9,19 @@ const discussionsController = {
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    const result = discussionsData.find((el) => el.id === Number(id));
-    if (result) res.status(200).json(result);
-    else res.status(404).send('오류입니다.');
+    const {id} = req.params;
+    let filteredData = discussionsData.filter((discussion) => {
+      //req.params.id가 string 형태로 들어온다. Number로 변환
+      return discussion.id === Number(id);
+    });
+    if (filteredData.length === 0) {
+      //필터되어 나온 게 없다면
+      return res.status(404).json('${id} is not found');
+    } else {
+      //필터되어서 나온 게 있다면
+      return res.status(200).json(filteredData[0]);
+      //[0]으로 뽑아와야만 출력이 된다.
+    }
   },
 };
 
