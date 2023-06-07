3a34ca1db5b060519bbd0dd8d02e0e7dd656b18b
사월 <april35592@gmail.com>
2023-06-07 11:36:16 +0900

    Bare Minimum 완성

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,14 +4,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.status(200).send(discussionsData);
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
-  }
-
+    console.log(req.params.id);
+    const result = discussionsData.find(
+      (item) => item.id === Number(req.params.id)
+    );
+    console.log(result);
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send();
+    }
+  },
 };
 
 module.exports = {
