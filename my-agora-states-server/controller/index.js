8f71049c0e5d976c8bd66c6cd37fd5ada9cdb524
udaeng8286 <udaenglvup@gmail.com>
2023-06-07 13:12:00 +0900

    과제 완료

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -4,12 +4,19 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    res.send(discussionsData)
   },
 
   findById: (req, res) => {
     // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const {id} = req.params;
+    const filtered = discussionsData.filter(discussion => {
+      return discussion.id.toString() === id
+    })
+    if (filtered.length === 0) {
+      res.status(404).send("에러다잉");
+    }
+    res.send(...filtered)
   }
 
 };
