afe87a208e9f3aeaac550166f3bf790db77f3708
kwonis <gwon-inseung@gwon-inseung-ui-MacBookAir.local>
2023-06-07 11:36:37 +0900

    과제

--- my-agora-states-server/controller/index.js
+++ my-agora-states-server/controller/index.js
@@ -3,16 +3,21 @@ const discussionsData = agoraStatesDiscussions;
 
 const discussionsController = {
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
-    res.send('TODO:')
+    // 모든 discussions 목록을 응답합니다.
+    res.send(discussionsData);
   },
 
   findById: (req, res) => {
-    // TODO: 요청으로 들어온 id와 일치하는 discussion을 응답합니다.
-    res.send('TODO:')
+    const id = req.params.id;
+    const discussion = discussionsData.find(item => item.id === parseInt(id));
+  
+    if (discussion) {
+      res.send(discussion);
+    } else {
+      res.status(404).send("Discussion not found");
+    }
   }
-
-};
+}  
 
 module.exports = {
   discussionsController,
